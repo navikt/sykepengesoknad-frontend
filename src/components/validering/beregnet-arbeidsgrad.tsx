de557be789fba0cf8ffd2129405be8dfda13fc6d
Kjell Arne Brødreskift <kjarbr@gmail.com>
2019-08-12 15:09:15 +0200

    Validering

---
+++ src/components/validering/beregnet-arbeidsgrad.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { getHtmlLedetekst } from '@navikt/digisyfo-npm';
+import { Normaltekst } from 'nav-frontend-typografi';
+import parser from 'html-react-parser';
+import { TidsPeriode } from '../../types/types';
+import { antallVirkedagerIPeriode, antallVirkedagerIPerioder } from '../../utils/periode-utils';
+import { RSSoknadsperiode } from '../../types/rs-types/rs-soknadsperiode';
+
+interface BeregnetArbeidsgradProps {
+    stillingsprosent: number,
+}
+
+const BeregnetArbeidsgrad = ({ stillingsprosent }: BeregnetArbeidsgradProps) => {
+    return (
+        <Normaltekst>
+            {parser(getHtmlLedetekst('sykepengesoknad.angi-tid.dette-tilsvarer', {
+                '%STILLINGSPROSENT%': stillingsprosent,
+            }))}
+        </Normaltekst>
+    );
+};
+
+export default BeregnetArbeidsgrad;
+
+export const getStillingsprosent = (
+    antallTimerIArbeid: string,
+    normalArbeidstid: string,
+    periode: RSSoknadsperiode,
+    ferieOgPermisjonPerioder: TidsPeriode[] = []) => {
+
+    const ANTALL_VIRKEDAGER_I_EN_UKE = 5;
+    const virkedager = antallVirkedagerIPeriode(periode) - antallVirkedagerIPerioder(ferieOgPermisjonPerioder, periode.fom);
+    const _antallTimerIArbeid = tilInt(antallTimerIArbeid);
+    const _normalArbeidstid = tilInt(normalArbeidstid);
+    const desimaltall = _antallTimerIArbeid / ((_normalArbeidstid / ANTALL_VIRKEDAGER_I_EN_UKE) * virkedager);
+
+    if (!_antallTimerIArbeid || !_normalArbeidstid || virkedager === 0) {
+        return 0;
+    }
+    return Math.round(desimaltall * 100);
+};
+
+const tilInt = (streng: string): number => {
+    return !streng ? 0 : parseFloat(streng.replace(',', '.'));
+};
---
+++ src/components/validering/valider-graderte-arbeidssporsmal.ts
@@ -0,0 +1,83 @@
+import { fraInputdatoTilJSDato, getLedetekst } from '@navikt/digisyfo-npm';
+import { SvarEnums, TagTyper } from '../../types/enums';
+import { Soknad, Sporsmal } from '../../types/types';
+import { fjernIndexFraTag, formaterEnkeltverdi } from '../sporsmal/field-utils';
+import { getStillingsprosent } from './beregnet-arbeidsgrad';
+
+export const hentFerieOgPermisjonperioder = (skjemaverdier: any) => {
+    const harHattFerie = formaterEnkeltverdi(skjemaverdier[TagTyper.FERIE_V2]) === SvarEnums.JA;
+    const harHattPermisjon = formaterEnkeltverdi(skjemaverdier[TagTyper.PERMISJON_V2]) === SvarEnums.JA;
+    const ferieperioder = harHattFerie ? skjemaverdier[TagTyper.FERIE_NAR_V2] : [];
+    const permisjonperioder = harHattPermisjon ? skjemaverdier[TagTyper.PERMISJON_NAR_V2] : [];
+
+    return [
+        ...ferieperioder,
+        ...permisjonperioder,
+    ].map((periode) => {
+        return {
+            fom: fraInputdatoTilJSDato(periode.fom),
+            tom: fraInputdatoTilJSDato(periode.tom),
+        };
+    });
+};
+
+const validerGraderteArbeidssporsmal = (sporsmalsliste: Sporsmal[], skjemaverdier: any, soknad: Soknad) => {
+    const feilmeldinger: any = {};
+    const graderteArbeidssporsmal = sporsmalsliste
+        .filter((sporsmal) => {
+            return fjernIndexFraTag(sporsmal.tag) === TagTyper.JOBBET_DU_GRADERT;
+        })
+        .filter((sporsmal) => {
+            return formaterEnkeltverdi(skjemaverdier[sporsmal.tag]) === sporsmal.kriterieForVisningAvUndersporsmal;
+        })
+        .filter((sporsmal) => {
+            const feriesporsmal = soknad.sporsmal.find((spm) => {
+                return spm.tag === TagTyper.FERIE_V2;
+            });
+            const feriePermUtlandsporsmal = soknad.sporsmal.find((spm) => {
+                return spm.tag === TagTyper.FERIE_PERMISJON_UTLAND;
+            });
+            const hovedsporsmalTags = soknad.sporsmal.map((s) => {
+                return s.tag;
+            });
+            return feriesporsmal
+                ? hovedsporsmalTags.indexOf(sporsmal.tag) > hovedsporsmalTags.indexOf(TagTyper.FERIE_V2)
+                : !feriePermUtlandsporsmal;
+        });
+    graderteArbeidssporsmal.forEach((gradertArbeidssporsmal) => {
+        const index = parseInt(gradertArbeidssporsmal.tag.split(`${TagTyper.JOBBET_DU_GRADERT}_`)[1], 10);
+        const erSvarOppgittITimer = formaterEnkeltverdi(skjemaverdier[leggIndexPaTag(TagTyper.HVOR_MYE_TIMER, index)]);
+
+        if (erSvarOppgittITimer) {
+            const antallTimerPerNormalUke = formaterEnkeltverdi(skjemaverdier[leggIndexPaTag(TagTyper.HVOR_MANGE_TIMER_PER_UKE, index)]);
+            const antallTimerJobbet = formaterEnkeltverdi(skjemaverdier[leggIndexPaTag(TagTyper.HVOR_MYE_TIMER_VERDI, index)]);
+            const periode = soknad.soknadPerioder[index];
+            // @ts-ignore
+            const minsteArbeidsgrad: number = gradertArbeidssporsmal.undersporsmal
+                .find((underspm: Sporsmal) => {
+                    return fjernIndexFraTag(underspm.tag) === TagTyper.HVOR_MYE_HAR_DU_JOBBET;
+                })
+                .undersporsmal.find((underspm) => {
+                    return fjernIndexFraTag(underspm.tag) === TagTyper.HVOR_MYE_PROSENT;
+                })
+                .undersporsmal.find((underspm) => {
+                    return fjernIndexFraTag(underspm.tag) === TagTyper.HVOR_MYE_PROSENT_VERDI;
+                }).min;
+            const arbeidsgrad = getStillingsprosent(antallTimerJobbet, antallTimerPerNormalUke, periode, hentFerieOgPermisjonperioder(skjemaverdier));
+            if (arbeidsgrad < minsteArbeidsgrad) {
+                const key = leggIndexPaTag(TagTyper.HVOR_MYE_TIMER_VERDI, index);
+                feilmeldinger[key] = getLedetekst(`soknad.feilmelding.${TagTyper.HVOR_MYE_TIMER_VERDI.toLowerCase()}.min`, {
+                    '%MIN%': minsteArbeidsgrad - 1,
+                });
+            }
+        }
+    });
+
+    return feilmeldinger;
+};
+
+export default validerGraderteArbeidssporsmal;
+
+const leggIndexPaTag = (tag: TagTyper, index: number): string => {
+    return `${tag}_${index}`;
+};
