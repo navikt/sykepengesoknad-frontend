97e021554b72d64314086ac659b7aef877a1a09e
Kjell Arne Brødreskift <kjarbr@gmail.com>
2019-10-02 08:28:16 +0200

    Flyttet konvertert kode fra gammel app ut av prosjektet

--- ubrukt/components/sporsmal/checkbox-panel.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import CheckboxComp from './checkbox-comp';
-import CheckboxSelvstendig from '../skjema/checkbox-selvstendig';
-
-export default (props: any) => {
-    return <CheckboxComp {...props} renderComponent={CheckboxSelvstendig} />;
-};
