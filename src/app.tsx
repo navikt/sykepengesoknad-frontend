0dc28b9bfabc934c08446415bd4e30abcb5d2e1a
Kent Daleng <kent.stefan.daleng@nav.no>
2019-10-31 11:44:33 +0100

    Fjerner dekoratør

--- src/app.tsx
+++ src/app.tsx
@@ -4,19 +4,12 @@ import Soknad from './pages/soknaden/soknaden';
 import Soknader from './pages/soknader/soknader';
 import StoreProvider from './data/stores/store-provider';
 import { DataFetcher } from './data/data-fetcher';
-import Decorator from '@navikt/react-decorator';
 import Sporsmalene from './pages/sporsmal/sporsmalene';
 import Kvittering from './pages/kvittering/kvittering';
 import './app.less';
 
-type Miljo = 'LOCAL' | 'DEV' | 'PROD';
-
 const App = (): any => {
-    const {hostname} = window.location;
-    const miljo = (hostname.indexOf('localhost') > -1 ? 'LOCAL' : 'DEV') as Miljo;
-
     return (
-        <Decorator miljo={miljo}>
             <StoreProvider>
                 <DataFetcher>
                     <main id="maincontent" role="main" tabIndex={-1}>
@@ -31,7 +24,6 @@ const App = (): any => {
                     </main>
                 </DataFetcher>
             </StoreProvider>
-        </Decorator>
     );
 };
 
