9496e1c7fbdc35ddc953361c17b89adc52f6b03a
Kjell Arne Brødreskift <kjarbr@gmail.com>
2019-12-17 13:38:12 +0100

    Styling av form-elementer

--- src/components/sporsmal/sporsmal-form/sporsmal-form.tsx
+++ src/components/sporsmal/sporsmal-form/sporsmal-form.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import { useAppStore } from '../../data/stores/app-store';
-import { Sporsmal } from '../../types/types';
-import FeilOppsummering from '../skjema/feiloppsummering/feil-oppsummering';
+import { useAppStore } from '../../../data/stores/app-store';
+import { Sporsmal } from '../../../types/types';
+import FeilOppsummering from '../../skjema/feiloppsummering/feil-oppsummering';
 import useForm, { FormContext } from 'react-hook-form';
-import Knapperad from './sporsmal-form/knapperad';
-import SporsmalSwitch from './undersporsmal/sporsmal-switch';
-import { pathUtenSteg, settSvar } from './sporsmal-utils';
-import './sporsmal-form/sporsmal-form.less';
+import Knapperad from './knapperad';
+import SporsmalSwitch from '../undersporsmal/sporsmal-switch';
+import { pathUtenSteg, settSvar } from '../sporsmal-utils';
+import './sporsmal-form.less';
 
 export interface SpmProps {
     sporsmal: Sporsmal;
 }
 
 const SporsmalForm = () => {
-    const { setValgtSoknad, valgtSoknad } = useAppStore();
+    const { setValgtSoknad, valgtSoknad, setVisUnderspm } = useAppStore();
     const { stegId } = useParams();
     const history = useHistory();
     const spmIndex = parseInt(stegId) - 1;
     const methods = useForm();
     const sporsmal = valgtSoknad.sporsmal[spmIndex];
-    //console.log('sporsmal', sporsmal); // eslint-disable-line
 
     const onSubmit = (data: Record<string, any>) => {
+        setVisUnderspm(false);
         settSvar(sporsmal, data);
         methods.reset();
         methods.unregister('spm_' + sporsmal.id);
