c82028e08cf316aead43a70270e2b59f7ca247e3
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-02-12 12:27:17 +0100

    Lar oss utvikle mot lokal backend

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import env from './utils/environment';
 import App from './app';
 import dayjs from 'dayjs';
 import 'dayjs/locale/nb';
+
 dayjs.locale('nb');
 
 if (env.isDevelopment || env.isRunningOnHeroku) {
-    require('./data/mock');
+    if (process.env.REACT_APP_BACKEND_PROXY !== "true") {
+        require('./data/mock');
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+ReactDOM.render(<App/>, document.getElementById('root') as HTMLElement);
