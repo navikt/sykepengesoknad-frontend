0f1ccc4d64bac95628c21187fdbeab57a98c218c
Kent Daleng <kent.stefan.daleng@nav.no>
2020-02-12 13:13:27 +0100

    Begynner arbeid på avbryt søknad-dialog

--- src/components/sporsmal/sporsmal-form/knapperad-tekster.ts
+++ src/components/sporsmal/sporsmal-form/knapperad-tekster.ts
@@ -3,6 +3,9 @@ const KnapperadTekster: any = {
         'sykepengesoknad.ga-videre': 'Gå videre',
         'sykepengesoknad.send': 'Send søknaden',
         'sykepengesoknad.avbryt.trigger': 'Jeg ønsker ikke å bruke denne søknaden',
+        'sykepengesoknad.avbryt.sporsmal': 'Er du sikker på at du ikke ønsker å bruke denne søknaden?',
+        'sykepengesoknad.avbryt.ja': 'Ja, jeg er sikker',
+        'sykepengesoknad.avbryt.angre': 'Angre'
     }, 'en': {}
 };
 
--- src/components/sporsmal/sporsmal-form/knapperad.tsx
+++ src/components/sporsmal/sporsmal-form/knapperad.tsx
@@ -1,30 +1,58 @@
-import React from 'react';
-import { Knapp } from 'nav-frontend-knapper';
-import { Normaltekst } from 'nav-frontend-typografi';
+import React, {MouseEvent, useEffect, useRef, useState} from 'react';
+import {Knapp} from 'nav-frontend-knapper';
+import {Normaltekst} from 'nav-frontend-typografi';
 import tekster from './knapperad-tekster';
-import { Link, useParams } from 'react-router-dom';
-import { useAppStore } from '../../../data/stores/app-store';
+import {useParams} from 'react-router-dom';
+import {useAppStore} from '../../../data/stores/app-store';
+import Vis from "../../vis";
+import {getTop} from "../../../utils/browser-utils";
+
+type Event = MouseEvent<HTMLAnchorElement | HTMLButtonElement>;
 
 interface KnapperadProps {
     onSubmit: Function;
 }
 
-const Knapperad = ({ onSubmit }: KnapperadProps) => {
-    const { valgtSoknad } = useAppStore();
-    const { stegId } = useParams();
+const Knapperad = ({onSubmit}: KnapperadProps) => {
+    const {valgtSoknad} = useAppStore();
+    const {stegId} = useParams();
     const spmIndex = parseInt(stegId) - 2;
     const nokkel = spmIndex === valgtSoknad.sporsmal.length - 3
         ? 'sykepengesoknad.send'
         : 'sykepengesoknad.ga-videre';
+    const avbrytDialog = useRef<HTMLDivElement>(null);
+    const [vilAvbryte, setVilAvbryte] = useState<boolean>(false);
 
-    // TODO: Lenke i avbryt-knapp mangler
+    useEffect(() => {
+        if (vilAvbryte) {
+            const end = getTop(avbrytDialog.current, 600);
+            window.scrollTo(end, 800);
+        }
+    }, [vilAvbryte]);
+
+    const handleVilAvbryte = (event: Event) => {
+        event.preventDefault();
+        setVilAvbryte(!vilAvbryte);
+    };
+
+    const handleAvbryt = (event: Event) => {
+        event.preventDefault();
+        alert(`Nå vil jeg avbryte ${valgtSoknad.id}!!!`);
+    };
 
     return (
         <div className="knapperad">
             <Knapp type="hoved" onClick={() => onSubmit}>{tekster[nokkel]}</Knapp>
-            <Link to={'asdf'} className="avbrytlenke">
-                <Normaltekst tag="span">{tekster['sykepengesoknad.avbryt.trigger']}</Normaltekst>
-            </Link>
+            <div className="avbrytDialog blokk-l">
+                <a className="lenke avbrytlenke avbrytDialog__trigger" onClick={handleVilAvbryte}>{tekster['sykepengesoknad.avbryt.trigger']}</a>
+                <Vis hvis={vilAvbryte}>
+                    <div ref={avbrytDialog} className="avbrytDialog__dialog pekeboble">
+                        <Normaltekst className="blokk-s">{tekster['sykepengesoknad.avbryt.sporsmal']}</Normaltekst>
+                        <Knapp className="blokk-xs" onClick={handleAvbryt} type="fare">{tekster['sykepengesoknad.avbryt.ja']}</Knapp>
+                        <a className="lenke" onClick={handleVilAvbryte}>{tekster['sykepengesoknad.avbryt.angre']}</a>
+                    </div>
+                </Vis>
+            </div>
         </div>
     )
 };
