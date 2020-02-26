8905c0f57866f5c0e15372fb434a5ab5662b6264
kulsveen <bjorn.inge.kulsveen@nav.no>
2020-02-26 16:06:24 +0100

    Setter location i test og npm update

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './app';
 
 it('Rendrer søknadens startside', async () => {
-    render(<App />);
+    render(
+        <MemoryRouter initialEntries={["/"]}>
+            <App/>
+        </MemoryRouter>);
 
     expect(screen.queryAllByTitle('Søknad om sykepenger'));
 });
