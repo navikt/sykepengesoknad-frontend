5eefa4e0d20b187395c299d30351ed45b3ca1f35
Kjell Arne Brødreskift <kjarbr@gmail.com>
2020-05-13 09:44:09 +0200

    Lagt til cypress e2e-test-rammeverk

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
+
 describe('CRA', () => {
-    it('shows learn link', function() {
-        // eslint-disable-next-line no-undef
-        cy.visit('http://localhost:8080')
-        // eslint-disable-next-line no-undef
-        cy.get('.App-link').should('be.visible')
-            .and('have.text', 'Learn React')
+    it('shows learn link', function () {
+        cy.visit('http://localhost:8080/nysykepengesoknad');
+        cy.get('.sidetopp__tittel').should('be.visible').and('have.text', 'Søknader om sykepenger');
+        cy.contains('Gjelder perioden 1. – 10. juni 2019').click();
+        cy.url().should('include', '977ce8fc-a83a-4454-ab81-893ff0284437/1');
     })
 })
