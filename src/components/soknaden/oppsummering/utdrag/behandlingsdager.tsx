499eaa46a52c1ffc6b4baaa4ba0544c129896439
kulsveen <bjorn.inge.kulsveen@nav.no>
2020-05-28 11:59:02 +0200

    Bruker lesbarDato i oppsummering

--- src/components/soknaden/oppsummering/utdrag/behandlingsdager.tsx
+++ src/components/soknaden/oppsummering/utdrag/behandlingsdager.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { OppsummeringProps } from '../oppsummering';
 import { Element, Normaltekst } from 'nav-frontend-typografi';
 import Vis from '../../../vis';
-import dayjs from 'dayjs';
-import { tilLesbarPeriodeUtenArstall } from '../../../../utils/dato-utils';
+import { tilLesbarDatoUtenAarstall, tilLesbarPeriodeUtenArstall } from '../../../../utils/dato-utils';
 import { RSSvar } from '../../../../types/rs-types/rs-svar';
 
 const datoEllerIkkeTilBehandling = (svar: RSSvar) => {
     if (svar === undefined || svar.verdi === '') {
         return 'Ikke til behandling';
     }
-    return dayjs(svar.verdi).format('DD.MM.YYYY');
+    return tilLesbarDatoUtenAarstall(svar.verdi);
 };
 
 const Behandlingsdager = ({ sporsmal }: OppsummeringProps) => {
