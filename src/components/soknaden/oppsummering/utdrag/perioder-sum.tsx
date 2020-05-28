8d3d96f69b47e1fdcfad96cd020a4ffcc3bda95d
kulsveen <bjorn.inge.kulsveen@nav.no>
2020-05-28 12:41:52 +0200

    Invalid date i periode oppsummering

--- src/components/soknaden/oppsummering/utdrag/perioder-sum.tsx
+++ src/components/soknaden/oppsummering/utdrag/perioder-sum.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Element, Normaltekst } from 'nav-frontend-typografi';
 import { OppsummeringProps } from '../oppsummering';
-import dayjs from 'dayjs';
 import Vis from '../../../vis';
-import { empty, PERIODE_SKILLE } from '../../../../utils/constants';
+import { empty } from '../../../../utils/constants';
+import { tilLesbarPeriodeMedArstall } from '../../../../utils/dato-utils';
+import { hentPeriode } from '../../../sporsmal/hent-svar';
 
 const PerioderSum = ({ sporsmal }: OppsummeringProps) => {
     return (
@@ -11,14 +12,11 @@ const PerioderSum = ({ sporsmal }: OppsummeringProps) => {
             <Element tag='h3'>{sporsmal.sporsmalstekst}</Element>
             <div className='oppsummering__tekstsvar'>
                 {sporsmal.svarliste.svar.map((p, i) => {
-                    const periode = p.verdi.toString().split(PERIODE_SKILLE);
+                    const periode = hentPeriode(sporsmal, i);
                     return (
                         <Vis hvis={p.verdi !== empty} key={i}>
                             <Normaltekst className='oppsummering__dato'>
-                                {
-                                    'Fra ' + dayjs(periode[0]).format('DD.MM.YYYY') +
-                                    ' til ' + dayjs(periode[1]).format('DD.MM.YYYY')
-                                }
+                                { tilLesbarPeriodeMedArstall(periode[0], periode[1]) }
                             </Normaltekst>
                         </Vis>
                     );
