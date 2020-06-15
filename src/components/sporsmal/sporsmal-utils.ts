93108f168e8b63593152567b037d32af96fff835
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-06-15 14:36:15 +0200

    Fjern feilmelding når det endres

--- src/components/sporsmal/sporsmal-utils.ts
+++ src/components/sporsmal/sporsmal-utils.ts
@@ -40,16 +40,6 @@ export const fjernIndexFraTag = (tag: TagTyper): TagTyper => {
     return TagTyper[stringtag as keyof typeof TagTyper]
 }
 
-export const sporsmalIdListe = (sporsmal: Sporsmal[]) => {
-    let svar: any = []
-    sporsmal.forEach(spm => {
-        svar.push(spm.id)
-        const alleUndersporsmalId: any = sporsmalIdListe(spm.undersporsmal)
-        svar = [ ...svar, ...alleUndersporsmalId ]
-    })
-    return svar
-}
-
 interface FeilmeldingProps {
     global: string;
     lokal: string;
