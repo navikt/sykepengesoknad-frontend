1682439de28f49ae335cd140b9e8d2d5260be0a7
Kent Daleng <kent.stefan.daleng@nav.no>
2020-06-22 18:08:18 +0200

    Fikser skrivefeil, fjerner noen whitespaces

--- src/components/soknad-intro/soknad-intro-tekster.ts
+++ src/components/soknad-intro/soknad-intro-tekster.ts
@@ -1,36 +1,36 @@
 const SoknadIntroTekster: any = {
     'nb': {
-        'sykepengesoknad.soknad-intro.personvern': '<p>Ifølge folketrygdloven kan du få sykepenger hvis du oppfyller kravene. or å utbetale riktig trenger vi å vite hva som skjedde i perioden du var sykmeldt. Derfor må du søke hver gang vi ber deg om det. </p>',
+        'sykepengesoknad.soknad-intro.personvern': '<p>Ifølge folketrygdloven kan du få sykepenger hvis du oppfyller kravene. For å utbetale riktig trenger vi å vite hva som skjedde i perioden du var sykmeldt. Derfor må du søke hver gang vi ber deg om det.</p>',
         'sykepengesoknad.soknad-intro.personvern-les-mer': 'Les mer om hvordan NAV behandler personopplysninger',
         'sykepengesoknad.soknad-intro.personvern-modal-header': 'Slik behandler NAV personopplysningene dine',
         'sykepengesoknad.soknad-intro.personvern-modal-innhold':
-            `<p>Du har valgt å søke om sykepenger fra NAV.  Det er penger du kan få hvis du fyller kravene. Du kan lese om kravene på <a target="_blank" rel="noopener noreferrer" href="https://nav.no/sykepenger">nav.no/sykepenger</a>. </p>
-            <p>NAV innhenter opplysninger om deg når du sender inn en søknad. Det er nødvendig for at du skal kunne få det du har krav på. For å behandle søknaden din henter vi inn opplysninger både fra deg og fra offentlige registre. </p>
-            <p>Søknaden din vil bli behandlet automatisk hvis NAV har tilstrekkelige opplysninger. Den automatiske behandlingen baserer seg på følgende opplysninger: </p>
+            `<p>Du har valgt å søke om sykepenger fra NAV. Det er penger du kan få hvis du fyller kravene. Du kan lese om kravene på <a target="_blank" rel="noopener noreferrer" href="https://nav.no/sykepenger">nav.no/sykepenger</a>.</p>
+            <p>NAV innhenter opplysninger om deg når du sender inn en søknad. Det er nødvendig for at du skal kunne få det du har krav på. For å behandle søknaden din henter vi inn opplysninger både fra deg og fra offentlige registre.</p>
+            <p>Søknaden din vil bli behandlet automatisk hvis NAV har tilstrekkelige opplysninger. Den automatiske behandlingen baserer seg på følgende opplysninger:</p>
             <ul>
             <li>
-                Folkeregisteret: <b>navn, fødselsnummer, adresse, familie o.l.</b> 
+                Folkeregisteret: <b>navn, fødselsnummer, adresse, familie o.l.</b>
             </li>
             <li>
-                Arbeidstaker- og arbeidsgiverregisteret og Enhetsregisteret: <b>arbeidsforholdene dine </b> 
+                Arbeidstaker- og arbeidsgiverregisteret og Enhetsregisteret: <b>arbeidsforholdene dine</b>
             </li>
             <li>
-                A-ordningen: <b>inntekten din </b> 
+                A-ordningen: <b>inntekten din</b>
             </li>
             <li>
-                NAV: <b>andre utbetalinger du får </b> 
+                NAV: <b>andre utbetalinger du får</b>
             </li>
             <li>
-                Behandleren din: <b>medisinske opplysninger om deg</b> 
+                Behandleren din: <b>medisinske opplysninger om deg</b>
             </li>
             <li>
-                Arbeidsgiveren din: <b>inntektsmelding</b> 
+                Arbeidsgiveren din: <b>inntektsmelding</b>
             </li>
             <li>
-                Andre land: <b>trygdeordninger du kan ha rett til</b> 
-            </li>                                
+                Andre land: <b>trygdeordninger du kan ha rett til</b>
+            </li>
             </ul>
-            <p>Vi kan også sende opplysninger om deg til trygdeordninger i andre land. </p>
+            <p>Vi kan også sende opplysninger om deg til trygdeordninger i andre land.</p>
             <p>Du kan lese mer om hvordan NAV behandler personopplysninger i Arbeids- og velferdsetatens personvernerklæring på <a target="_blank" rel="noopener noreferrer" href="https://nav.no/personvern">nav.no/personvern</a>.</p>
             `,
     }, 'en': {}
