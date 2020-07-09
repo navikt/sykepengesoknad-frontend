fad8e02aa159ac3da67e583eef979cc2e060f88d
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-07-09 10:58:38 +0200

    Fiks url'er i q og p

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -1,5 +1,4 @@
 import { Soknad } from '../types/types'
-import env from './environment'
 
 export const getUrlTilSoknad = (soknad: Soknad, stegId: string | undefined) => {
     const baseUrl = `/soknader/${soknad.id}`
@@ -9,15 +8,6 @@ export const getUrlTilSoknad = (soknad: Soknad, stegId: string | undefined) => {
 }
 
 
-const basenameUtenTrailingSlash = () => {
-    const baseName = env.baseName
-    if (baseName.endsWith('/')) {
-        return baseName.substring(0, baseName.length - 1)
-    }
-    return baseName
-}
+export const utlandssoknadUrl = '/sykepengesoknad-utland'
 
-
-export const utlandssoknadUrl = basenameUtenTrailingSlash() + '/sykepengesoknad-utland'
-
-export const oversiktside = basenameUtenTrailingSlash()
+export const oversiktside = '/'
