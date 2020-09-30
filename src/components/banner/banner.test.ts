daa3e9d66f18ab11f4c6fc75ff4c6d4577dbadf9
Kjell Arne Brødreskift <kjarbr@gmail.com>
2020-09-30 13:04:23 +0200

    Rettet feil i banner-test

--- src/components/banner/banner.test.ts
+++ src/components/banner/banner.test.ts
@@ -1,18 +1,8 @@
 import 'dayjs/locale/nb'
 
-import dayjs from 'dayjs'
-
-import { getLedetekst, tekst } from '../../utils/tekster'
+import { tekst } from '../../utils/tekster'
 
 it('Returns text from bundle', () => {
-    const text = tekst('sykepengesoknad.sidetittel.periode-2')
-    expect(text).toEqual('Gjelder for perioden %PERIODE%')
-})
-
-it('Returns text from bundle', () => {
-    expect(getLedetekst(tekst('sykepengesoknad.sidetittel.periode-2'),
-        {
-            '%PERIODE%': dayjs('2019-10-01').locale('nb').format('DD. MMMM YYYY') + ' - ' +
-                dayjs('2020-01-20').locale('nb').format('DD. MMMM YYYY')
-        })).toEqual('Gjelder for perioden 01. oktober 2019 - 20. januar 2020')
+    const text = tekst('soknader.sidetittel')
+    expect(text).toEqual('Søknader om sykepenger')
 })
