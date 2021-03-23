25c583861e18d838321b9dbbd0745844891b5e7e
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-03-23 12:28:31 +0100

    Endre testdata vær klar over at

--- src/data/mock/data/reisetilskudd.ts
+++ src/data/mock/data/reisetilskudd.ts
@@ -205,8 +205,7 @@ export const nyttReisetilskudd: RSSoknad = {
             'id': '361',
             'tag': 'VAER_KLAR_OVER_AT',
             'sporsmalstekst': 'Viktig å være klar over:',
-            // TODO: Denne kan ikke være den samme som for sykepengesøknad
-            'undertekst': '<ul><li>Du kan bare få sykepenger hvis det er din egen sykdom eller skade som hindrer deg i å jobbe. Sosiale eller økonomiske problemer gir ikke rett til sykepenger.</li><li>Du kan miste retten til sykepenger hvis du nekter å opplyse om din egen arbeidsevne, eller hvis du ikke tar imot behandling eller tilrettelegging.</li><li>Retten til sykepenger gjelder bare inntekt du har mottatt som lønn og betalt skatt av på sykmeldingstidspunktet.</li><li>NAV kan innhente opplysninger som er nødvendige for å behandle søknaden.</li><li>Du må melde fra til NAV hvis du satt i varetekt, sonet straff eller var under forvaring i sykmeldingsperioden.</li><li>Fristen for å søke sykepenger er som hovedregel 3 måneder</li></ul><p>Du kan lese mer om rettigheter og plikter på <a href="https://www.nav.no/sykepenger" target="_blank">nav.no/sykepenger</a>.</p>',
+            'undertekst': '<ul><li>Retten til reisetilskudd gjelder bare hvis du trenger midlertidig transport til og fra arbeidsstedet på grunn av helseplager.</li><li>Du kan få reisetilskudd hvis du i utgangspunktet har rett til sykepenger.</li><li>NAV kan innhente flere opplysninger som er nødvendige for å behandle søknaden.</li><li>NAV kan holde igjen eller kreve tilbake penger hvis du gir uriktige eller ufullstendige opplysninger.</li><li>Det å gi feil opplysninger kan være straffbart.</li><li>Fristen for å søke sykepenger er som hovedregel 3 måneder.</li></ul><p>Du kan lese mer om rettigheter og plikter på <a href="https://www.nav.no/sykepenger" target="_blank">nav.no/sykepenger</a>.</p>',
             'svartype': 'IKKE_RELEVANT',
             'min': null,
             'max': null,
