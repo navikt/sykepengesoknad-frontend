6d7b0f7a9dcdac292b113dd704a1c99777e252a5
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-06-01 07:35:18 +0200

    Bjørn i periode utland (#311)

--- src/components/sporsmal/bjorn/bjorn-under-tittel.tsx
+++ src/components/sporsmal/bjorn/bjorn-under-tittel.tsx
@@ -17,6 +17,7 @@ const BjornUnderTittel = ({ sporsmal }: SpmProps) => {
         tag === TagTyper.FRAVER_FOR_BEHANDLING ||
         tag === TagTyper.PERMISJON_V2 ||
         tag === TagTyper.PERMITTERT_NAA ||
+        tag === TagTyper.PERIODEUTLAND ||
         tag === TagTyper.PERMITTERT_PERIODE ||
         (tag === TagTyper.FERIE_V2 && valgtSoknad?.soknadstype === RSSoknadstype.ARBEIDSTAKERE)
 
