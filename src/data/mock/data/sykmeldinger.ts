95a2b1904cecf9ad8abd4a9dbe12ef046302a9ff
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-07-26 13:54:08 +0200

    Gradert reisetilskuddsøknad

--- src/data/mock/data/sykmeldinger.ts
+++ src/data/mock/data/sykmeldinger.ts
@@ -1071,11 +1071,111 @@ export const syk8 = {
     ]
 }
 
+export const gradertReisetilskuddSm =  {
+    'id': '76214ab1-2eae-439b-8f73-2713eb97989e',
+    'pasient': { 'fnr': '24127426046', 'fornavn': 'LUR', 'mellomnavn': null, 'etternavn': 'SNERK' },
+    'mottattTidspunkt': '2020-03-31T20:00:00Z',
+    'behandlingsutfall': { 'status': 'MANUAL_PROCESSING', 'ruleHits': [] },
+    'legekontorOrgnummer': '223456789',
+    'arbeidsgiver': { 'navn': 'LOMMEN BARNEHAVE', 'stillingsprosent': 100 },
+    'sykmeldingsperioder': [ {
+        'fom': '2020-04-01',
+        'tom': '2020-04-24',
+        'gradert': { 'grad': 60, 'reisetilskudd': true },
+        'behandlingsdager': null,
+        'innspillTilArbeidsgiver': null,
+        'type': 'GRADERT',
+        'aktivitetIkkeMulig': null,
+        'reisetilskudd': false
+    } ],
+    'sykmeldingStatus': {
+        'statusEvent': 'SENDT',
+        'timestamp': '2021-07-26T11:48:32.182605Z',
+        'arbeidsgiver': { 'orgnummer': '967170232', 'juridiskOrgnummer': '928497704', 'orgNavn': 'SNILL TORPEDO' },
+        'sporsmalOgSvarListe': [ {
+            'tekst': 'Jeg er sykmeldt som',
+            'shortName': 'ARBEIDSSITUASJON',
+            'svar': { 'svarType': 'ARBEIDSSITUASJON', 'svar': 'ARBEIDSTAKER' }
+        } ]
+    },
+    'medisinskVurdering': {
+        'hovedDiagnose': { 'kode': 'L87', 'system': 'ICPC-2', 'tekst': 'TENDINITT INA' },
+        'biDiagnoser': [ { 'kode': 'L87', 'system': 'ICPC-2', 'tekst': 'GANGLION SENE' } ],
+        'annenFraversArsak': null,
+        'svangerskap': false,
+        'yrkesskade': false,
+        'yrkesskadeDato': '2020-04-01'
+    },
+    'skjermesForPasient': false,
+    'prognose': {
+        'arbeidsforEtterPeriode': true,
+        'hensynArbeidsplassen': 'Må ta det pent',
+        'erIArbeid': {
+            'egetArbeidPaSikt': true,
+            'annetArbeidPaSikt': true,
+            'arbeidFOM': '2020-04-01',
+            'vurderingsdato': '2020-04-01'
+        },
+        'erIkkeIArbeid': null
+    },
+    'utdypendeOpplysninger': {
+        '6.2': {
+            '6.2.1': {
+                'sporsmal': 'Beskriv kort sykehistorie, symptomer og funn i dagens situasjon.',
+                'svar': 'Langvarig korsryggsmerter. Ømhet og smerte',
+                'restriksjoner': [ 'SKJERMET_FOR_ARBEIDSGIVER' ]
+            },
+            '6.2.2': {
+                'sporsmal': 'Hvordan påvirker sykdommen arbeidsevnen',
+                'svar': 'Kan ikke utføre arbeidsoppgaver 100% som kreves fra yrket. Duplikatbuster: ad349a50-38df-40bc-aa5a-1ff063cda46c',
+                'restriksjoner': [ 'SKJERMET_FOR_ARBEIDSGIVER' ]
+            },
+            '6.2.3': {
+                'sporsmal': 'Har behandlingen frem til nå bedret arbeidsevnen?',
+                'svar': 'Nei',
+                'restriksjoner': [ 'SKJERMET_FOR_ARBEIDSGIVER' ]
+            },
+            '6.2.4': {
+                'sporsmal': 'Beskriv Pågående og planlagt henvisning, utredning og/eller behandling',
+                'svar': 'Henvist til fysio',
+                'restriksjoner': [ 'SKJERMET_FOR_ARBEIDSGIVER' ]
+            }
+        }
+    },
+    'tiltakArbeidsplassen': 'Fortsett som sist.',
+    'tiltakNAV': 'Pasienten har plager som er kommet tilbake etter operasjon. Det er nylig tatt MR bildet som viser forandringer i hånd som mulig må opereres. Venter på time. Det er mulig sykemledingen vil vare utover aktuell sm periode. ',
+    'andreTiltak': null,
+    'meldingTilNAV': null,
+    'meldingTilArbeidsgiver': null,
+    'kontaktMedPasient': { 'kontaktDato': null, 'begrunnelseIkkeKontakt': null },
+    'behandletTidspunkt': '2020-04-01T00:00:00Z',
+    'behandler': {
+        'fornavn': 'Frida',
+        'mellomnavn': 'Perma',
+        'etternavn': 'Frost',
+        'adresse': {
+            'gate': 'Kirkegårdsveien 3',
+            'postnummer': 1348,
+            'kommune': 'Rykkinn',
+            'postboks': null,
+            'land': 'Country'
+        },
+        'tlf': 'tel:94431152'
+    },
+    'syketilfelleStartDato': '2020-04-01',
+    'navnFastlege': 'Victor Frankenstein',
+    'egenmeldt': false,
+    'papirsykmelding': false,
+    'harRedusertArbeidsgiverperiode': false,
+    'merknader': null
+}
+
 export const sykmeldinger = [
     arbeidstaker100Syk,
     arbeidstaker50Syk,
     arbeidsledig100Syk,
     frilanser100Syk,
+    gradertReisetilskuddSm,
     arbeidstakerBehandlingsdagSyk,
     arbeidstakerReisetilskuddSyk,
     syk1, syk2, syk3, syk4, syk5, syk7, syk8,
