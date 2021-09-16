5527bb9558394034158ac401b84d9baa9a87056b
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-09-16 15:38:56 +0200

    Riktig dekorator env

--- server/dekorator.js
+++ server/dekorator.js
@@ -2,7 +2,7 @@ const { injectDecoratorServerSide } = require('@navikt/nav-dekoratoren-moduler/s
 
 const getHtmlWithDecorator = (filePath) =>
     injectDecoratorServerSide({
-        env: 'prod',
+        env: process.env.DECORATOR_ENV,
         filePath: filePath,
         simple: 'true',
         chatbot: 'false',
