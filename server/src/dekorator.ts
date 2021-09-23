8ac690e3f76f23c734d32174fe3493c5fb48a15c
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2021-09-23 15:34:04 +0200

    Dekoratoren backend rendring (#367)
    * Bruker nav-dekoratoren-moduler@1.6.3
    * Bruk dekoratorenUrl til backend rending
    
    For at backend -endring av dekoratøren skal fungere i et docker-compose
    miljø må URL til dekoratøren sendes med som dekoratorenUrl.

--- server/src/dekorator.ts
+++ server/src/dekorator.ts
@@ -1,12 +1,13 @@
 import { injectDecoratorServerSide } from '@navikt/nav-dekoratoren-moduler/ssr'
 
+import { logger } from './logger'
 
 export const getHtmlWithDecorator = (filePath) =>
     injectDecoratorServerSide({
+        dekoratorenUrl: process.env.DECORATOR_URL,
         env: process.env.DECORATOR_ENV as any,
         filePath: filePath,
         simple: true,
         chatbot: false,
         urlLookupTable: false,
     })
-
