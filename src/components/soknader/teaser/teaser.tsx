868aa84b3342dc007e6ebe3ee65f22ed2fcfaac5
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-02-14 16:52:08 +0100

    Revert "Revert "Vis label for delvis utfylt søknad (#437)""
    This reverts commit ee61236a26ad93ba214b0785c376a78f2fbff8f2.

--- src/components/soknader/teaser/teaser.tsx
+++ src/components/soknader/teaser/teaser.tsx
@@ -5,40 +5,57 @@ import React from 'react'
 import { RSSoknadstype } from '../../../types/rs-types/rs-soknadstype'
 import { tilLesbarPeriodeMedArstall } from '../../../utils/dato-utils'
 import env from '../../../utils/environment'
-import { getUrlTilSoknad } from '../../../utils/url-utils'
+import { tekst } from '../../../utils/tekster'
+import { erDelvisUtfylt, getUrlTilSoknad } from '../../../utils/url-utils'
 import Vis from '../../vis'
-import { InngangsIkon, Inngangspanel } from '../inngang/inngangspanel'
-import { hentIkon, hentIkonHover, leggTilSoknadstypeForDemoside, periodeListevisning, SykepengesoknadTeaserProps, teaserTittel } from './teaser-util'
+import { InngangsIkon, Inngangspanel, InngangsStatus } from '../inngang/inngangspanel'
+import {
+    hentIkon,
+    hentIkonHover,
+    leggTilSoknadstypeForDemoside,
+    periodeListevisning,
+    SykepengesoknadTeaserProps,
+    teaserTittel
+} from './teaser-util'
 
 const Teaser = ({ soknad }: SykepengesoknadTeaserProps) => {
+
     return (
         <article
             aria-labelledby={`soknader-header-${soknad.id}`}
         >
             <Inngangspanel to={getUrlTilSoknad(soknad)} className="inngangspanel--ny">
-                <div className="inngangspanel__del1">
-                    <InngangsIkon
-                        ikon={hentIkon(soknad)}
-                        ikonHover={hentIkonHover(soknad)}
-                    />
-                    <div id={`soknader-header-${soknad.id}`} className="inngangspanel__innhold">
-                        <Vis hvis={soknad.soknadstype !== RSSoknadstype.OPPHOLD_UTLAND}
-                            render={() =>
-                                <Undertekst className="inngangspanel__periode">
-                                    {tilLesbarPeriodeMedArstall(soknad.fom, soknad.tom)}
-                                </Undertekst>
-                            }
+                <div className="inngangspanel__ytre">
+                    <div className="inngangspanel__del1">
+                        <InngangsIkon
+                            ikon={hentIkon(soknad)}
+                            ikonHover={hentIkonHover(soknad)}
                         />
-                        <Undertittel tag="h3" className="inngangspanel__tittel">
-                            {teaserTittel(soknad)}
-                        </Undertittel>
+                        <div id={`soknader-header-${soknad.id}`} className="inngangspanel__innhold">
+                            <Vis hvis={soknad.soknadstype !== RSSoknadstype.OPPHOLD_UTLAND}
+                                render={() =>
+                                    <Undertekst className="inngangspanel__periode">
+                                        {tilLesbarPeriodeMedArstall(soknad.fom, soknad.tom)}
+                                    </Undertekst>
+                                }
+                            />
+                            <Undertittel tag="h3" className="inngangspanel__tittel">
+                                {teaserTittel(soknad)}
+                            </Undertittel>
 
-                        {periodeListevisning(soknad)}
+                            {periodeListevisning(soknad)}
 
-                        <Vis hvis={env.isOpplaering()}
-                            render={() => leggTilSoknadstypeForDemoside(soknad)}
-                        />
+                            <Vis hvis={env.isOpplaering()}
+                                render={() => leggTilSoknadstypeForDemoside(soknad)}
+                            />
+                        </div>
                     </div>
+
+                    <Vis hvis={erDelvisUtfylt(soknad)}
+                        render={() =>
+                            <InngangsStatus status={soknad.status} tekst={tekst('soknad.teaser.delvis-utfylt.tekst')} />
+                        }
+                    />
                 </div>
                 <HoyreChevron />
             </Inngangspanel>
