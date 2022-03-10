ed669458266a7b0e8c887248bc34c74f4853a1ec
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2022-03-10 08:22:57 +0100

    Erstattet ekspanderbart panel med accordion

--- src/components/hvorfor-soknad-sykepenger/hvorfor-soknad-sykepenger.tsx
+++ src/components/hvorfor-soknad-sykepenger/hvorfor-soknad-sykepenger.tsx
@@ -1,8 +1,8 @@
 import './hvorfor-soknad-sykepenger.less'
 
-import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel'
+import { Accordion } from '@navikt/ds-react'
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi'
-import React from 'react'
+import React, { useState } from 'react'
 
 import { RSSoknadstype } from '../../types/rs-types/rs-soknadstype'
 import { tekst } from '../../utils/tekster'
@@ -12,36 +12,42 @@ interface HvorforSoknadSykepengerProps {
     soknadstype: RSSoknadstype
 }
 
-const HvorforSoknadSykepenger = ({ soknadstype }:HvorforSoknadSykepengerProps) => {
+const HvorforSoknadSykepenger = ({ soknadstype }: HvorforSoknadSykepengerProps) => {
+    const [ open, setOpen ] = useState<boolean>(false)
+
     return (
-        <Ekspanderbartpanel apen={false} className="hvorfor-soknad-sykepenger" tittel={
-            <Undertittel tag={'h2'}>{tekst('hvorfor-soknad-sykepenger.overskrift')}</Undertittel>
-        }>
-            <Vis hvis={soknadstype === RSSoknadstype.REISETILSKUDD}
-                render={() =>
-                    <Normaltekst>{tekst('hvorfor-soknad-reisetilskudd')}</Normaltekst>
-                }
-            />
+        <Accordion>
+            <Accordion.Item open={open} className="hvorfor-soknad-sykepenger">
+                <Accordion.Header onClick={() => setOpen(!open)}>
+                    <Undertittel tag="h2">{tekst('hvorfor-soknad-sykepenger.overskrift')}</Undertittel>
+                </Accordion.Header>
+                <Accordion.Content>
+                    <Vis hvis={soknadstype === RSSoknadstype.REISETILSKUDD}
+                        render={() =>
+                            <Normaltekst>{tekst('hvorfor-soknad-reisetilskudd')}</Normaltekst>
+                        }
+                    />
 
-            <Vis hvis={soknadstype === RSSoknadstype.GRADERT_REISETILSKUDD}
-                render={() =>
-                    <>
-                        <Normaltekst>{tekst('hvorfor-soknad-gradertreisetilskudd')}</Normaltekst>
-                        <Normaltekst>{tekst('hvorfor-soknad-sykepenger.jobba')}</Normaltekst>
-                    </>
-                }
-            />
+                    <Vis hvis={soknadstype === RSSoknadstype.GRADERT_REISETILSKUDD}
+                        render={() =>
+                            <>
+                                <Normaltekst>{tekst('hvorfor-soknad-gradertreisetilskudd')}</Normaltekst>
+                                <Normaltekst>{tekst('hvorfor-soknad-sykepenger.jobba')}</Normaltekst>
+                            </>
+                        }
+                    />
 
-            <Vis hvis={soknadstype !== RSSoknadstype.REISETILSKUDD && soknadstype !== RSSoknadstype.GRADERT_REISETILSKUDD}
-                render={() =>
-                    <>
-                        <Normaltekst>{tekst('hvorfor-soknad-sykepenger')}</Normaltekst>
-                        <Normaltekst>{tekst('hvorfor-soknad-sykepenger.jobba')}</Normaltekst>
-                    </>
-                }
-            />
-
-        </Ekspanderbartpanel>
+                    <Vis hvis={soknadstype !== RSSoknadstype.REISETILSKUDD && soknadstype !== RSSoknadstype.GRADERT_REISETILSKUDD}
+                        render={() =>
+                            <>
+                                <Normaltekst>{tekst('hvorfor-soknad-sykepenger')}</Normaltekst>
+                                <Normaltekst>{tekst('hvorfor-soknad-sykepenger.jobba')}</Normaltekst>
+                            </>
+                        }
+                    />
+                </Accordion.Content>
+            </Accordion.Item>
+        </Accordion>
     )
 }
 
--- src/components/utvidbar/utvidbar.tsx
+++ src/components/utvidbar/utvidbar.tsx
@@ -1,6 +1,6 @@
 import './utvidbar.less'
 
-import { Collapse,Expand } from '@navikt/ds-icons'
+import { Accordion } from '@navikt/ds-react'
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi'
 import React, { useEffect, useRef, useState } from 'react'
 
@@ -25,6 +25,7 @@ interface UtvidbarProps {
 const Utvidbar = (props: UtvidbarProps) => {
     const [ erApen, setErApen ] = useState<boolean>(props.erApen)
     const [ innholdHeight, setInnholdHeight ] = useState<number>(0)
+
     const utvidbar = useRef<HTMLDivElement>(null)
     const jsToggle = useRef<HTMLButtonElement>(null)
     const btnImage = useRef<HTMLImageElement>(null)
@@ -34,6 +35,7 @@ const Utvidbar = (props: UtvidbarProps) => {
 
     useEffect(() => {
         setErApen(props.erApen)
+        console.log('innhold.current', innhold.current) // eslint-disable-line
         setInnholdHeight(
             props.fixedHeight
                 ? 3000
@@ -52,63 +54,56 @@ const Utvidbar = (props: UtvidbarProps) => {
     }
 
     return (
-        <div ref={utvidbar}
-            className={`utvidbar ${props.className ? props.className : ''} ${props.type ? props.type : ''}`}
-        >
-            <button aria-expanded={erApen}
-                ref={jsToggle}
-                onMouseEnter={props.ikon !== undefined ? () => btnImage.current!.src = props.ikonHover! : undefined}
-                onMouseLeave={props.ikon !== undefined ? () => btnImage.current!.src = props.ikon! : undefined}
-                onClick={onKlikk}
-                type="button"
-                className="utvidbar__toggle"
+        <Accordion>
+            <Accordion.Item ref={utvidbar} renderContentWhenClosed={true}
+                className={`utvidbar ${props.className ? props.className : ''} ${props.type ? props.type : ''}`}
             >
-                <Vis hvis={props.ikon}
-                    render={() =>
-                        <img aria-hidden="true" className="utvidbar__ikon"
-                            ref={btnImage}
-                            alt={props.ikonAltTekst}
-                            src={props.ikon}
-                        />
-                    }
-                />
+                <Accordion.Header
+                    className="utvidbar__toggle"
+                    ref={jsToggle}
+                    onMouseEnter={props.ikon !== undefined ? () => btnImage.current!.src = props.ikonHover! : undefined}
+                    onMouseLeave={props.ikon !== undefined ? () => btnImage.current!.src = props.ikon! : undefined}
+                    onClick={onKlikk}
+                >
+                    <Vis hvis={props.ikon}
+                        render={() =>
+                            <img aria-hidden="true" className="utvidbar__ikon"
+                                ref={btnImage}
+                                alt={props.ikonAltTekst}
+                                src={props.ikon}
+                            />
+                        }
+                    />
 
-                <Vis hvis={props.type === undefined}
-                    render={() => <Undertittel tag="h2" className="utvidbar__tittel">{props.tittel}</Undertittel>}
-                />
+                    <Vis hvis={props.type === undefined}
+                        render={() => <Undertittel tag="h2" className="utvidbar__tittel">{props.tittel}</Undertittel>}
+                    />
 
-                <Vis hvis={props.type === 'intern'}
-                    render={() => <Normaltekst tag="h2" className="utvidbar__tittel">{props.tittel}</Normaltekst>}
-                />
+                    <Vis hvis={props.type === 'intern'}
+                        render={() => <Normaltekst tag="h2" className="utvidbar__tittel">{props.tittel}</Normaltekst>}
+                    />
 
-                <span className="utvidbar__handling">
-                    <Normaltekst tag="em">
+                    <Normaltekst tag="em" className="utvidbar__handling">
                         {erApen ? 'Lukk' : 'Åpne'}
                     </Normaltekst>
-                    <Vis hvis={erApen} render={() =>
-                        <Collapse className="chevron--opp" />
-                    } />
-                    <Vis hvis={!erApen} render={() =>
-                        <Expand className="chevron--ned" />
-                    } />
-                </span>
-            </button>
+                </Accordion.Header>
 
-            <div ref={container} className={'utvidbar__innholdContainer' + (erApen ? ' apen' : '')}
-                style={{ maxHeight: erApen ? (innholdHeight * 2) + 'px' : '0' }}
-            >
-                <div ref={innhold} className="utvidbar__innhold">
-                    {props.children}
-                    <div className="lenkerad ikke-print">
-                        <button type="button" className="lenke" aria-pressed={!erApen}
-                            tabIndex={(erApen ? null : -1) as any} onClick={() => setErApen(!erApen)}
-                        >
-                            <Normaltekst tag="span">Lukk</Normaltekst>
-                        </button>
+                <Accordion.Content ref={container} className={'utvidbar__innholdContainer' + (erApen ? ' apen' : '')}
+                    style={{ maxHeight: erApen ? (innholdHeight * 2) + 'px' : '0' }}
+                >
+                    <div ref={innhold} className="utvidbar__innhold">
+                        {props.children}
+                        <div className="lenkerad ikke-print">
+                            <button type="button" className="lenke" aria-pressed={!erApen}
+                                tabIndex={(erApen ? null : -1) as any} onClick={() => setErApen(!erApen)}
+                            >
+                                <Normaltekst tag="span">Lukk</Normaltekst>
+                            </button>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                </Accordion.Content>
+            </Accordion.Item>
+        </Accordion>
     )
 }
 
