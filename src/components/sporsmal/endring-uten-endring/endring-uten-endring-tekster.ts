2e1e20184e7183331c9810be8b8b31487a632afa
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-04-04 14:18:04 +0200

    Flere endringer

--- src/components/sporsmal/endring-uten-endring/endring-uten-endring-tekster.ts
+++ src/components/sporsmal/endring-uten-endring/endring-uten-endring-tekster.ts
@@ -1,8 +1,5 @@
 export const EndringUtenEndringTekster = {
-
-    'endring-uten-endring.popup.tittel': 'Du har ikke gjort noen endringer i søknaden',
-    'endring-uten-endring.popup.innhold': 'Du har ikke gjort noen endringer. Denne søknaden blir derfor ikke sendt. Vi behandler den opprinnelige sykepengesøknaden din som normalt.',
-    'endring-uten-endring.popup.ok': 'Ok',
-    'endring-uten-endring.popup.tilbake': 'Tilbake'
+    'endring-uten-endring.popup.innhold': 'Du har ikke gjort noen endringer. Vi behandler den opprinnelige sykepengesøknaden din.',
+    'endring-uten-endring.popup.ok': 'OK',
 }
 
