a9d9f55298126f029a6c9ff2844650d490ec3901
golshidbahadorian <golshid.bahadorian@nav.no>
2022-06-14 12:11:12 +0200

    prettier

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,7 +2,6 @@ import pino from 'pino'
 
 const createLogger = (): pino.Logger =>
     pino({
-        transport: { target: 'pino-pretty' },
         timestamp: pino.stdTimeFunctions.isoTime,
         formatters: {
             level: (label) => {
