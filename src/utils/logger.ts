61c2747ed898e7ae77ecde0639b7daa50661c65c
golshidbahadorian <golshid.bahadorian@nav.no>
2022-06-14 11:46:39 +0200

    Tweaker logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,25 +1,14 @@
 import pino from 'pino'
 
-const getFrontendLogger = (): pino.Logger =>
+const createLogger = (): pino.Logger =>
     pino({
-        browser: {
-            transmit: {
-                send: async (level, logEvent) => {
-                    try {
-                        await fetch('/syk/sykepengesoknad/api/logger', {
-                            method: 'POST',
-                            headers: { 'content-type': 'application/json' },
-                            body: JSON.stringify(logEvent),
-                        })
-                    } catch (e) {
-                        // eslint-disable-next-line no-console
-                        console.warn(e)
-                        // eslint-disable-next-line no-console
-                        console.warn('Unable to log to backend', logEvent)
-                    }
-                },
+        transport: { target: 'pino-pretty' },
+        timestamp: pino.stdTimeFunctions.isoTime,
+        formatters: {
+            level: (label) => {
+                return { level: label.toUpperCase() }
             },
-        },
+        }
     })
 
-export const logger = getFrontendLogger()
+export const logger = createLogger()
