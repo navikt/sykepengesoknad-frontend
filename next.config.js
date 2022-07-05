ac418a2d1f13a85c9ba07aa14d64af445bf25732
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2022-07-05 10:12:30 +0200

    Lagt til csp-header i next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,50 @@ const withLess = require('next-with-less')
 const { withSentryConfig } = require('@sentry/nextjs')
 const withPlugins = require('next-compose-plugins')
 
+const csp = {
+    'default-src': ["'none'"],
+    'connect-src': [
+        "'self'",
+        'https://*.nav.no',
+        'https://www.google-analytics.com',
+        'https://nav.psplugin.com',
+        'https://ta-survey-v2.herokuapp.com',
+    ],
+    'img-src': [
+        "'self'",
+        'data:',
+        'https://*.nav.no',
+        'https://www.google-analytics.com',
+    ],
+    'font-src': ["'self'", 'data:', 'https://*.psplugin.com'],
+    'frame-src': ["'self'", 'data:', 'https://vars.hotjar.com'],
+    'worker-src': ['blob:', '*.nais.io'],
+    'style-src': ["'self'", "'unsafe-inline'", 'https://*.nav.no'],
+    'script-src': [
+        "'self'",
+        "'unsafe-inline'",
+        "'unsafe-eval'",
+        'https://*.nav.no',
+        'https://www.googletagmanager.com',
+        'https://www.google-analytics.com',
+        'https://static.hotjar.com',
+        'https://script.hotjar.com',
+        'https://in2.taskanalytics.com',
+        'https://account.psplugin.com',
+    ],
+}
+
+const cspString = Object.entries(csp)
+    .map((entry) => `${entry[0]} ${entry[1].join(' ')}`)
+    .join('; ')
+
+const cspHeader = [
+    {
+        key: 'Content-Security-Policy-Report-Only',
+        value: cspString,
+    },
+]
+
 module.exports = withPlugins(
     [
         [withLess],
@@ -16,6 +60,14 @@ module.exports = withPlugins(
         ],
     ],
     {
+        async headers() {
+            return [
+                {
+                    source: '/:path*',
+                    headers: cspHeader,
+                },
+            ]
+        },
         basePath: '/syk/sykepengesoknad',
         lessLoaderOptions: {},
         assetPrefix: process.env.ASSET_PREFIX || '',
