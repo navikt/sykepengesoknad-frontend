318a108ed445d055d273ed25dbf408379b9864c8
Bjørn Inge Kulsveen <bjorn.inge.kulsveen@nav.no>
2022-08-16 16:42:04 +0200

    validationCallback

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,4 +2,17 @@ const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
 
 module.exports = (on) => {
     on('file:preprocessor', cypressTypeScriptPreprocessor)
+
+    on('task', {
+        log(message) {
+            console.log(message) // eslint-disable-line
+
+            return null
+        },
+        table(message) {
+            console.table(message) // eslint-disable-line
+
+            return null
+        },
+    })
 }
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -35,13 +35,33 @@ afterEach(() => {
                 'svg-img-alt': { enabled: false },
             },
         },
-        undefined,
+        terminalLog,
         true
     )
     cy.get('@winFetch').should((a: any) => {
         lyttTilNettverksKall(a)
     })
 })
+
+function terminalLog(violations: any) {
+    cy.task(
+        'log',
+        `${violations.length} accessibility violation${
+            violations.length === 1 ? '' : 's'
+        } ${violations.length === 1 ? 'was' : 'were'} detected`
+    )
+    // pluck specific keys to keep the table readable
+    const violationData = violations.map(
+        ({ id, impact, description, nodes }: any) => ({
+            id,
+            impact,
+            description,
+            nodes: nodes.length,
+        })
+    )
+
+    cy.task('table', violationData)
+}
 
 const lyttTilNettverksKall = (a: any) => {
     const spy = a ? a['getCalls']() : []
