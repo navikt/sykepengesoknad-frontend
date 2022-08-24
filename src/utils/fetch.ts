98df056394c84a624d25b69f3a8d895a7628839e
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-08-24 09:22:29 +0200

    Logg url når nettverkskall feiler

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -2,17 +2,18 @@ import { v4 as uuidv4 } from 'uuid'
 
 import { logger } from './logger'
 
-const fetchMedRequestId = async (path: string, options: RequestInit) => {
+const fetchMedRequestId = async (url: string, options: RequestInit) => {
     const uuid = uuidv4()
 
     options.headers = options.headers ? { ...options.headers, 'x-request-id': uuid } : { 'x-request-id': uuid }
 
     try {
-        return await fetch(path, options)
+        // fetch() kaster exception for nettverksfeil, men ikke HTTP-statuskoder.
+        return await fetch(url, options)
     } catch (e: any) {
         // Logger x_request_id i stedet for x-request-id for å matche logging fra
         // ingress-controller og sykepengesoknad-backend.
-        logger.error(`Kall med x_request_id: ${uuid} feilet.`, e)
+        logger.error(`Kall til url: ${url} med x_request_id: ${uuid} feilet.`, e)
         throw e
     }
 }
