632ae8db02e9c646a9495fd9b25ff8578a030828
Golshid Bahadorian <36138378+golshidb93@users.noreply.github.com>
2022-08-24 11:06:28 +0200

    Labs fikser p warning confirmationpanel (#615)
    * Fikser p warning forårsaket av confimationPanel
    
    * fjerner feillokal, i og med confirmation panel viser valideringsfeil selv
    
    * Legger til error på confirmation panel
    
    * Bruker controller for å unngå ukontrollert komponent i formen

--- src/components/sporsmal/typer/checkbox-panel.tsx
+++ src/components/sporsmal/typer/checkbox-panel.tsx
@@ -1,10 +1,9 @@
 import { ConfirmationPanel, Label } from '@navikt/ds-react'
 import React from 'react'
-import { useFormContext, useWatch } from 'react-hook-form'
+import { Controller } from 'react-hook-form'
 
 import { TagTyper } from '../../../types/enums'
 import { tekst } from '../../../utils/tekster'
-import FeilLokal from '../../feil/feil-lokal'
 import Vis from '../../vis'
 import { SpmProps } from '../sporsmal-form/sporsmal-form'
 import { hentFeilmelding } from '../sporsmal-utils'
@@ -12,29 +11,29 @@ import { hentFeilmelding } from '../sporsmal-utils'
 const CheckboxInput = ({ sporsmal }: SpmProps) => {
     const spm = sporsmal.tag === TagTyper.BEKREFT_OPPLYSNINGER_UTLAND_INFO ? sporsmal.undersporsmal[0] : sporsmal
 
-    const { register } = useFormContext()
-
     const feilmelding = hentFeilmelding(spm)
-    const watchCheckbox = useWatch({ name: spm.id })
-
     return (
         <>
-            <ConfirmationPanel
-                className={'bekreftCheckboksPanel'}
-                label={spm.sporsmalstekst}
-                id={spm.id}
-                {...register(spm.id, {
-                    required: feilmelding.global,
-                })}
-                checked={watchCheckbox}
-            >
-                <Vis
-                    hvis={sporsmal.tag === TagTyper.ANSVARSERKLARING}
-                    render={() => <Label>{tekst('sporsmal.riktige-opplysninger-tittel')}</Label>}
-                />
-            </ConfirmationPanel>
-
-            <FeilLokal sporsmal={spm} />
+            <Controller
+                defaultValue={false}
+                name={spm.id}
+                rules={{ required: feilmelding.global }}
+                render={({ field, fieldState }) => (
+                    <ConfirmationPanel
+                        {...field}
+                        className={'bekreftCheckboksPanel'}
+                        checked={field.value}
+                        id={field.name}
+                        label={spm.sporsmalstekst}
+                        error={fieldState.error?.message}
+                    >
+                        <Vis
+                            hvis={sporsmal.tag === TagTyper.ANSVARSERKLARING}
+                            render={() => <Label as="span">{tekst('sporsmal.riktige-opplysninger-tittel')}</Label>}
+                        />
+                    </ConfirmationPanel>
+                )}
+            />
         </>
     )
 }
