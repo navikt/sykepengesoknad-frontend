72dfebb0b4b6640d8d46dfb3f6b79df59063e307
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-08-26 08:29:30 +0200

    Logg x-request-id når Frontendlogger brukes

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,21 +1,21 @@
 import pino from 'pino'
 
+import fetchMedRequestId from './fetch'
+
 const getFrontendLogger = (): pino.Logger =>
     pino({
         browser: {
             transmit: {
                 send: async (level, logEvent) => {
                     try {
-                        await fetch('/syk/sykepengesoknad/api/logger', {
+                        await fetchMedRequestId('/syk/sykepengesoknad/api/logger', {
                             method: 'POST',
                             headers: { 'content-type': 'application/json' },
                             body: JSON.stringify(logEvent),
                         })
                     } catch (e) {
                         // eslint-disable-next-line no-console
-                        console.warn(e)
-                        // eslint-disable-next-line no-console
-                        console.warn('Unable to log to backend', logEvent)
+                        console.warn(`Unable to log the following event to backend: ${logEvent}`, e)
                     }
                 },
             },
