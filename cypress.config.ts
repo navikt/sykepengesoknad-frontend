c033129c11cf5c9952fd791f82626154db2d553e
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2022-08-31 11:48:57 +0200

    cypress-config endret pga kræsj: numTestsKeptInMemory: 10, default 50

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     video: false,
     viewportWidth: 1440,
     viewportHeight: 900,
+    numTestsKeptInMemory: 10,
     e2e: {
         // We've imported your old cypress plugins here.
         // You may want to clean this up later by importing these.
