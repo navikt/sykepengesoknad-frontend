2947de6cadd7010d28299ac7307978ededb5d82a
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-09-04 19:01:30 +0200

    Slett ubrukte metoder i utils.ts

--- src/data/rest/utils.ts
+++ src/data/rest/utils.ts
@@ -1,69 +1,7 @@
 import { logger } from '../../utils/logger'
 
-export enum FetchStatus {
-    NOT_STARTED = 'NOT_STARTED',
-    PENDING = 'PENDING',
-    FINISHED = 'FINISHED',
-}
-
-export interface FetchState<D = {}> {
-    status: FetchStatus
-    error: any
-    data: D | null
-    httpCode: number
-}
-
-export interface FetchStateWithData<D = {}> extends FetchState<D> {
-    data: D
-}
-
-export const isAnyNotStartedOrPending = (fetch: FetchState | FetchState[]): boolean => {
-    if (Array.isArray(fetch)) {
-        return fetch.some((f) => isNotStartedOrPending(f))
-    }
-    return isNotStartedOrPending(fetch)
-}
-
-export const hasAnyFailed = (fetch: FetchState | FetchState[]): boolean => {
-    if (Array.isArray(fetch)) {
-        return fetch.some((f) => hasFailed(f))
-    }
-    return hasFailed(fetch)
-}
-
-export const hasAny401 = (fetch: FetchState | FetchState[]): boolean => {
-    if (Array.isArray(fetch)) {
-        return fetch.some((f) => has401(f))
-    }
-    return has401(fetch)
-}
-
-export const isNotStarted = (fetch: FetchState): boolean => {
-    return fetch.status === FetchStatus.NOT_STARTED
-}
-
-export const isNotStartedOrPending = (fetch: FetchState): boolean => {
-    return fetch.status === FetchStatus.NOT_STARTED || fetch.status === FetchStatus.PENDING
-}
-
-export const hasFinished = (fetch: FetchState): boolean => {
-    return fetch.status === FetchStatus.FINISHED
-}
-
-export const hasFailed = (fetch: FetchState): boolean => {
-    return fetch.error != null || fetch.httpCode >= 400
-}
-
-export const has401 = (fetch: FetchState): boolean => {
-    return fetch.httpCode === 401
-}
-
-export const hasData = <D = {}>(fetch: FetchState<D>): fetch is FetchStateWithData<D> => {
-    return fetch.data != null
-}
-
-export const redirectTilLoginHvis401 = (res: Response) => {
-    if (res.status === 401) {
+export const redirectTilLoginHvis401 = (response: Response) => {
+    if (response.status == 401) {
         logger.info('Redirecter til login grunnet 401.')
         window.location.reload()
         return true
