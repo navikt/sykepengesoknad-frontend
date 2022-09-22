f03556fd913321fde5c871eece79a1cdc571975b
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-09-22 09:25:57 +0200

    Amplitudelogging om sykepenger (#641)

--- src/components/om-sykepenger/om-sykepenger.tsx
+++ src/components/om-sykepenger/om-sykepenger.tsx
@@ -3,16 +3,26 @@ import parser from 'html-react-parser'
 import React, { useState } from 'react'
 
 import { tekst } from '../../utils/tekster'
+import { useAmplitudeInstance } from '../amplitude/amplitude'
 
 const OmSykepenger = () => {
     const [open1, setOpen1] = useState<boolean>(false)
     const [open2, setOpen2] = useState<boolean>(false)
     const [open3, setOpen3] = useState<boolean>(false)
+    const { logEvent } = useAmplitudeInstance()
 
     return (
         <Accordion>
             <Accordion.Item open={open1} className="om_sykepenger">
-                <Accordion.Header onClick={() => setOpen1(!open1)}>
+                <Accordion.Header
+                    onClick={() => {
+                        logEvent(open1 ? 'accordion lukket' : 'accordion åpnet', {
+                            tekst: tekst('om.sykepenger.tittel'),
+                            komponent: 'listevisning søkad - om sykepenger',
+                        })
+                        setOpen1(!open1)
+                    }}
+                >
                     <Heading size="small" level="2">
                         {tekst('om.sykepenger.tittel')}
                     </Heading>
@@ -24,7 +34,15 @@ const OmSykepenger = () => {
 
                     <Accordion>
                         <Accordion.Item open={open2}>
-                            <Accordion.Header onClick={() => setOpen2(!open2)}>
+                            <Accordion.Header
+                                onClick={() => {
+                                    logEvent(open2 ? 'accordion lukket' : 'accordion åpnet', {
+                                        tekst: tekst('om.sykepenger.arbeidstakere.tittel'),
+                                        komponent: 'listevisning søkad - om sykepenger',
+                                    })
+                                    setOpen2(!open2)
+                                }}
+                            >
                                 {tekst('om.sykepenger.arbeidstakere.tittel')}
                             </Accordion.Header>
                             <Accordion.Content>
@@ -35,7 +53,15 @@ const OmSykepenger = () => {
                         </Accordion.Item>
 
                         <Accordion.Item open={open3}>
-                            <Accordion.Header onClick={() => setOpen3(!open3)}>
+                            <Accordion.Header
+                                onClick={() => {
+                                    logEvent(open3 ? 'accordion lukket' : 'accordion åpnet', {
+                                        tekst: tekst('om.sykepenger.selvstendige.tittel'),
+                                        komponent: 'listevisning søkad - om sykepenger',
+                                    })
+                                    setOpen3(!open3)
+                                }}
+                            >
                                 {tekst('om.sykepenger.selvstendige.tittel')}
                             </Accordion.Header>
                             <Accordion.Content>
