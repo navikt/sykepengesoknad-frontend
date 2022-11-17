4a72856495fe07465fe90f54b846325543a50e2b
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-11-17 14:47:58 +0100

    Revert "Flytt logging på innsiden av if"
    This reverts commit b18819a114ddcfb122f2eb16378896ab184f5ad7.

--- src/utils/tekster.ts
+++ src/utils/tekster.ts
@@ -64,8 +64,8 @@ const tekster = {
 
 export const tekst = (tekst: keyof typeof tekster): string => {
     const verdi = tekster[tekst]
+    logger.error(`Mangler teksten [ ${tekst} ].`)
     if (!verdi === undefined && !tekst.includes('soknad.feilmelding')) {
-        logger.error(`Mangler teksten [ ${tekst} ].`)
         return undefined as any
     }
     return verdi
