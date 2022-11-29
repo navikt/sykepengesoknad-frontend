232ca7429da286ecb8fd0c1cb65338ac549fd241
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-11-29 13:54:47 +0100

    Tester innholdet i cypress

---
+++ cypress/e2e/run-1/egenmeldingsdager-arbeidsgiver.cy.ts
@@ -0,0 +1,17 @@
+import { kortFomTomArbeidstakerSoknad } from '../../../src/data/mock/data/kort-soknad'
+
+describe('Tester søknad som er opprettet fra egenmeldingsdager i inntektsmeldingen', () => {
+    const soknad = kortFomTomArbeidstakerSoknad
+
+    it('Viser guidepanel på førsteside', function () {
+        cy.visit(
+            `http://localhost:8080/syk/sykepengesoknad/soknader/${soknad.id}?testperson=egenmeldingsdager%20arbeidsgiver`,
+        )
+        cy.contains('Søknad om sykepenger').and('be.visible')
+
+        cy.get('.navds-guide-panel').within(() => {
+            cy.contains('Du må søke om sykepenger')
+            cy.contains('Siden sykefraværet ditt er på mer enn 16 dager må du søke om sykepenger.')
+        })
+    })
+})
