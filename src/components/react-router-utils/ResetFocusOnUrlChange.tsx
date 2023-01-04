6ae81ac6d007cbed2408b7aafaa3275c531e46a8
Øivind <oivind.thorsen.kulsrud@nav.no>
2023-01-04 12:30:03 +0100

    Resetter tab focus ved ny reactrouter url, tester keyboard navigasjon

---
+++ src/components/react-router-utils/ResetFocusOnUrlChange.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
+
+interface ResetFocusOnUrlChangeProps {
+    children: React.ReactNode
+}
+
+const ResetFocusOnUrlChange = (props: ResetFocusOnUrlChangeProps) => {
+    const ref = useRef<HTMLDivElement>(null)
+    const history = useHistory()
+    const location = useLocation()
+
+    // remove once react-router accessibility issue is fixed
+    // https://github.com/remix-run/react-router/issues/5210
+    useEffect(() => {
+        ref.current?.focus()
+    }, [location.pathname])
+
+    return (
+        <div ref={ref} tabIndex={-1} style={{ outline: 'none' }}>
+            {props.children}
+        </div>
+    )
+}
+
+export default ResetFocusOnUrlChange
