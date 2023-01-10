e70b4b224778629324f64bae4b56ef871ab27c36
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-01-10 08:55:49 +0100

    Ubrukt kode

--- src/components/kvittering/harSvartJa.ts
+++ src/components/kvittering/harSvartJa.ts
@@ -1,37 +1,6 @@
 import { RSSoknadstype } from '../../types/rs-types/rs-soknadstype'
 import { Soknad } from '../../types/types'
-import { TagTyper } from '../../types/enums'
 
 export function erArbeidstakersoknad(soknad: Soknad) {
     return soknad.soknadstype == RSSoknadstype.ARBEIDSTAKERE
-}
-
-export function harSvartJaJobbetDuUnderveis(soknad: Soknad) {
-    if (soknad.soknadstype != RSSoknadstype.ARBEIDSTAKERE) {
-        return false
-    }
-    const spm = soknad.sporsmal.find(
-        (s) => s.tag == TagTyper.JOBBET_DU_100_PROSENT || s.tag == TagTyper.JOBBET_DU_GRADERT,
-    )
-    if (!spm) {
-        return false
-    }
-    if (spm.svarliste.svar.length == 1) {
-        return spm.svarliste.svar[0].verdi == 'JA'
-    }
-    return false
-}
-
-export function harSvartJaFravaerForSykmeldingen(soknad: Soknad) {
-    if (soknad.soknadstype != RSSoknadstype.ARBEIDSTAKERE) {
-        return false
-    }
-    const spm = soknad.sporsmal.find((s) => s.tag == TagTyper.FRAVAR_FOR_SYKMELDINGEN)
-    if (!spm) {
-        return false
-    }
-    if (spm.svarliste.svar.length == 1) {
-        return spm.svarliste.svar[0].verdi == 'JA'
-    }
-    return false
 }
