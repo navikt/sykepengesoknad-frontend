1514f2c683a830bef4527b2c8421c43abd6bd036
Øivind <oivind.thorsen.kulsrud@nav.no>
2023-01-16 13:03:02 +0100

    ny datepicker

--- src/components/sporsmal/typer/dato-komp.tsx
+++ src/components/sporsmal/typer/dato-komp.tsx
@@ -1,6 +1,7 @@
-import { Datepicker } from '@navikt/ds-datepicker'
-import React from 'react'
+//import { Datepicker } from '@navikt/ds-datepicker'
+import React, { useState } from 'react'
 import { Controller, useFormContext } from 'react-hook-form'
+import { Fieldset, UNSAFE_DatePicker, UNSAFE_useDatepicker } from '@navikt/ds-react'
 
 import { skalBrukeFullskjermKalender } from '../../../utils/browser-utils'
 import { fraBackendTilDate } from '../../../utils/dato-utils'
@@ -10,53 +11,80 @@ import { SpmProps } from '../sporsmal-form/sporsmal-form'
 import UndersporsmalListe from '../undersporsmal/undersporsmal-liste'
 
 const DatoInput = ({ sporsmal }: SpmProps) => {
-    const { getValues } = useFormContext()
+    const { getValues, setValue } = useFormContext() // how was this used?
+    const [dato, setDato] = useState('')
 
+    console.log(getValues())
+    console.log(sporsmal)
+    // log field
+   
+
+    
+    const { datepickerProps, inputProps, selectedDay } = UNSAFE_useDatepicker({
+        //fromDate: new Date('Aug 23 2019'),
+        
+        defaultMonth: new Date(sporsmal.max || "1900"),
+        // onDateChange: (x : any) => {
+        //     console.log(x)
+        //     setDato(x.toLocaleDateString('nb-NO'))
+        //     if (validerDato(x, sporsmal)) {
+        //         // setValue(sporsmal.id, x.toLocaleDateString('nb-NO'))
+        //     }
+            
+        // },
+    })
     return (
-        <div className="dato-komp">
-            <Controller
-                name={sporsmal.id}
-                rules={{
-                    validate: () => {
-                        const div: HTMLDivElement | null = document.querySelector('.ds-datepicker')
-                        const detteFeilet = validerDato(sporsmal, getValues())
-                        if (detteFeilet !== true) {
-                            div?.classList.add('skjemaelement__input--harFeil')
-                            return detteFeilet
-                        }
-                        div?.classList.remove('skjemaelement__input--harFeil')
-                        return true
-                    },
-                }}
-                render={({ field }) => (
-                    <Datepicker
-                        locale="nb"
-                        id={sporsmal.id}
-                        label={sporsmal.sporsmalstekst}
-                        onChange={field.onChange}
-                        value={field.value}
-                        inputName={field.name}
-                        calendarSettings={{
-                            showWeekNumbers: true,
-                            position: skalBrukeFullskjermKalender(),
-                        }}
-                        showYearSelector={false}
-                        limitations={{
-                            weekendsNotSelectable: false,
-                            minDate: sporsmal.min || undefined,
-                            maxDate: sporsmal.max || undefined,
-                        }}
-                        dayPickerProps={{
-                            initialMonth: fraBackendTilDate(sporsmal.max!),
-                        }}
-                    />
-                )}
-            />
-
-            <FeilLokal sporsmal={sporsmal} />
+        <div className="dato-komp" data-cy="dato-komp">
+            <FeilLokal sporsmal={sporsmal}/>
 
             <div aria-live="assertive" className="undersporsmal">
                 <UndersporsmalListe oversporsmal={sporsmal} />
+            </div>
+
+            {/* http://localhost:8080/syk/sykepengesoknad/soknader/5b769c04-e171-47c9-b79b-23ab8fce331e/3 */}
+
+            <div>
+                <Controller
+
+                    name={sporsmal.id}
+                    rules={{
+                        validate: () => {
+                            // const div: HTMLDivElement | null = document.querySelector('.ds-datepicker')
+                        const detteFeilet = validerDato(sporsmal, getValues())
+                        if (detteFeilet !== true) {
+                            //div?.classList.add('skjemaelement__input--harFeil')
+                            return detteFeilet
+                        }
+                        // div?.classList.remove('skjemaelement__input--harFeil')
+                        return true
+
+                        },
+                    }}
+                    render={({ field }) => (
+                        <div>
+                            <div className="min-h-96">
+                                <UNSAFE_DatePicker
+                                    {...datepickerProps}
+                                    {...(sporsmal.min && { fromDate: new Date(sporsmal.min) })}
+                                    {...(sporsmal.max && { toDate: new Date(sporsmal.max) })}                   
+                                    dropdownCaption={true}
+                                    showWeekNumber={true}
+                                    disableWeekends={true}
+                                    locale="nb"
+                                    id={sporsmal.id}
+                                    onSelect={field.onChange}
+                                    selected={field.value}
+        
+                                >
+                                    <UNSAFE_DatePicker.Input {...inputProps}                         
+                                    label={sporsmal.sporsmalstekst}
+
+                                    />
+                                </UNSAFE_DatePicker>
+                            </div>
+                        </div>
+                    )}
+                />
             </div>
         </div>
     )
