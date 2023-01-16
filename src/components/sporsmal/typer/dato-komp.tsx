0596e420c9ace635b832030e6edad19596e39729
Øivind <oivind.thorsen.kulsrud@nav.no>
2023-01-16 13:52:44 +0100

    ryddet litt

--- src/components/sporsmal/typer/dato-komp.tsx
+++ src/components/sporsmal/typer/dato-komp.tsx
@@ -11,27 +11,10 @@ import { SpmProps } from '../sporsmal-form/sporsmal-form'
 import UndersporsmalListe from '../undersporsmal/undersporsmal-liste'
 
 const DatoInput = ({ sporsmal }: SpmProps) => {
-    const { getValues, setValue } = useFormContext() // how was this used?
-    const [dato, setDato] = useState('')
-
-    console.log(getValues())
-    console.log(sporsmal)
-    // log field
-   
-
+    const { getValues } = useFormContext()
     
     const { datepickerProps, inputProps, selectedDay } = UNSAFE_useDatepicker({
-        //fromDate: new Date('Aug 23 2019'),
-        
-        defaultMonth: new Date(sporsmal.max || "1900"),
-        // onDateChange: (x : any) => {
-        //     console.log(x)
-        //     setDato(x.toLocaleDateString('nb-NO'))
-        //     if (validerDato(x, sporsmal)) {
-        //         // setValue(sporsmal.id, x.toLocaleDateString('nb-NO'))
-        //     }
-            
-        // },
+        defaultMonth: new Date(sporsmal.max || new Date()),
     })
     return (
         <div className="dato-komp" data-cy="dato-komp">
@@ -40,7 +23,8 @@ const DatoInput = ({ sporsmal }: SpmProps) => {
             <div aria-live="assertive" className="undersporsmal">
                 <UndersporsmalListe oversporsmal={sporsmal} />
             </div>
-
+            
+            {/* fjernes før pr: */}
             {/* http://localhost:8080/syk/sykepengesoknad/soknader/5b769c04-e171-47c9-b79b-23ab8fce331e/3 */}
 
             <div>
@@ -49,7 +33,6 @@ const DatoInput = ({ sporsmal }: SpmProps) => {
                     name={sporsmal.id}
                     rules={{
                         validate: () => {
-                            // const div: HTMLDivElement | null = document.querySelector('.ds-datepicker')
                         const detteFeilet = validerDato(sporsmal, getValues())
                         if (detteFeilet !== true) {
                             //div?.classList.add('skjemaelement__input--harFeil')
@@ -78,7 +61,6 @@ const DatoInput = ({ sporsmal }: SpmProps) => {
                                 >
                                     <UNSAFE_DatePicker.Input {...inputProps}                         
                                     label={sporsmal.sporsmalstekst}
-
                                     />
                                 </UNSAFE_DatePicker>
                             </div>
