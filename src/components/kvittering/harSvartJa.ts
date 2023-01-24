865bf5aa5da403f9c1af9e9165cdcd3f08b8bcc4
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-01-24 13:50:48 +0100

    Rekrutter alle til denne torsdagen

--- src/components/kvittering/harSvartJa.ts
+++ src/components/kvittering/harSvartJa.ts
@@ -3,7 +3,6 @@ import dayjs from 'dayjs'
 import { RSSoknadmetadata } from '../../types/rs-types/rs-soknadmetadata'
 import { Soknad } from '../../types/types'
 import { RSSoknadstype } from '../../types/rs-types/rs-soknadstype'
-import { TagTyper } from '../../types/enums'
 
 export function harKorrigertArbeidstakersoknadIDetSiste(soknader: RSSoknadmetadata[]) {
     const toMndSiden = dayjs().subtract(64, 'days').toDate()
@@ -17,16 +16,6 @@ export function harKorrigertArbeidstakersoknadIDetSiste(soknader: RSSoknadmetada
     )
 }
 
-export function harSvartTilbakeIArbeid(soknad: Soknad) {
-    if (soknad.soknadstype != RSSoknadstype.ARBEIDSTAKERE) {
-        return false
-    }
-    const spm = soknad.sporsmal.find((s) => s.tag == TagTyper.TILBAKE_I_ARBEID)
-    if (!spm) {
-        return false
-    }
-    if (spm.svarliste.svar.length == 1) {
-        return spm.svarliste.svar[0].verdi == 'JA'
-    }
-    return false
+export function erArbeidstakerSoknad(soknad: Soknad) {
+    return soknad.soknadstype === RSSoknadstype.ARBEIDSTAKERE
 }
