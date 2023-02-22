055819b8a82f214b95f3e33bf4f379881a40c97b
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-02-22 08:14:35 +0100

    Fjerner italic i hjelpetekst

--- src/components/hjelpetekster/ekspanderbar-hjelp/ekspanderbar-hjelp-tekst.ts
+++ src/components/hjelpetekster/ekspanderbar-hjelp/ekspanderbar-hjelp-tekst.ts
@@ -46,9 +46,7 @@ export const EkspanderbarHjelpTekster = {
     'ekspanderbarhjelp.tilbake_i_arbeid.body1':
         'Du kan begynne å jobbe fullt igjen før sykmeldingen er slutt. Vi trenger opplysninger om du var tilbake i fullt arbeid for å beregne hvor mye du skal få utbetalt i sykepenger.',
     'ekspanderbarhjelp.tilbake_i_arbeid.body2':
-        ', hvis du var fullt tilbake på jobb i løpet av perioden. Dette betyr at du ikke lenger er sykmeldt fra datoen du oppgir under.',
-    'ekspanderbarhjelp.tilbake_i_arbeid.body2.span': 'Svar ja',
+        'Svar ja, hvis du var fullt tilbake på jobb i løpet av perioden. Dette betyr at du ikke lenger er sykmeldt fra datoen du oppgir under.',
     'ekspanderbarhjelp.tilbake_i_arbeid.body3':
-        ', dersom du kun var delvis tilbake på jobb i denne perioden. Du vil senere i søknaden bli spurt om hvor mange timer du har jobbet.',
-    'ekspanderbarhjelp.tilbake_i_arbeid.body3.span': 'Svar nei',
+        'Svar nei, dersom du kun var delvis tilbake på jobb i denne perioden. Du vil senere i søknaden bli spurt om hvor mange timer du har jobbet.',
 }
