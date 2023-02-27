7822e165225e0b3ebe092024d6907f20408fa767
Øivind <oivind.thorsen.kulsrud@nav.no>
2023-02-27 18:43:30 +0100

    wip

--- cypress/support/utilities.ts
+++ cypress/support/utilities.ts
@@ -1,7 +1,7 @@
 export const setPeriodeFraTil = (fom: number, tom: number, periodeIndex = 0) => {
     // i periode komponentet med en gitt ID er fra og med feltet det første og til og med det andre
     // henter periode komponentet med riktig id (inkludert 0, 1 osv for hvor i rekken det er)
-    // velger sariktig kalender popup button
+    // velger sa riktig kalender dato popup button for dag i mnd
 
     cy.get(`[data-cy="periode"]`).eq(periodeIndex).find('.navds-date__field-button').eq(0).click()
     cy.get('.rdp-day').contains(fom.toString()).click()
