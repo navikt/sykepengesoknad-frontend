7850db6d03ae12f73070bb84bf8de5b3156e7467
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-02-28 09:57:00 +0100

    fjern state

--- src/components/sporsmal/typer/dager-komp.tsx
+++ src/components/sporsmal/typer/dager-komp.tsx
@@ -1,14 +1,13 @@
 import { Label, UNSAFE_DatePicker as DatePicker } from '@navikt/ds-react'
 import dayjs from 'dayjs'
-import React, { useState } from 'react'
+import React from 'react'
 import { Controller } from 'react-hook-form'
 
 import FeilLokal from '../../feil/feil-lokal'
 import { SpmProps } from '../sporsmal-form/sporsmal-form'
 
 const DagerKomp = ({ sporsmal }: SpmProps) => {
-    const [verdier, setVerdier] = useState(sporsmal.svarliste.svar.map((i) => new Date(i.verdi)))
-
+    const labelen = 'dager-kalender-label'
     return (
         <>
             <Label as="h3" className="skjema__sporsmal">
@@ -19,8 +18,8 @@ const DagerKomp = ({ sporsmal }: SpmProps) => {
                 <Controller
                     name={sporsmal.id}
                     rules={{
-                        validate: () => {
-                            if (verdier.length === 0) {
+                        validate: (value) => {
+                            if (value.length === 0) {
                                 return 'Du må oppgi hvilke dager du brukte bil'
                             }
                             return true
@@ -29,17 +28,17 @@ const DagerKomp = ({ sporsmal }: SpmProps) => {
                     render={({ field }) => (
                         <DatePicker.Standalone
                             mode="multiple"
+                            aria-describedby={labelen}
+                            showWeekNumber={true}
                             fromDate={dayjs(sporsmal.min).toDate()}
                             toDate={dayjs(sporsmal.max).toDate()}
-                            selected={verdier}
+                            selected={field.value}
                             onSelect={(a) => {
-                                setVerdier(a || [])
                                 field.onChange(a)
                             }}
                         />
                     )}
                 />
-
                 <FeilLokal sporsmal={sporsmal} />
             </div>
         </>
