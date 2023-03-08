a84450b8bbc2a0767e4265b1ecc5278839700ac8
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-03-08 15:48:51 +0100

    Labs testdata vaerklaroverat (#1132)
    * felles vær klar over at
    
    * gjenbruk
    
    * Reisetilskudd og gradert

--- cypress/e2e/run-0/arbeidsledig_med_keyboard.cy.ts
+++ cypress/e2e/run-0/arbeidsledig_med_keyboard.cy.ts
@@ -70,6 +70,7 @@ describe('Tester arbeidsledigsøknad', () => {
         cy.realPress('Tab')
         cy.realPress('Tab')
         cy.realPress('Tab')
+        cy.realPress('Tab')
         cy.focused()
             .parent()
             .contains(
---
+++ src/data/mock/data/sporsmal/bekreft-opplysninger.ts
@@ -0,0 +1,19 @@
+import { RSSporsmal } from '../../../../types/rs-types/rs-sporsmal'
+import { RSSvar } from '../../../../types/rs-types/rs-svar'
+
+export const bekreftOpplysninger = (svar: RSSvar[] = []): RSSporsmal => {
+    return {
+        id: '1623833',
+        tag: 'BEKREFT_OPPLYSNINGER',
+        sporsmalstekst:
+            'Jeg har lest all informasjonen jeg har fått i søknaden og bekrefter at opplysningene jeg har gitt er korrekte.',
+        undertekst: null,
+        svartype: 'CHECKBOX_PANEL',
+        min: null,
+        max: null,
+        pavirkerAndreSporsmal: false,
+        kriterieForVisningAvUndersporsmal: null,
+        svar,
+        undersporsmal: [],
+    }
+}
