e6b45a34b9d8d3d14be77e08b0b3955ba3616af8
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-04-26 13:09:51 +0200

    Litt tailwind på guidepanel (#1335)

--- src/components/soknad-med-to-deler/soknad-med-to-deler.tsx
+++ src/components/soknad-med-to-deler/soknad-med-to-deler.tsx
@@ -2,19 +2,16 @@ import { BodyLong, GuidePanel, Heading } from '@navikt/ds-react'
 import React from 'react'
 
 import { tekst } from '../../utils/tekster'
-import VeilederSVG from '../soknad-intro/veileder'
 
 const SoknadMedToDeler = () => {
     return (
-        <div className="to-deler">
-            <GuidePanel poster illustration={VeilederSVG}>
-                <Heading size="small" level="2">
-                    {tekst('to-deler.overskrift')}
-                </Heading>
-                <BodyLong>{tekst('to-deler.avsnitt.1')}</BodyLong>
-                <BodyLong>{tekst('to-deler.avsnitt.2')}</BodyLong>
-            </GuidePanel>
-        </div>
+        <GuidePanel poster>
+            <Heading size="small" level="2" spacing className={'text-center'}>
+                {tekst('to-deler.overskrift')}
+            </Heading>
+            <BodyLong spacing>{tekst('to-deler.avsnitt.1')}</BodyLong>
+            <BodyLong spacing>{tekst('to-deler.avsnitt.2')}</BodyLong>
+        </GuidePanel>
     )
 }
 
