208fa1502ead1dcab18afc54747c2f38aeb608fc
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-04-27 12:19:59 +0200

    Endre Permisjon hjelpetekst

--- src/components/hjelpetekster/ekspanderbar-hjelp/permisjon-hjelp-body.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/permisjon-hjelp-body.tsx
@@ -8,15 +8,13 @@ export const PermisjonHjelpBody = () => {
                 utbetales ikke sykepenger for den delen du tar permisjon. Det er ikke alltid vi vet om du er i permisjon
                 og vi må derfor spørre deg.
             </BodyLong>
-
             <BodyLong className={'mt-4'}>
-                Hvis du blir syk før en planlagt permisjon, har foreldrepermisjon, ferie, er permittert eller har
-                seniordager, svarer du nei.
-            </BodyLong>
-
-            <BodyLong className={'mt-4'}>
-                Blir du sykmeldt mens du er i permisjon, svarer du ja selv om permisjonen er utdanningspermisjon eller
+                Svar ja, hvis du blir sykmeldt mens du er i permisjon selv om permisjonen er utdanningspermisjon eller
                 foreldrepermisjon.
+            </BodyLong>
+            <BodyLong className={'mt-4'}>
+                Svar nei, hvis du blir syk før en planlagt permisjon, har foreldrepermisjon, ferie, er permittert eller
+                har seniordager.
             </BodyLong>
         </>
     )
