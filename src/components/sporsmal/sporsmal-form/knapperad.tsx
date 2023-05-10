fe6d396d26f9ec4fb0793340d2e7dfceeef38c95
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-05-10 10:22:58 +0200

    padding

--- src/components/sporsmal/sporsmal-form/knapperad.tsx
+++ src/components/sporsmal/sporsmal-form/knapperad.tsx
@@ -38,7 +38,7 @@ const Knapperad = ({ soknad, poster }: KnapperadProps) => {
 
     return (
         <div className="my-8" data-cy="knapperad">
-            <Button variant="primary" type="submit" loading={poster} className={'mb-4'}>
+            <Button variant="primary" type="submit" loading={poster} className={'mb-12 mt-6'}>
                 {tekst(nokkel())}
             </Button>
 
