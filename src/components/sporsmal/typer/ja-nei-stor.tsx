00101099165b52f7a4c1340a2734ea1e85a21ec7
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-05-16 11:24:38 +0200

    shadow focus istedenfor outline

--- src/components/sporsmal/typer/ja-nei-stor.tsx
+++ src/components/sporsmal/typer/ja-nei-stor.tsx
@@ -51,7 +51,7 @@ const JaNeiStor = ({ sporsmal }: SpmProps) => {
 
     function radioClassName(value: 'JA' | 'NEI') {
         return cn(
-            'focus-within:outline mb-2 block w-full rounded border-2 border-b-border-action px-4 py-1 text-text-action hover:bg-surface-action-subtle-hover md:mb-0 md:w-[48.7%]',
+            'focus-within:shadow-focus mb-2 block w-full rounded border-2 border-b-border-action px-4 py-1 text-text-action hover:bg-surface-action-subtle-hover md:mb-0 md:w-[48.7%]',
             {
                 'bg-surface-action-subtle': watchJaNei === value,
             },
