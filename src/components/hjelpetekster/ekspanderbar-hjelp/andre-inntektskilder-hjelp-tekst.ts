18158ecf61c4fed555aa34e96302bc318728417c
oivindkulsrud <oivind.thorsen.kulsrud@nav.no>
2023-05-23 14:31:08 +0200

    Legger til punkt om selvstendig næringsdrivende i hjelpetekst til andre inntektskilder (#1470)

--- src/components/hjelpetekster/ekspanderbar-hjelp/andre-inntektskilder-hjelp-tekst.ts
+++ src/components/hjelpetekster/ekspanderbar-hjelp/andre-inntektskilder-hjelp-tekst.ts
@@ -2,7 +2,7 @@ export const AndreInntektskilderHjelpTekster = {
     'ekspanderbarhjelp.andre_inntektskilder_v2.tittel': 'Hva betyr dette?',
     'hjelpetekst.del1':
         'NAV trenger å vite om dine inntektskilder i sykmeldingsperioden for å kunne beregne riktig utbetaling av sykepenger til deg. Arbeidsforhold som er registrert på deg henter NAV fra offentlige registre.',
-    'hjelpetekst.del2': 'Svar ja, hvis du har begynt i ny jobb, ',
+    'hjelpetekst.del2': 'Svar ja, hvis du har begynt i ny jobb, har inntekt som selvstendig næringsdrivende, ',
     'modal1.del1':
         'Du er frilanser når du mottar inntekt uten å være ansatt hos den du utfører oppdraget for, eller har et enkeltpersonsforetak.',
     'modal1.del2':
