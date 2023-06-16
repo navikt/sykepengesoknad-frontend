fd194b78a9024e40d8b5e1d2886082577b8a6f8c
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-06-16 10:29:45 +0200

    wip

--- cypress/e2e/run-2/korrigering-ferie.cy.ts
+++ cypress/e2e/run-2/korrigering-ferie.cy.ts
@@ -1,19 +1,17 @@
-import { svarJaHovedsporsmal, svarNeiHovedsporsmal, svarCheckboxPanel, setPeriodeFraTil } from '../../support/utilities'
+import { svarJaHovedsporsmal, svarNeiHovedsporsmal, svarCheckboxPanel, setPeriodeFraTil, klikkGaVidere } from '../../support/utilities'
 
 describe('Tester korrigering av ferie', () => {
-    function gaVidere() {
-        cy.contains('Gå videre').click()
-    }
+
 
     it('Sender inn søknad med ja på ferie spørsmålet', () => {
         cy.clearCookies()
         cy.visit('/syk/sykepengesoknad/soknader/5b769c04-e171-47c9-b79b-23ab8fce331e')
         svarCheckboxPanel()
-        gaVidere()
+        klikkGaVidere()
         svarNeiHovedsporsmal()
-        gaVidere()
+        klikkGaVidere()
         svarNeiHovedsporsmal()
-        gaVidere()
+        klikkGaVidere()
         svarNeiHovedsporsmal()
         cy.get('[data-cy="sporsmal-tittel"]').should('contain', 'Ferie')
 
@@ -23,17 +21,17 @@ describe('Tester korrigering av ferie', () => {
         cy.contains('Når tok du ut feriedager?')
 
         cy.get('[data-cy="feriekorrigeringvarsel"]').should('not.exist')
-        gaVidere()
+        klikkGaVidere()
         svarNeiHovedsporsmal()
-        gaVidere()
+        klikkGaVidere()
         svarNeiHovedsporsmal()
-        gaVidere()
+        klikkGaVidere()
         svarNeiHovedsporsmal()
-        gaVidere()
+        klikkGaVidere()
         svarNeiHovedsporsmal()
-        gaVidere()
+        klikkGaVidere()
         svarNeiHovedsporsmal()
-        gaVidere()
+        klikkGaVidere()
         svarCheckboxPanel()
         cy.contains('Send søknaden').click()
     })
@@ -45,13 +43,13 @@ describe('Tester korrigering av ferie', () => {
         cy.contains(
             'Jeg vet at jeg kan miste retten til sykepenger hvis opplysningene jeg gir ikke er riktige eller fullstendige. Jeg vet også at NAV kan holde igjen eller kreve tilbake penger, og at å gi feil opplysninger kan være straffbart.',
         ).click()
-        gaVidere()
+        klikkGaVidere()
 
         svarNeiHovedsporsmal()
-        gaVidere()
+        klikkGaVidere()
 
         svarNeiHovedsporsmal()
-        gaVidere()
+        klikkGaVidere()
 
         svarJaHovedsporsmal()
         cy.contains('Når tok du ut feriedager?')
