598b4fe6702d98c937e018727b700a6d8598cd82
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-06-16 08:41:22 +0200

    sjekkurl

--- cypress/e2e/run-1/desimal-tall-komp.cy.ts
+++ cypress/e2e/run-1/desimal-tall-komp.cy.ts
@@ -12,7 +12,9 @@ describe('Tester at riktig antall desimaler sendes til backend', () => {
         cy.get('input#13acfccb-3f39-3893-8054-058270add6ab').type('50.321') // ingen desimaler tas med på PROSENT
 
         cy.contains('Gå videre').click()
+        cy.url().should('include', `${gradertReisetilskudd.id}/8`)
         cy.contains('Tilbake').click()
+        cy.url().should('include', `${gradertReisetilskudd.id}/7`)
 
         cy.get('input#495730df-717d-3774-bd19-e6bcf76e3ba2').should('have.value', '37.32')
         cy.get('input#13acfccb-3f39-3893-8054-058270add6ab').should('have.value', '50')
@@ -32,7 +34,9 @@ describe('Tester at riktig antall desimaler sendes til backend', () => {
         cy.get('input#1547265').type('12.321') // maks 1 desimal tas med på KILOMETER
 
         cy.contains('Gå videre').click()
+        cy.url().should('include', `${gradertReisetilskudd.id}/13`)
         cy.contains('Tilbake').click()
+        cy.url().should('include', `${gradertReisetilskudd.id}/12`)
 
         cy.get('input#1547264').should('have.value', '500.32')
         cy.get('input#1547265').should('have.value', '12.3')
@@ -49,7 +53,9 @@ describe('Tester at riktig antall desimaler sendes til backend', () => {
         cy.get('input#13acfccb-3f39-3893-8054-058270add6ab').type('50.321wsergergwegr') // ingen desimaler tas med på PROSENT
 
         cy.contains('Gå videre').click()
+        cy.url().should('include', `${gradertReisetilskudd.id}/8`)
         cy.contains('Tilbake').click()
+        cy.url().should('include', `${gradertReisetilskudd.id}/7`)
 
         cy.get('input#495730df-717d-3774-bd19-e6bcf76e3ba2').should('have.value', '36.99')
         cy.get('input#13acfccb-3f39-3893-8054-058270add6ab').should('have.value', '50')
@@ -66,7 +72,9 @@ describe('Tester at riktig antall desimaler sendes til backend', () => {
         cy.get('input#13acfccb-3f39-3893-8054-058270add6ab').type('50.321wsergergwegr') // ingen desimaler tas med på PROSENT
 
         cy.contains('Gå videre').click()
+        cy.url().should('include', `${gradertReisetilskudd.id}/8`)
         cy.contains('Tilbake').click()
+        cy.url().should('include', `${gradertReisetilskudd.id}/7`)
 
         cy.get('input#495730df-717d-3774-bd19-e6bcf76e3ba2').should('have.value', '36')
         cy.get('input#13acfccb-3f39-3893-8054-058270add6ab').should('have.value', '50')
