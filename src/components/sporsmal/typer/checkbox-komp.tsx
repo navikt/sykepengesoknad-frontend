61920ff43e8476534374b1661021132d2050ada5
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-06-30 15:41:57 +0200

    Erstatter HelpText med forklaring av svaralternativ for checkbox

--- src/components/sporsmal/typer/checkbox-komp.tsx
+++ src/components/sporsmal/typer/checkbox-komp.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Checkbox, CheckboxGroup, Heading, HelpText } from '@navikt/ds-react'
+import { Checkbox, CheckboxGroup } from '@navikt/ds-react'
 import React, { Fragment } from 'react'
 import { Controller, useFormContext } from 'react-hook-form'
 
@@ -6,8 +6,6 @@ import { SpmProps } from '../sporsmal-form/sporsmal-form'
 import { hentFeilmelding } from '../sporsmal-utils'
 import UndersporsmalListe from '../undersporsmal/undersporsmal-liste'
 import Vis from '../../vis'
-import { TagTyper } from '../../../types/enums'
-import { Sporsmal } from '../../../types/types'
 
 const CheckboxKomp = ({ sporsmal }: SpmProps) => {
     const {
@@ -20,49 +18,6 @@ const CheckboxKomp = ({ sporsmal }: SpmProps) => {
         watchCheckbox = getValues(sporsmal.id)
     }
     const feilmelding = hentFeilmelding(sporsmal)
-
-    const HjelpeTekstTilSvaralternativ = (svaralternativ: Sporsmal) => {
-        if (svaralternativ.tag === TagTyper.INNTEKTSKILDE_ANDRE_ARBEIDSFORHOLD) {
-            return (
-                <HelpText>
-                    <Heading size="small">Ansatt eller mottar inntekt et annet sted</Heading>
-                    <BodyShort>Du kan ha startet i ny jobb, eller.......</BodyShort>
-                    <BodyShort>
-                        Ikke alle inntektskilder vises i offentlige registre ved sykemeldingstidspunktet, derfor er det
-                        viktig at du informerer om annen inntekt i denne perioden.
-                    </BodyShort>
-                </HelpText>
-            )
-        }
-
-        if (svaralternativ.tag === TagTyper.INNTEKTSKILDE_SELVSTENDIG) {
-            return (
-                <HelpText>
-                    <Heading size="small">Er jeg selvstendig næringsdrivende?</Heading>
-                    <BodyShort>
-                        En selvstendig næringsdrivende er en person som driver en virksomhet alene, uten å være ansatt
-                        av noen annen. De har det fulle ansvaret for driften og er den eneste eieren av virksomheten.
-                    </BodyShort>
-                    <BodyShort>Stemmer dette for deg svarer du på spørsmålene under.</BodyShort>
-                </HelpText>
-            )
-        }
-
-        if (svaralternativ.tag === TagTyper.INNTEKTSKILDE_FRILANSER) {
-            return (
-                <HelpText>
-                    <Heading size="small">Er jeg frilanser?</Heading>
-                    <BodyShort>
-                        Frilans er når en person jobber på selvstendig basis for forskjellige kunder uten å være fast
-                        ansatt.
-                    </BodyShort>
-                    <BodyShort>Stemmer dette for deg svarer du på spørsmålene under.</BodyShort>
-                </HelpText>
-            )
-        }
-
-        return <></>
-    }
 
     return (
         <Controller
@@ -81,7 +36,6 @@ const CheckboxKomp = ({ sporsmal }: SpmProps) => {
                                 <Checkbox id={uspm.id} value={uspm.sporsmalstekst}>
                                     {uspm.sporsmalstekst}
                                 </Checkbox>
-                                {HjelpeTekstTilSvaralternativ(uspm)}
                             </div>
                             <Vis
                                 hvis={watchCheckbox?.includes(uspm.sporsmalstekst)}
--- src/components/sporsmal/undersporsmal/undersporsmal-liste.tsx
+++ src/components/sporsmal/undersporsmal/undersporsmal-liste.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Label } from '@navikt/ds-react'
+import React, { Fragment } from 'react'
+import { BodyShort, Label } from '@navikt/ds-react'
 
 import { Sporsmal } from '../../../types/types'
 import SporsmalSwitch from '../sporsmal-switch'
@@ -17,7 +17,70 @@ const UndersporsmalListe = ({ oversporsmal, oversporsmalSvar }: UndersporsmalLis
         (!oversporsmal.kriterieForVisningAvUndersporsmal ||
             oversporsmal.kriterieForVisningAvUndersporsmal === oversporsmalSvar) &&
         oversporsmal.undersporsmal.length > 0
+
+    const ForklaringAvValgtCheckbox = (svaralternativ: Sporsmal) => {
+        // TODO: Tekstene er ikke helt ferdig og de vises bare dersom det finnes underspørsmål inni en checkbox
+        if (svaralternativ.tag === TagTyper.INNTEKTSKILDE_ANDRE_ARBEIDSFORHOLD) {
+            return (
+                <BodyShort>
+                    Du kan ha startet i ny jobb, eller....... Ikke alle inntektskilder vises i offentlige registre ved
+                    sykemeldingstidspunktet, derfor er det viktig at du informerer om annen inntekt i denne perioden.
+                </BodyShort>
+            )
+        }
+
+        if (svaralternativ.tag === TagTyper.INNTEKTSKILDE_SELVSTENDIG) {
+            return (
+                <BodyShort>
+                    Dette betyr at du er selvstendig næringsdrivende. Du driver en bedrift for egen regning og risiko;
+                    leverer skattemelding for næringsdrivende, og fakturerer kunder og (ofte) lever av overskuddet. Du
+                    er følgelig din egen sjef, og ikke ansatt av andre i et arbeidsforhold.
+                </BodyShort>
+            )
+        }
+
+        if (svaralternativ.tag === TagTyper.INNTEKTSKILDE_FRILANSER) {
+            return (
+                <BodyShort>
+                    Du jobber frilans når du jobber på selvstendig basis for forskjellige kunder uten å være fast
+                    ansatt.
+                </BodyShort>
+            )
+        }
+
+        if (svaralternativ.tag === TagTyper.INNTEKTSKILDE_STYREVERV) {
+            return (
+                <BodyShort>
+                    Å inneha et styreverv betyr at du er valgt inn i et selskaps styre av generalforsamlingen, og sitter
+                    vanligvis for to år av gangen.
+                </BodyShort>
+            )
+        }
+
+        if (svaralternativ.tag === TagTyper.INNTEKTSKILDE_FOSTERHJEM) {
+            return (
+                <BodyShort>
+                    Å motta fosterhjemsgodtgjørelse betyr at du mottar penger for å ha omsorg for et barn som ikke kan
+                    bo hos foreldrene sine.
+                </BodyShort>
+            )
+        }
+
+        if (svaralternativ.tag === TagTyper.INNTEKTSKILDE_SELVSTENDIG_DAGMAMMA) {
+            return (
+                <BodyShort>
+                    Dette betyr at du jobber som dagmamma og har regelmessig ansvar for andres barn mot betaling på
+                    dagtid. En dagmamma har som regel ansvar for flere barn samtidig, enten hjemme hos seg selv eller
+                    hjemme hos ett av barna.
+                </BodyShort>
+            )
+        }
+
+        return <Fragment />
+    }
+
     if (!skalVise) return null
+
     return (
         <div className="mt-4">
             {oversporsmal.tag == TagTyper.UTENLANDSK_SYKMELDING_BOSTED && (
@@ -25,6 +88,7 @@ const UndersporsmalListe = ({ oversporsmal, oversporsmalSvar }: UndersporsmalLis
                     {UndersporsmalTekster['undersporsmal.UTENLANDSK_SYKMELDING_BOSTED']}
                 </Label>
             )}
+            {ForklaringAvValgtCheckbox(oversporsmal)}
             {oversporsmal.undersporsmal
                 .map((underspm: Sporsmal, idx: number) => <SporsmalSwitch key={idx} sporsmal={underspm} />)
                 .filter((underspm: any) => underspm !== null)}
