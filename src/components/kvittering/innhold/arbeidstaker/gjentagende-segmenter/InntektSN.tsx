5cfb462a2a3a8bb486a8e516e5924d6a1f7bc5eb
Øivind <oivind.thorsen.kulsrud@nav.no>
2023-07-07 15:44:25 +0200

    wip

--- src/components/kvittering/innhold/arbeidstaker/gjentagende-segmenter/InntektSN.tsx
+++ src/components/kvittering/innhold/arbeidstaker/gjentagende-segmenter/InntektSN.tsx
@@ -1,10 +1,10 @@
 import { Button, BodyLong, Label } from '@navikt/ds-react'
 import React from 'react'
+import { ExternalLinkIcon } from '@navikt/aksel-icons'
 
 import { tekst } from '../../../../../utils/tekster'
 import { LenkeMedIkon } from '../../../../lenke-med-ikon/LenkeMedIkon'
 import Vis from '../../../../vis'
-import { ExternalLinkIcon } from '@navikt/aksel-icons'
 
 function InntektSN({ skalSendeInntektsmelding }: { skalSendeInntektsmelding: boolean }) {
     // TODO Fikse label i kvittering
@@ -17,21 +17,45 @@ function InntektSN({ skalSendeInntektsmelding }: { skalSendeInntektsmelding: boo
                     <Label as="h2">TODO Vi trenger inntektsopplysninger fra deg som selvstendig næringsdrivende</Label>
                     <BodyLong as="span">{tekst('kvittering.naeringsdrivende.brodtekst')} </BodyLong>
 
-                    {/* How can I make this button behave us a <link rel="stylesheet" href="" /> */}
-                    <Button iconPosition="right" variant="secondary" icon={<ExternalLinkIcon aria-hidden />} as="a" href={tekst('kvittering.naeringsdrivende.lenke.url')}>{tekst('kvittering.naeringsdrivende.lenke')}</Button>
-                    <br />
-                    <br />
-                    <Button iconPosition="right" variant="secondary" icon={<ExternalLinkIcon aria-hidden />} as="a" href={tekst('kvittering.naeringsdrivende.lenke.url')}>Gå til inntektsmelding for selvstendig næringsdrivende</Button>
-                    <br />
-                    <br />
-                    <Button iconPosition="right" variant="secondary" icon={<ExternalLinkIcon aria-hidden />} as="a" href={tekst('kvittering.naeringsdrivende.lenke.url')}>Send inntektsmelding</Button>
-                    
+                    {/* TODO velg en versjon av disse og rydd opp */}
 
-                     {/* <LenkeMedIkon
+                    {/* How can I make this button behave us a <link rel="stylesheet" href="" /> */}
+                    <Button
+                        iconPosition="right"
+                        variant="secondary"
+                        icon={<ExternalLinkIcon aria-hidden />}
+                        as="a"
+                        href={tekst('kvittering.naeringsdrivende.lenke.url')}
+                    >
+                        {tekst('kvittering.naeringsdrivende.lenke')}
+                    </Button>
+                    <br />
+                    <br />
+                    <Button
+                        iconPosition="right"
+                        variant="secondary"
+                        icon={<ExternalLinkIcon aria-hidden />}
+                        as="a"
+                        href={tekst('kvittering.naeringsdrivende.lenke.url')}
+                    >
+                        Gå til inntektsmelding for selvstendig næringsdrivende
+                    </Button>
+                    <br />
+                    <br />
+                    <Button
+                        iconPosition="right"
+                        variant="secondary"
+                        icon={<ExternalLinkIcon aria-hidden />}
+                        as="a"
+                        href={tekst('kvittering.naeringsdrivende.lenke.url')}
+                    >
+                        Send inntektsmelding
+                    </Button>
+
+                    {/* <LenkeMedIkon
                         href={tekst('kvittering.naeringsdrivende.lenke.url')}
                         text={tekst('kvittering.naeringsdrivende.lenke')}
                     />  */}
-                    
                 </div>
             )}
         />
