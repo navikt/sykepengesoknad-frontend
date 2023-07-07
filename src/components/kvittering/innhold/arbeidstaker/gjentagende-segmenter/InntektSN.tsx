3968417ac88f3108f2dc102e908f00ab50adf697
Øivind <oivind.thorsen.kulsrud@nav.no>
2023-07-07 15:30:27 +0200

    wip

--- src/components/kvittering/innhold/arbeidstaker/gjentagende-segmenter/InntektSN.tsx
+++ src/components/kvittering/innhold/arbeidstaker/gjentagende-segmenter/InntektSN.tsx
@@ -1,9 +1,10 @@
-import { BodyLong, Label } from '@navikt/ds-react'
+import { Button, BodyLong, Label } from '@navikt/ds-react'
 import React from 'react'
 
 import { tekst } from '../../../../../utils/tekster'
 import { LenkeMedIkon } from '../../../../lenke-med-ikon/LenkeMedIkon'
 import Vis from '../../../../vis'
+import { ExternalLinkIcon } from '@navikt/aksel-icons'
 
 function InntektSN({ skalSendeInntektsmelding }: { skalSendeInntektsmelding: boolean }) {
     // TODO Fikse label i kvittering
@@ -15,11 +16,18 @@ function InntektSN({ skalSendeInntektsmelding }: { skalSendeInntektsmelding: boo
                 <div className="mt-8">
                     <Label as="h2">TODO Vi trenger inntektsopplysninger fra deg som selvstendig næringsdrivende</Label>
                     <BodyLong as="span">{tekst('kvittering.naeringsdrivende.brodtekst')} </BodyLong>
-                    <LenkeMedIkon
+
+                    {/* How can I make this button behave us a <link rel="stylesheet" href="" /> */}
+                    <Button iconPosition="right" variant="secondary" icon={<ExternalLinkIcon aria-hidden />} as="a" href={tekst('kvittering.naeringsdrivende.lenke.url')}>{tekst('kvittering.naeringsdrivende.lenke')}</Button>
+                    <br />
+                    <br />
+                    <Button iconPosition="right" variant="secondary" icon={<ExternalLinkIcon aria-hidden />} as="a" href={tekst('kvittering.naeringsdrivende.lenke.url')}>Send inn skjema</Button>
+
+                     {/* <LenkeMedIkon
                         href={tekst('kvittering.naeringsdrivende.lenke.url')}
                         text={tekst('kvittering.naeringsdrivende.lenke')}
-                    />
-                    .
+                    />  */}
+                    
                 </div>
             )}
         />
