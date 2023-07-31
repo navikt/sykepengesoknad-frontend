192a887398568b17482f505fc14ecd27291e9c9e
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-07-31 09:24:26 +0200

    Behøver ikke være useEffect

--- src/components/sporsmal/sporsmal-form/sendes-til.tsx
+++ src/components/sporsmal/sporsmal-form/sendes-til.tsx
@@ -1,27 +1,26 @@
 import { BodyShort } from '@navikt/ds-react'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import { RSMottaker } from '../../../types/rs-types/rs-mottaker'
 import { getLedetekst, tekst } from '../../../utils/tekster'
 import { Soknad } from '../../../types/types'
 import { parserWithReplace } from '../../../utils/html-react-parser-utils'
 import useMottakerSoknad from '../../../hooks/useMottakerSoknad'
+import SoknadenTekster from '../../soknad/soknaden-tekster'
 
 export default function SendesTil({ soknad }: { soknad: Soknad }) {
-    const [nokkel, setNokkel] = useState<string>()
     const { data: mottaker } = useMottakerSoknad(soknad.id)
 
-    useEffect(() => {
-        if (mottaker === RSMottaker.ARBEIDSGIVER) {
-            setNokkel('sykepengesoknad.oppsummering.arbeidsgiver-som-mottaker')
-        }
-        if (mottaker === RSMottaker.NAV) {
-            setNokkel('sykepengesoknad.oppsummering.nav-som-mottaker')
-        }
-        if (mottaker === RSMottaker.ARBEIDSGIVER_OG_NAV) {
-            setNokkel('sykepengesoknad.oppsummering.nav-arbeidsgiver-som-mottaker')
-        }
-    }, [mottaker])
+    let nokkel: keyof typeof SoknadenTekster | undefined
+    if (mottaker === RSMottaker.ARBEIDSGIVER) {
+        nokkel = 'sykepengesoknad.oppsummering.arbeidsgiver-som-mottaker'
+    }
+    if (mottaker === RSMottaker.NAV) {
+        nokkel = 'sykepengesoknad.oppsummering.nav-som-mottaker'
+    }
+    if (mottaker === RSMottaker.ARBEIDSGIVER_OG_NAV) {
+        nokkel = 'sykepengesoknad.oppsummering.nav-arbeidsgiver-som-mottaker'
+    }
 
     if (!mottaker || !nokkel || soknad.arbeidssituasjon !== 'ARBEIDSTAKER') {
         return null
@@ -31,11 +30,11 @@ export default function SendesTil({ soknad }: { soknad: Soknad }) {
         <BodyShort as="div" className="mt-8 border-t border-gray-400 py-8 text-left">
             {soknad.arbeidsgiver !== undefined
                 ? parserWithReplace(
-                      getLedetekst(tekst(nokkel as any), {
+                      getLedetekst(tekst(nokkel), {
                           '%ARBEIDSGIVER%': soknad.arbeidsgiver.navn,
                       }),
                   )
-                : parserWithReplace(tekst(nokkel as any))}
+                : parserWithReplace(tekst(nokkel))}
         </BodyShort>
     )
 }
