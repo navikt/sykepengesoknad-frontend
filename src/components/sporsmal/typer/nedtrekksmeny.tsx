d7cd0551fce787c2a45d1abd59dbeac8f8b9bd16
golshidbahadorian <golshid.bahadorian@nav.no>
2023-08-01 15:09:23 +0200

    Registrerer svaret til nedtrekksmenyen og fikser styling på den

--- src/components/sporsmal/typer/nedtrekksmeny.tsx
+++ src/components/sporsmal/typer/nedtrekksmeny.tsx
@@ -1,24 +1,39 @@
-import { useFormContext } from 'react-hook-form'
+import { useFormContext, Controller } from 'react-hook-form'
 import { Select } from '@navikt/ds-react'
 
 import { SpmProps } from '../sporsmal-form/sporsmal-form'
 import { landlisteEøs, landlisteUtenforEøs } from '../landvelger/landliste'
+import { hentFeilmelding } from '../sporsmal-utils'
 
 const Nedtrekksmeny = ({ sporsmal }: SpmProps) => {
     const {
-        formState: {},
+        formState: { errors },
     } = useFormContext()
+
+    const feilmelding = hentFeilmelding(sporsmal)
 
     const alleLand = [...landlisteUtenforEøs, ...landlisteEøs].sort()
     return (
-        <Select className="mt-4" label={sporsmal.sporsmalstekst}>
-            <option value="">Velg land</option>
-            {alleLand.map((land, index) => (
-                <option key={index} value={land.toLowerCase()}>
-                    {land}
-                </option>
-            ))}
-        </Select>
+        <Controller
+            name={sporsmal.id}
+            rules={{ required: feilmelding.global }}
+            render={({ field }) => (
+                <Select
+                    id={sporsmal.id}
+                    {...field}
+                    label={sporsmal.sporsmalstekst}
+                    error={errors[sporsmal.id] !== undefined && feilmelding.lokal}
+                    className="mt-4 w-full md:w-1/2"
+                >
+                    <option value="">Velg land</option>
+                    {alleLand.map((land, index) => (
+                        <option key={index} value={land.toLowerCase()}>
+                            {land}
+                        </option>
+                    ))}
+                </Select>
+            )}
+        />
     )
 }
 
