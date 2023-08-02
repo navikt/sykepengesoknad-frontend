8f1c738a06443b82b0b13ec41bd41ed7df77dcc0
Øivind <oivind.thorsen.kulsrud@nav.no>
2023-08-02 14:55:38 +0200

    returns tekst to endring sn

--- src/components/hjelpetekster/ekspanderbar-hjelp/endring-sn-hjelp-tekst.ts
+++
@@ -1,6 +0,0 @@
-export const EndringSNHjelpTekster = {
-    introduksjon: 'Endring av arbeidssituasjon eller virksomhet kan handle om at:',
-    detalj1: 'tidligere virksomhet er lagt ned',
-    detalj2: 'det har vært en omlegging av virksomheten',
-    detalj3: 'den næringsdrivende har økt eller redusert arbeidsinnsatsen',
-}
