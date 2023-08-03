f072c7e1f9038f2645b1e355472166b3439ce2d6
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-08-03 14:23:52 +0200

    Kvitteirng sendt inntekt sn

--- src/components/kvittering/innhold/arbeidstaker/gjentagende-segmenter/InntektSN.tsx
+++ src/components/kvittering/innhold/arbeidstaker/gjentagende-segmenter/InntektSN.tsx
@@ -6,54 +6,27 @@ import { tekst } from '../../../../../utils/tekster'
 import Vis from '../../../../vis'
 
 function InntektSN({ skalSendeInntektsmelding }: { skalSendeInntektsmelding: boolean }) {
-    // TODO Fikse label i kvittering
-
     return (
         <Vis
             hvis={skalSendeInntektsmelding}
             render={() => (
-                <div className="mt-8">
-                    <Label as="h2">TODO Vi trenger inntektsopplysninger fra deg som selvstendig næringsdrivende</Label>
-                    <BodyLong as="span">{tekst('kvittering.naeringsdrivende.brodtekst')} </BodyLong>
+                <>
+                    <Label as="h2">Vi trenger inntektsopplysninger fra deg som selvstendig næringsdrivende</Label>
+                    <BodyLong as="span" spacing>
+                        {tekst('kvittering.naeringsdrivende.brodtekst')}{' '}
+                    </BodyLong>
 
-                    {/* How can I make this button behave us a <link rel="stylesheet" href="" /> */}
                     <Button
                         iconPosition="right"
                         variant="secondary"
                         icon={<ExternalLinkIcon aria-hidden />}
                         as="a"
                         href={tekst('kvittering.naeringsdrivende.lenke.url')}
+                        className="mt-2"
                     >
-                        {tekst('kvittering.naeringsdrivende.lenke')}
+                        Send inn inntektsopplysninger
                     </Button>
-                    <br />
-                    <br />
-                    <Button
-                        iconPosition="right"
-                        variant="secondary"
-                        icon={<ExternalLinkIcon aria-hidden />}
-                        as="a"
-                        href={tekst('kvittering.naeringsdrivende.lenke.url')}
-                    >
-                        Gå til inntektsmelding for selvstendig næringsdrivende
-                    </Button>
-                    <br />
-                    <br />
-                    <Button
-                        iconPosition="right"
-                        variant="secondary"
-                        icon={<ExternalLinkIcon aria-hidden />}
-                        as="a"
-                        href={tekst('kvittering.naeringsdrivende.lenke.url')}
-                    >
-                        Send inntektsmelding
-                    </Button>
-
-                    {/* <LenkeMedIkon
-                        href={tekst('kvittering.naeringsdrivende.lenke.url')}
-                        text={tekst('kvittering.naeringsdrivende.lenke')}
-                    />  */}
-                </div>
+                </>
             )}
         />
     )
