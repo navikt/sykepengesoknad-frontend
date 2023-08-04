70c092cc26ba106ee55a6115cc277c10c98a8a0a
Øivind <oivind.thorsen.kulsrud@nav.no>
2023-08-04 12:49:52 +0200

    gpt endringer i listen

--- src/components/sporsmal/inntektsbulletpoints.tsx
+++ src/components/sporsmal/inntektsbulletpoints.tsx
@@ -9,42 +9,39 @@ import { Soknad, Sporsmal } from '../../types/types'
 export const Inntektsbulletpoints = ({ sporsmal, soknad }: { sporsmal: Sporsmal; soknad: Soknad }) => {
     if (!skalHaInntektsbulletpoints(sporsmal, soknad)) return null
 
+
+    interface ListItemWithIconProps {
+        content: string | null | undefined;
+    }
+
+    const ListItemWithIcon = ({ content }: ListItemWithIconProps) => (
+        <li className="mb-4 flex max-w-sm gap-4 rounded-lg bg-gray-50 p-4 py-6 ">
+            <CheckmarkCircleIcon
+                title="a11y-title"
+                className="m-width-[24px] m-height-[24px] pt1 flex-shrink-0 text-xl font-bold"
+                height={24}
+                width={24}
+            />
+            <BodyShort className="">{content}</BodyShort>
+        </li>
+    );
+
+
+
     return (
         <>
-            <div className="flex max-w-sm gap-4 rounded-lg bg-gray-50 p-4 py-6">
-                <InformationIcon
-                    title="informasjon"
-                    className="m-width-[37px] m-height-[37px] flex-shrink-0 rounded-full bg-gray-200 text-xl font-bold"
-                    height={37}
-                    width={37}
-                />
-                <BodyShort size="small">
-                    Informasjon om andre arbeidsforhold blir behandlet konfidensielt, og blir ikke delt med arbeidsgiver
-                </BodyShort>
-            </div>
+            
             <Label as="p" className="mb-4 mt-10">
                 Arbeidsforhold vi har registrert på deg:
             </Label>
 
             <ul data-cy="inntektskilder--fra-inntektskomponenten-liste" className="mb-10">
-                <li className="mb-4 flex max-w-sm gap-4 rounded-lg bg-gray-50 p-4 py-6 ">
-                    <CheckmarkCircleIcon
-                        title="a11y-title"
-                        className="m-width-[24px] m-height-[24px] pt1 flex-shrink-0 text-xl font-bold"
-                        height={24}
-                        width={24} // padding top for the icon
-                    />{' '}
-                    <BodyShort className="">{soknad.arbeidsgiver?.navn}</BodyShort>
-                </li>
-
+                <ListItemWithIcon content={soknad.arbeidsgiver?.navn} />
                 {soknad.inntektskilderDataFraInntektskomponenten?.map((inntektskilde, index) => (
-                    <div key={index}>
-                        <BodyShort as="li" className="mb-4 ">
-                            {inntektskilde.navn}
-                        </BodyShort>
-                    </div>
+                    <ListItemWithIcon key={index} content={inntektskilde.navn} />
                 ))}
             </ul>
+
         </>
     )
 }
