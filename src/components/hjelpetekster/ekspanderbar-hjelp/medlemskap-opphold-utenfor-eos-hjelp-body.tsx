794c5e01de56d046e575be4961946a1807ea472c
golshidbahadorian <golshid.bahadorian@nav.no>
2023-08-07 11:57:42 +0200

    opphold utenfor EØS spørsmål forklart

--- src/components/hjelpetekster/ekspanderbar-hjelp/medlemskap-opphold-utenfor-eos-hjelp-body.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/medlemskap-opphold-utenfor-eos-hjelp-body.tsx
@@ -3,15 +3,18 @@ import { BodyLong } from '@navikt/ds-react'
 export const MedlemskapOppholdUtenforEOSHjelpBody = () => {
     return (
         <>
-            <BodyLong>Rett til sykepenger forutsetter at du har et aktivt medlemsskap i Norsk folketrygd.</BodyLong>
+            <BodyLong>Du må være medlem av folketrygden for å ha rett til sykepenger fra NAV.</BodyLong>
 
             <BodyLong className="mt-4">
-                I Norge kan du være medlem som bosatt, eller som arbeidstaker. Du kan også være medlem i folketrygden
-                under opphold i utlandet.
+                Et ledd i vår vurdering av dette er om du har oppholdt deg utenfor EØS området. Vi vet ikke alltid dette
+                og må derfor spørre deg.
             </BodyLong>
-            <BodyLong className="mt-4 font-bold">
-                Her skal du bare svare på perioder i utlandet hvor du ikke har arbeidet. Se bort fra ferieopphold
-                utenfor Norge med inntil 5 uker hvert kalenderår.
+            <BodyLong className="mt-4">
+                Svar ja, hvis du har vært i utlandet utenfor EØS, men ikke utført arbeid. Eller hvis du har hatt
+                ferieopphold mer enn 5 uker hvert kalenderår.
+            </BodyLong>
+            <BodyLong className="mt-4">
+                Svar nei, hvis du ikke har oppholdt deg utenfor EØS, eller hatt ferie opptil 5 uker utenfor EØS.
             </BodyLong>
         </>
     )
