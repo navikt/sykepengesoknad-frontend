5457e52c3283ecf127b488c7a0d675e86e4b6cb9
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-08-08 16:51:24 +0200

    wip

--- src/components/kvittering/kvittering-side.tsx
+++ src/components/kvittering/kvittering-side.tsx
@@ -22,6 +22,7 @@ import { SoknadHeader } from '../soknad/soknad-header'
 
 import Kvittering from './kvittering'
 import { harKorrigertArbeidstakersoknadIDetSiste } from './harSvartJa'
+import {KvitteringFeedback} from "../feedback/kvittering-feedback";
 
 const KvitteringSide = () => {
     const router = useRouter()
@@ -91,6 +92,7 @@ const KvitteringSide = () => {
                     )}
                 </>
             )}
+            <KvitteringFeedback />
 
             {skalViseEndre && <Endreknapp />}
             {skalViseSendTilArbeidsgiver && (
