bb50ed0627e60befb62b8c1315e67c2d647da58c
golshidbahadorian <golshid.bahadorian@nav.no>
2023-08-10 12:09:25 +0200

    Viser underspørsmålene til en IKKE_RELEVANT spørsmål

--- src/components/sporsmal/typer/ikke-relevant.tsx
+++ src/components/sporsmal/typer/ikke-relevant.tsx
@@ -2,22 +2,30 @@ import { BodyLong, Label } from '@navikt/ds-react'
 import React from 'react'
 
 import { parserWithReplace } from '../../../utils/html-react-parser-utils'
-import Vis from '../../vis'
 import { SpmProps } from '../sporsmal-form/sporsmal-form'
+import { TagTyper } from '../../../types/enums'
+import UndersporsmalListe from '../undersporsmal/undersporsmal-liste'
 
+const skalHaUndersporsmal = [
+    TagTyper.MEDLEMSKAP_OPPHOLD_UTENFOR_EOS_PERIODE,
+    TagTyper.MEDLEMSKAP_OPPHOLD_UTENFOR_NORGE_PERIODE,
+    TagTyper.MEDLEMSKAP_UTFORT_ARBEID_UTENFOR_NORGE_PERIODE,
+]
 const IkkeRelevant = ({ sporsmal }: SpmProps) => {
+    if (!skalHaUndersporsmal.includes(sporsmal.tag)) {
+        return (
+            <div className="mt-4 rounded-md border border-gray-600 p-4">
+                <Label as="h2" className="mb-4">
+                    {sporsmal.sporsmalstekst}
+                </Label>
+                {sporsmal.undertekst && <BodyLong as="div">{parserWithReplace(sporsmal.undertekst)}</BodyLong>}
+            </div>
+        )
+    }
     return (
-        <Vis
-            hvis={sporsmal.sporsmalstekst}
-            render={() => (
-                <div className="mt-4 rounded-md border border-gray-600 p-4">
-                    <Label as="h2" className="mb-4">
-                        {sporsmal.sporsmalstekst}
-                    </Label>
-                    {sporsmal.undertekst && <BodyLong as="div">{parserWithReplace(sporsmal.undertekst)}</BodyLong>}
-                </div>
-            )}
-        />
+        <div aria-live="assertive">
+            <UndersporsmalListe oversporsmal={sporsmal} />
+        </div>
     )
 }
 
