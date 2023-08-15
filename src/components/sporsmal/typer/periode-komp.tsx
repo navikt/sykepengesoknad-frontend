94b404c7a66b835f7b10ef890d7f06fbc4f45257
golshidbahadorian <golshid.bahadorian@nav.no>
2023-08-15 10:41:38 +0200

    Bruker design system feilmeldingene

--- src/components/sporsmal/typer/periode-komp.tsx
+++ src/components/sporsmal/typer/periode-komp.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Button, DatePicker, RangeValidationT, useRangeDatepicker } from '@navikt/ds-react'
+import { Button, DatePicker, RangeValidationT, useRangeDatepicker } from '@navikt/ds-react'
 import dayjs from 'dayjs'
 import React, { useEffect, useState } from 'react'
 import { Controller, useFormContext } from 'react-hook-form'
@@ -8,7 +8,6 @@ import { tekst } from '../../../utils/tekster'
 import Vis from '../../vis'
 import { hentPeriode } from '../hent-svar'
 import { SpmProps } from '../sporsmal-form/sporsmal-form'
-import { hentFeilmelding } from '../sporsmal-utils'
 
 interface PeriodeProps {
     index: number
@@ -25,13 +24,12 @@ const PeriodeKomp = ({ sporsmal, index, slettPeriode }: AllProps) => {
     const {
         setValue,
         getValues,
-        formState: { errors, isSubmitted },
+        formState: { isSubmitted },
         trigger,
     } = useFormContext()
 
     const [periode, setPeriode] = useState<FormPeriode>({ fom: '', tom: '' })
     const id = sporsmal.id + '_' + index
-    const feilmelding = hentFeilmelding(sporsmal, errors[id])
     const [rangeValidation, setRangeValidation] = useState<RangeValidationT | null>(null)
 
     useEffect(() => {
@@ -68,98 +66,57 @@ const PeriodeKomp = ({ sporsmal, index, slettPeriode }: AllProps) => {
 
     return (
         <li id={id} data-cy="periode">
-            <div>
-                <Controller
-                    name={id}
-                    rules={{
-                        validate: {
-                            fom: () => {
-                                const validert = validerFom(sporsmal, id, getValues(), rangeValidation)
-                                const div: HTMLElement | null = document.getElementById(id + '_fom')!.parentElement
-                                if (validert !== true) {
-                                    div?.classList.add('skjemaelement__input--harFeil')
-                                } else {
-                                    div?.classList.remove('skjemaelement__input--harFeil')
-                                }
-                                return validert
-                            },
-                            tom: () => {
-                                const validert = validerTom(sporsmal, id, getValues(), rangeValidation)
-                                const div: HTMLElement | null = document.getElementById(id + '_tom')!.parentElement
-                                if (validert !== true) {
-                                    div?.classList.add('skjemaelement__input--harFeil')
-                                } else {
-                                    div?.classList.remove('skjemaelement__input--harFeil')
-                                }
-                                return validert
-                            },
-                            periode: () => {
-                                const validert = validerPeriode(sporsmal, id, getValues())
-                                const div: HTMLElement | null = document.getElementById(id + '_fom')!.parentElement
-                                if (validert !== true) {
-                                    div?.classList.add('skjemaelement__input--harFeil')
-                                } else {
-                                    div?.classList.remove('skjemaelement__input--harFeil')
-                                }
-                                return validert
-                            },
-                        },
-                    }}
-                    render={() => (
-                        <div>
-                            <fieldset className="axe-exclude p-0">
-                                <DatePicker {...datepickerProps} dropdownCaption={captionSkalVises}>
-                                    <div>
-                                        <DatePicker.Input
-                                            {...fromInputProps}
-                                            label={tekst('sykepengesoknad.periodevelger.fom')}
-                                            id={sporsmal.id + '_' + index + '_fom'}
-                                            className="mt-6"
-                                            value={
-                                                periode.fom ? backendStrengTilFrontendFormat(periode.fom) : undefined
-                                            }
-                                        />
+            <Controller
+                name={id}
+                rules={{
+                    validate: {
+                        fom: () => validerFom(sporsmal, id, getValues(), rangeValidation),
+                        tom: () => validerTom(sporsmal, id, getValues(), rangeValidation),
+                        periode: () => validerPeriode(sporsmal, id, getValues()),
+                    },
+                }}
+                render={({ fieldState }) => (
+                    <fieldset className="axe-exclude p-0">
+                        <DatePicker {...datepickerProps} dropdownCaption={captionSkalVises}>
+                            <div>
+                                <DatePicker.Input
+                                    {...fromInputProps}
+                                    label={tekst('sykepengesoknad.periodevelger.fom')}
+                                    id={sporsmal.id + '_' + index + '_fom'}
+                                    className="mt-6"
+                                    error={fieldState.error?.type === 'fom' && fieldState.error.message}
+                                    value={periode.fom ? backendStrengTilFrontendFormat(periode.fom) : undefined}
+                                />
 
-                                        <DatePicker.Input
-                                            {...toInputProps}
-                                            label={tekst('sykepengesoknad.periodevelger.tom')}
-                                            id={sporsmal.id + '_' + index + '_tom'}
-                                            className="mt-4"
-                                            value={
-                                                periode.tom ? backendStrengTilFrontendFormat(periode.tom) : undefined
-                                            }
-                                        />
-                                        <Vis
-                                            hvis={index > 0}
-                                            render={() => (
-                                                <Button
-                                                    variant="tertiary"
-                                                    size="small"
-                                                    id={'btn_' + id}
-                                                    onClick={(e) => slettPeriode(e, index)}
-                                                >
-                                                    {tekst('sykepengesoknad.periodevelger.slett')}
-                                                </Button>
-                                            )}
-                                        />
-                                    </div>
-                                </DatePicker>
-                            </fieldset>
-                        </div>
-                    )}
-                />
-            </div>
-
-            <div role="alert" aria-live="assertive">
-                <Vis
-                    hvis={errors[id]}
-                    render={() => (
-                        <BodyShort as="span" className="mt-2 block font-bold text-surface-danger" data-cy="feil-lokal">
-                            {feilmelding.lokal}
-                        </BodyShort>
-                    )}
-                />
-            </div>
+                                <DatePicker.Input
+                                    {...toInputProps}
+                                    label={tekst('sykepengesoknad.periodevelger.tom')}
+                                    id={sporsmal.id + '_' + index + '_tom'}
+                                    className="mt-4"
+                                    error={
+                                        (fieldState.error?.type === 'tom' || fieldState.error?.type === 'periode') &&
+                                        fieldState.error.message
+                                    }
+                                    value={periode.tom ? backendStrengTilFrontendFormat(periode.tom) : undefined}
+                                />
+                                <Vis
+                                    hvis={index > 0}
+                                    render={() => (
+                                        <Button
+                                            variant="tertiary"
+                                            size="small"
+                                            id={'btn_' + id}
+                                            onClick={(e) => slettPeriode(e, index)}
+                                        >
+                                            {tekst('sykepengesoknad.periodevelger.slett')}
+                                        </Button>
+                                    )}
+                                />
+                            </div>
+                        </DatePicker>
+                    </fieldset>
+                )}
+            />
         </li>
     )
 }
