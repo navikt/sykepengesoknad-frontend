027257a0288dd5ff696b70611b7ba56fdda06c86
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-08-21 07:20:34 +0200

    Pixelpushing på liste (#1718)

--- src/components/hjelpetekster/ekspanderbar-hjelp/andre-inntektskilder-hjelp-body.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/andre-inntektskilder-hjelp-body.tsx
@@ -1,4 +1,4 @@
-import { BodyShort } from '@navikt/ds-react'
+import { BodyShort, List } from '@navikt/ds-react'
 
 import { Begrepsforklarer } from '../../begrepsforklarer/begrepsforklarer'
 
@@ -8,33 +8,38 @@ export const AndreInntektskilderHjelpBody = () => {
     return (
         <>
             <BodyShort spacing>{AndreInntektskilderHjelpTekster['hjelpetekst.del1']}</BodyShort>
-            <div>
-                {AndreInntektskilderHjelpTekster['hjelpetekst.del2']}
-                <Begrepsforklarer
-                    inlinetekst={AndreInntektskilderHjelpTekster['modal1.inlinetekst']}
-                    tittel={AndreInntektskilderHjelpTekster['modal1.tittel']}
-                >
-                    <BodyShort className="pt-3">{AndreInntektskilderHjelpTekster['modal1.del1']}</BodyShort>
+            {AndreInntektskilderHjelpTekster['hjelpetekst.del2']}
+            <Begrepsforklarer
+                inlinetekst={AndreInntektskilderHjelpTekster['modal1.inlinetekst']}
+                tittel={AndreInntektskilderHjelpTekster['modal1.tittel']}
+            >
+                <BodyShort className="pt-3">{AndreInntektskilderHjelpTekster['modal1.del1']}</BodyShort>
 
-                    <BodyShort className="pt-3">{AndreInntektskilderHjelpTekster['modal1.del2']}</BodyShort>
-                </Begrepsforklarer>
-                {AndreInntektskilderHjelpTekster['hjelpetekst.del3']}{' '}
-                <Begrepsforklarer
-                    inlinetekst={AndreInntektskilderHjelpTekster['modal2.inlinetekst']}
-                    tittel={AndreInntektskilderHjelpTekster['modal2.tittel']}
-                >
-                    <BodyShort spacing>{AndreInntektskilderHjelpTekster['modal2.tekst']}</BodyShort>
-                </Begrepsforklarer>{' '}
-                {AndreInntektskilderHjelpTekster['hjelpetekst.del4']}{' '}
-            </div>
-
+                <BodyShort className="pt-3">{AndreInntektskilderHjelpTekster['modal1.del2']}</BodyShort>
+            </Begrepsforklarer>
+            {AndreInntektskilderHjelpTekster['hjelpetekst.del3']}{' '}
+            <Begrepsforklarer
+                inlinetekst={AndreInntektskilderHjelpTekster['modal2.inlinetekst']}
+                tittel={AndreInntektskilderHjelpTekster['modal2.tittel']}
+            >
+                <BodyShort spacing>{AndreInntektskilderHjelpTekster['modal2.tekst']}</BodyShort>
+            </Begrepsforklarer>{' '}
+            {AndreInntektskilderHjelpTekster['hjelpetekst.del4']}
             <BodyShort className="pt-3"> {AndreInntektskilderHjelpTekster['liste.tittel']} </BodyShort>
-            <BodyShort as="ul" className="pl-2 pt-3">
-                <li>{AndreInntektskilderHjelpTekster['liste.listepunkt1']}</li>
-                <li>{AndreInntektskilderHjelpTekster['liste.listepunkt2']}</li>
-                <li>{AndreInntektskilderHjelpTekster['liste.listepunkt3']}</li>
-                <li>{AndreInntektskilderHjelpTekster['liste.listepunkt4']}</li>
-            </BodyShort>
+            <List as="ul" size="small" className="[&>ul]:mt-2">
+                <List.Item>
+                    <BodyShort>{AndreInntektskilderHjelpTekster['liste.listepunkt1']}</BodyShort>
+                </List.Item>
+                <List.Item>
+                    <BodyShort>{AndreInntektskilderHjelpTekster['liste.listepunkt2']}</BodyShort>
+                </List.Item>
+                <List.Item>
+                    <BodyShort>{AndreInntektskilderHjelpTekster['liste.listepunkt3']}</BodyShort>
+                </List.Item>
+                <List.Item>
+                    <BodyShort>{AndreInntektskilderHjelpTekster['liste.listepunkt4']}</BodyShort>
+                </List.Item>
+            </List>
         </>
     )
 }
