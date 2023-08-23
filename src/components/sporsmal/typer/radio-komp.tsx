7eafc6e67c028f63144e2128046b4d5b3806fe4e
oivindkulsrud <oivindkulsrud@gmail.com>
2023-08-23 11:38:12 +0200

    Legger inn støtte for forklaring av radiokomp sporsmal

--- src/components/sporsmal/typer/radio-komp.tsx
+++ src/components/sporsmal/typer/radio-komp.tsx
@@ -1,10 +1,14 @@
-import { Radio, RadioGroup } from '@navikt/ds-react'
-import React from 'react'
+import { BodyShort, List, Radio, RadioGroup } from '@navikt/ds-react'
+import React, { Fragment } from 'react'
 import { Controller, useFormContext } from 'react-hook-form'
 
 import { SpmProps } from '../sporsmal-form/sporsmal-form'
 import { hentFeilmelding } from '../sporsmal-utils'
 import UndersporsmalListe from '../undersporsmal/undersporsmal-liste'
+import { Sporsmal } from '../../../types/types'
+import { TagTyper } from '../../../types/enums'
+import { Begrepsforklarer } from '../../begrepsforklarer/begrepsforklarer'
+import { AndreInntektskilderHjelpTekster } from '../../hjelpetekster/ekspanderbar-hjelp/andre-inntektskilder-hjelp-tekst'
 
 const RadioKomp = ({ sporsmal }: SpmProps) => {
     const {
@@ -18,6 +22,27 @@ const RadioKomp = ({ sporsmal }: SpmProps) => {
     }
 
     const feilmelding = hentFeilmelding(sporsmal)
+
+    const forklaringAvSporsmal = (sporsmal: Sporsmal) => {
+        if (sporsmal.tag == TagTyper.INNTEKTSKILDE_SELVSTENDIG_VARIG_ENDRING_GRUPPE) {
+            return (
+                <BodyShort>
+                    Varig endring av arbeidssituasjon eller virksomhet kan være at:
+                    <List as="ul" size="small" className="ml-4">
+                        <List.Item>
+                            <BodyShort>tidligere virksomhet er lagt ned</BodyShort>
+                        </List.Item>
+                        <List.Item>
+                            <BodyShort>det har vært en omlegging av virksomheten</BodyShort>
+                        </List.Item>
+                        <List.Item>
+                            <BodyShort>den næringsdrivende har økt eller redusert arbeidsinnsatsen</BodyShort>
+                        </List.Item>
+                    </List>
+                </BodyShort>
+            )
+        }
+    }
 
     return (
         <>
@@ -33,7 +58,10 @@ const RadioKomp = ({ sporsmal }: SpmProps) => {
                         error={errors[sporsmal.id] !== undefined && feilmelding.lokal}
                         key={sporsmal.id}
                         className="mt-8"
+                        data-cy="radio-komp"
                     >
+                        {forklaringAvSporsmal(sporsmal)}
+
                         {sporsmal.undersporsmal.map((uspm) => (
                             <Radio key={uspm.id} id={uspm.id} value={uspm.sporsmalstekst}>
                                 {uspm.sporsmalstekst}
