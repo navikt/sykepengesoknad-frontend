da99ebd24754ec9da310b3453ce439cfcf6f7548
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-08-23 16:38:34 +0200

    Alle land og bare land utenfor EØS

--- src/components/sporsmal/typer/combobox-single.tsx
+++ src/components/sporsmal/typer/combobox-single.tsx
@@ -5,6 +5,7 @@ import { useMemo } from 'react'
 import { SpmProps } from '../sporsmal-form/sporsmal-form'
 import { landlisteEøs, landlisteUtenforEøs } from '../landvelger/landliste'
 import { hentFeilmelding } from '../sporsmal-utils'
+import { TagTyper } from '../../../types/enums'
 
 const ComboboxSingle = ({ sporsmal }: SpmProps) => {
     const {
@@ -13,7 +14,13 @@ const ComboboxSingle = ({ sporsmal }: SpmProps) => {
 
     const feilmelding = hentFeilmelding(sporsmal)
 
-    const alleLand = useMemo(() => [...landlisteUtenforEøs, ...landlisteEøs].sort(), [])
+    const options = useMemo(() => {
+        if (sporsmal.tag === TagTyper.MEDLEMSKAP_OPPHOLD_UTENFOR_EOS_HVOR) {
+            return landlisteUtenforEøs.sort()
+        } else {
+            return landlisteUtenforEøs.concat(landlisteEøs).sort()
+        }
+    }, [sporsmal])
 
     return (
         <Controller
@@ -26,7 +33,7 @@ const ComboboxSingle = ({ sporsmal }: SpmProps) => {
                         id={sporsmal.id}
                         label={sporsmal.sporsmalstekst}
                         error={errors[sporsmal.id] !== undefined && feilmelding.lokal}
-                        options={alleLand}
+                        options={options}
                         className="mt-4 w-full md:w-1/2"
                         selectedOptions={[field.value]}
                         shouldShowSelectedOptions={true}
@@ -39,7 +46,7 @@ const ComboboxSingle = ({ sporsmal }: SpmProps) => {
                         onToggleSelected={(option, isSelected) => {
                             if (isSelected) {
                                 const optionLowerCase = option.toLowerCase()
-                                const valgtLand = alleLand.find((land) => optionLowerCase === land.toLowerCase())
+                                const valgtLand = options.find((land) => optionLowerCase === land.toLowerCase())
                                 if (valgtLand) {
                                     field.onChange(valgtLand)
                                 }
