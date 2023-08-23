adbb1262d0847648964454ca4f5862b15ef8eb58
oivindkulsrud <oivindkulsrud@gmail.com>
2023-08-23 13:24:09 +0200

    Fikser mangel på conditional rendering av anonymitetsinfo

--- src/components/sporsmal/typer/checkbox-komp.tsx
+++ src/components/sporsmal/typer/checkbox-komp.tsx
@@ -42,20 +42,20 @@ const CheckboxKomp = ({ sporsmal }: SpmProps) => {
             rules={{ required: feilmelding.global }}
             render={({ field }) => (
                 <>
-
-                    {sporsmal.tag === TagTyper.ANDRE_INNTEKTSKILDER_V2 && <div className="mt-4 flex max-w-sm gap-4 rounded-lg py-6">
-                        <InformationIcon
-                            title="informasjon"
-                            className="flex-shrink-0 rounded-full bg-gray-200 p-2 text-sm font-bold"
-                            height={37}
-                            width={37}
-                        />
-                        <BodyShort size="small">
-                            Informasjon om andre inntektskilder blir behandlet konfidensielt, og blir ikke delt med
-                            arbeidsgiver
-                        </BodyShort>
-                    </div>}
-
+                    {sporsmal.tag === TagTyper.HVILKE_ANDRE_INNTEKTSKILDER && (
+                        <div className="mt-4 flex max-w-sm gap-4 rounded-lg py-6">
+                            <InformationIcon
+                                title="informasjon"
+                                className="flex-shrink-0 rounded-full bg-gray-200 p-2 text-sm font-bold"
+                                height={37}
+                                width={37}
+                            />
+                            <BodyShort size="small">
+                                Informasjon om andre inntektskilder blir behandlet konfidensielt, og blir ikke delt med
+                                arbeidsgiver
+                            </BodyShort>
+                        </div>
+                    )}
 
                     <CheckboxGroup
                         {...field}
