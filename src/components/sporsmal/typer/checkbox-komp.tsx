cde6b56a77cb4492ce5d1eef3b5298ddd13b5bd0
oivindkulsrud <oivindkulsrud@gmail.com>
2023-08-24 10:24:05 +0200

    rydder etter rebase

--- src/components/sporsmal/typer/checkbox-komp.tsx
+++ src/components/sporsmal/typer/checkbox-komp.tsx
@@ -56,29 +56,11 @@ const CheckboxKomp = ({ sporsmal }: SpmProps) => {
             name={sporsmal.id}
             rules={{ required: feilmelding.global }}
             render={({ field }) => (
-               
-            
-                    {sporsmal.tag === TagTyper.HVILKE_ANDRE_INNTEKTSKILDER && (
-                        <div className="mt-4 flex max-w-sm gap-4 rounded-lg py-6">
-                            <InformationIcon
-                                title="informasjon"
-                                className="flex-shrink-0 rounded-full bg-gray-200 p-2 text-sm font-bold"
-                                height={37}
-                                width={37}
-                            />
-                            <BodyShort size="small">
-                                Informasjon om andre inntektskilder blir behandlet konfidensielt, og blir ikke delt med
-                                arbeidsgiver
-                            </BodyShort>
-                        </div>
-                    )}
-                    
-                     <div
-                     className={cn('', {
-                         'max-w-sm': checkboxesSkalHaSpesiellStyling(sporsmal.tag),
-                     })}
+                <div
+                    className={cn('', {
+                        'max-w-sm': checkboxesSkalHaSpesiellStyling(sporsmal.tag),
+                    })}
                 >
-
                     <CheckboxGroup
                         {...field}
                         legend={sporsmal.sporsmalstekst}
