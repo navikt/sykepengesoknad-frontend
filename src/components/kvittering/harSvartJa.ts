e303c4d97d94611e5dd869d2898528717b58b0e1
Bjørn Inge Kulsveen <35369790+kulsveen@users.noreply.github.com>
2023-10-17 13:16:50 +0200

    UxSignals på arbeid utenfor Norge (#1909)

--- src/components/kvittering/harSvartJa.ts
+++ src/components/kvittering/harSvartJa.ts
@@ -1,18 +1,12 @@
-import dayjs from 'dayjs'
-
-import { RSSoknadmetadata } from '../../types/rs-types/rs-soknadmetadata'
 import { Soknad } from '../../types/types'
 import { RSSoknadstype } from '../../types/rs-types/rs-soknadstype'
+import { TagTyper } from '../../types/enums'
 
-export function harKorrigertArbeidstakersoknadIDetSiste(soknader: RSSoknadmetadata[]) {
-    const toMndSiden = dayjs().subtract(64, 'days').toDate()
-
+export function harSvartJaArbeidUtenforNorge(soknad: Soknad) {
     return (
-        soknader
-            .filter((s) => s.status === 'SENDT')
-            .filter((s) => s.soknadstype === 'ARBEIDSTAKERE')
-            .filter((s) => s.korrigerer)
-            .filter((s) => s.opprettetDato > toMndSiden).length > 0
+        soknad.sporsmal
+            .find((spm) => spm.tag === TagTyper.ARBEID_UTENFOR_NORGE)
+            ?.svarliste.svar.find((svar) => svar.verdi === 'JA') !== undefined
     )
 }
 
