eaa4065d15affd7dd0fc3f4309cc92b89a655270
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-11-06 21:35:30 +0100

    De grå feltene bruker nå hele bredden på mobil

--- src/components/sporsmal/inntektsbulletpoints.tsx
+++ src/components/sporsmal/inntektsbulletpoints.tsx
@@ -22,7 +22,7 @@ export const Inntektsbulletpoints = ({ soknad }: { soknad: Soknad }) => {
 }
 
 const ListItemWithIcon = ({ content }: { content: string }) => (
-    <li className="mb-4 flex max-w-[320px] items-center gap-4 rounded-lg bg-gray-50 p-2 min-h-[48px]">
+    <li className="mb-4 flex w-full md:max-w-[320px] items-center gap-4 rounded-lg bg-gray-50 p-2 min-h-[48px]">
         <CheckmarkCircleIcon
             title="a11y-title"
             className="m-width-[24px] m-height-[24px] pt1  text-xl font-bold shrink-0"
