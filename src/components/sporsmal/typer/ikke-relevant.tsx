1a474a6fb284a213e58698e280ba341fcca16c7d
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-11-22 15:48:20 +0100

    wip

--- src/components/sporsmal/typer/ikke-relevant.tsx
+++ src/components/sporsmal/typer/ikke-relevant.tsx
@@ -5,6 +5,13 @@ import { parserWithReplace } from '../../../utils/html-react-parser-utils'
 import { SpmProps } from '../sporsmal-form/sporsmal-form'
 
 const IkkeRelevant = ({ sporsmal }: SpmProps) => {
+    if (sporsmal.tag.includes('KJENTE_INNTEKTKILDER_GRUPPE_TITTEL')) {
+        return (
+            <Label as="h2" className="mb-4">
+                {sporsmal.sporsmalstekst}
+            </Label>
+        )
+    }
     return (
         <div className="mt-4 rounded-md border border-gray-600 p-4">
             <Label as="h2" className="mb-4">
