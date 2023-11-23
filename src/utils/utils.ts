0b7d3ee4a8163f02afed1b167538c236a34f5a94
golshidbahadorian <golshid.bahadorian@nav.no>
2023-11-23 13:31:58 +0100

    Sender ren tekst til backend

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,3 +10,14 @@ export const formatterTall = (tall?: number, desimaler = 0): string => {
         return ''
     }
 }
+
+export function konverterLenkerTilRenTekst(htmlString: string): string {
+    const dokument = new DOMParser().parseFromString(htmlString, 'text/html')
+    dokument.querySelectorAll('a').forEach((lenke) => {
+        const lenkeTekst = lenke.textContent || ''
+        const lenkeHref = lenke.getAttribute('href') || ''
+        const tekstMedLenke = lenkeHref ? `${lenkeTekst} (${lenkeHref})` : lenkeTekst
+        lenke.replaceWith(dokument.createTextNode(tekstMedLenke))
+    })
+    return dokument.body.textContent || ''
+}
