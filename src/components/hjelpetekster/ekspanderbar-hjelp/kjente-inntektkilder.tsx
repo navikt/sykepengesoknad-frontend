9c03ee8d49797f13f2035f5e38b6ec51dea3ddde
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-12-04 11:44:50 +0100

    Hjelpetekst kjente inntektskilder

--- src/components/hjelpetekster/ekspanderbar-hjelp/kjente-inntektkilder.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/kjente-inntektkilder.tsx
@@ -3,14 +3,18 @@ import { BodyLong } from '@navikt/ds-react'
 export const KjenteInntektkilderHjelpBody = () => {
     return (
         <>
-            <BodyLong>
-                Det er kun inntekt fra inntektsforhold du har på sykmeldingstidspunktet (skjæringstidspunktet), som skal
-                regnes med. Hvis du har flere arbeidsforhold, skal sykepengene beregnes utfra den samlede inntekten fra
-                alle arbeidsforholdene dine. Dette gjelder også når du kun er sykmeldt fra ett av arbeidsforholdene.
+            <BodyLong spacing>
+                Sykepengene blir beregnet ut fra den samlede inntekten din. Fosterhjemsgodtgjørelse eller kommunal
+                omsorgslønn / omsorgsstønad i kombinasjon med annen inntekt, blir regnet som flere jobber.
             </BodyLong>
-            <BodyLong className="mt-4">
+            <BodyLong spacing>
+                Det er kun inntekt fra inntektsforhold du har på sykmeldingstidspunktet som skal regnes med. Hvis du har
+                flere arbeidsforhold, skal sykepengene beregnes ut fra den samlede inntekten fra alle arbeidsforholdene
+                dine. Dette gjelder også når du kun er sykmeldt fra ett av arbeidsforholdene.
+            </BodyLong>
+            <BodyLong spacing>
                 Vi vet ikke alltid hvilke inntektsforhold du faktisk har på sykmeldingstidspunktet, og som skal være med
-                i beregningen og må derfor spørre deg. Svarene du gir her vil brukes til å beregne sykepengene dine
+                i beregningen og må derfor spørre deg. Svarene du gir her vil brukes til å beregne sykepengene dine.
             </BodyLong>
         </>
     )
