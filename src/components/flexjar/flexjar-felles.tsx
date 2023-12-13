bde51a92d5dcf42d74a685e0e3389a8fb810ab45
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-12-13 15:52:09 +0100

    aria hidden

--- src/components/flexjar/flexjar-felles.tsx
+++ src/components/flexjar/flexjar-felles.tsx
@@ -94,7 +94,7 @@ export function FlexjarFelles({
                 <div className="mt-1 border-4 border-surface-subtle rounded-medium">
                     <div className="bg-surface-subtle p-6 flex gap-4 items-center">
                         <div className="bg-gray-900 w-10 h-10 rounded-full flex justify-center items-center">
-                            <MagnifyingGlassIcon className="text-white axe-exclude" />
+                            <MagnifyingGlassIcon aria-hidden={true} className="text-white axe-exclude" />
                         </div>
                         <div>
                             <Label as="h3" className="mb-2">
