84d2973d06f095b01f4c0c85108167a9335d2891
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-12-15 16:06:19 +0100

    Hjelpetekst permisjon

--- src/components/hjelpetekster/ekspanderbar-hjelp/permisjon-hjelp-body.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/permisjon-hjelp-body.tsx
@@ -4,16 +4,14 @@ export const PermisjonHjelpBody = () => {
     return (
         <>
             <BodyShort spacing>
-                Permisjon er dager du var borte fra jobb av andre grunner enn sykdom, ferie eller avspasering. Det
-                utbetales ikke sykepenger for den delen du tar permisjon. Det er ikke alltid vi vet om du er i permisjon
-                og vi må derfor spørre deg.
+                Permisjon er dager du var borte fra jobb av andre grunner enn sykdom, ferie eller avspasering. Det er
+                ikke alltid vi vet om du er i permisjon og vi må derfor spørre deg. Det utbetales som hovedregel ikke
+                sykepenger når du er i permisjon.
             </BodyShort>
-            <BodyShort spacing>
-                Svar ja, hvis du blir sykmeldt mens du er i permisjon selv om permisjonen er utdanningspermisjon eller
-                foreldrepermisjon.
-            </BodyShort>
+            <BodyShort spacing>Svar JA, hvis du har avtalt permisjon og derfor ikke skal ha sykepenger.</BodyShort>
             <BodyShort>
-                Svar nei, hvis du blir syk før en planlagt permisjon, har ferie, er permittert eller har seniordager.
+                Svar NEI hvis du søker om sykepenger i stedet for foreldrepermisjon. Du svarer også NEI hvis du har
+                ferie, er permittert, har seniordager eller blir syk før en planlagt permisjon.”
             </BodyShort>
         </>
     )
