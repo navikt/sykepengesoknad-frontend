9fdd234ff6500e8cae49f3f2985902e710ad8b2c
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-12-18 08:48:01 +0100

    Update permisjon-hjelp-body.tsx

--- src/components/hjelpetekster/ekspanderbar-hjelp/permisjon-hjelp-body.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/permisjon-hjelp-body.tsx
@@ -8,9 +8,9 @@ export const PermisjonHjelpBody = () => {
                 ikke alltid vi vet om du er i permisjon og vi må derfor spørre deg. Det utbetales som hovedregel ikke
                 sykepenger når du er i permisjon.
             </BodyShort>
-            <BodyShort spacing>Svar JA, hvis du har avtalt permisjon og derfor ikke skal ha sykepenger.</BodyShort>
+            <BodyShort spacing>Svar ja, hvis du har avtalt permisjon og derfor ikke skal ha sykepenger.</BodyShort>
             <BodyShort>
-                Svar NEI hvis du søker om sykepenger i stedet for foreldrepermisjon. Du svarer også NEI hvis du har
+                Svar nei hvis du søker om sykepenger i stedet for foreldrepermisjon. Du svarer også nei hvis du har
                 ferie, er permittert, har seniordager eller blir syk før en planlagt permisjon.”
             </BodyShort>
         </>
