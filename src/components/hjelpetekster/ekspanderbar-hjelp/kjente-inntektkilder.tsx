1db21fec97cfc09a69aff6f77e30a57f01dbb2c3
Golshid Bahadorian <36138378+golshidb93@users.noreply.github.com>
2023-12-20 15:11:28 +0100

    Tekstendring i spørsmål forklart kjente inntektskilder (#2131)

--- src/components/hjelpetekster/ekspanderbar-hjelp/kjente-inntektkilder.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/kjente-inntektkilder.tsx
@@ -4,16 +4,17 @@ export const KjenteInntektkilderHjelpBody = () => {
     return (
         <>
             <BodyLong spacing>
-                Sykepengene blir beregnet ut fra den samlede inntekten din. Fosterhjemsgodtgjørelse eller kommunal
-                omsorgslønn / omsorgsstønad i kombinasjon med annen inntekt, blir regnet som flere jobber.
+                Sykepengene blir beregnet ut fra den samlede pensjonsgivende inntekten din. Fosterhjemsgodtgjørelse
+                eller kommunal omsorgslønn / omsorgsstønad i kombinasjon med annen inntekt, blir regnet som flere
+                jobber.
             </BodyLong>
             <BodyLong spacing>
-                Det er kun inntekt fra inntektsforhold du har på sykmeldingstidspunktet som skal regnes med. Hvis du har
-                flere arbeidsforhold, skal sykepengene beregnes ut fra den samlede inntekten fra alle arbeidsforholdene
-                dine. Dette gjelder også når du kun er sykmeldt fra ett av arbeidsforholdene.
+                Det er kun pensjonsgivende inntekt fra inntektskilder du har på sykmeldingstidspunktet som skal regnes
+                med. Hvis du har flere inntektskilder, skal sykepengene beregnes ut fra den samlede inntekten fra alle
+                inntektskildene dine. Dette gjelder også når du kun er sykmeldt fra en av inntektskildene.
             </BodyLong>
             <BodyLong spacing>
-                Vi vet ikke alltid hvilke inntektsforhold du faktisk har på sykmeldingstidspunktet, og som skal være med
+                Vi vet ikke alltid hvilke inntektskilder du faktisk har på sykmeldingstidspunktet, og som skal være med
                 i beregningen og må derfor spørre deg. Svarene du gir her vil brukes til å beregne sykepengene dine.
             </BodyLong>
         </>
