42f49b8bb9c25380cfa8def9bb41dcb4dd53631a
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-01-11 10:31:35 +0100

    Listevisning opprydning (#2187)

--- src/components/om-sykepenger/om-sykepenger-tekster.ts
+++ src/components/om-sykepenger/om-sykepenger-tekster.ts
@@ -5,32 +5,11 @@ const OmSykepengerTekster = {
     'om.sykepenger.hvorfor': 'Hvorfor må jeg søke om sykepenger?',
     'om.sykepenger.tekst3':
         'I søknaden svarer du på noen spørsmål om hva som skjedde de dagene du var sykmeldt. Vi bruker svarene til å avgjøre hva du har krav på, og om det er NAV eller arbeidsgiveren din som skal betale. Disse opplysningene får vi foreløpig ikke innhentet automatisk.',
-    'om.sykepenger.arbeidstakere.tittel': 'For arbeidstakere',
-    'om.sykepenger.arbeidstakere.tekst1':
-        'De første 16 dagene er det arbeidsgiveren din som betaler sykepengene. Noen arbeidsgivere fortsetter å betale sykepenger, så får de pengene tilbake fra NAV senere.',
-    'om.sykepenger.arbeidstakere.tekst2':
-        'Uansett må arbeidsgiveren din sende oss inntektsmelding. Den inneholder opplysninger om inntekten din, som vi må ha for å behandle søknaden.',
-    'om.sykepenger.arbeidstakere.alertstripe':
-        'Hør gjerne med arbeidsgiveren din om inntektsmeldingen er sendt. Vi får ikke behandlet søknaden din før vi har den.',
-    'om.sykepenger.selvstendige.tittel': 'For selvstendig næringsdrivende og frilansere',
-    'om.sykepenger.selvstendige.tekst1':
-        'Har du tegnet <a href="https://www.nav.no/no/bedrift/tjenester-og-skjemaer/selvstendig-naringsdrivende-med-flere/forsikring" target="_blank" rel="noopener">forsikring</a>, får du sykepenger fra første dag i sykefraværet. Du leverer søknaden om sykepenger etter sykmeldingsperioden. ',
-    'om.sykepenger.selvstendige.tekst2':
-        'Har du ikke forsikring, får du sykepenger først etter 16 dager. Du trenger ikke levere søknad hvis sykefraværet er kortere enn 16 dager.',
-    'om.sykepenger.selvstendige.tekst3':
-        'Du trenger ikke å levere søknaden om sykefraværet ditt ikke går over 16 dager.',
-    'om.sykepenger.selvstendige.husk': 'Husk inntektsopplysninger',
-    'om.sykepenger.selvstendige.tekst4':
-        'I tillegg til søknaden må du sende oss <a href="https://www.nav.no/soknader/nb/bedrift/inntekt-og-trekk/inntektsopplysninger-sykepenger/NAV%2008-35.01/dokumentinnsending" target="_blank" rel="noopener">skjemaet for inntektsopplysninger</a>.',
-    'om.sykepenger.selvstendige.alertstripe':
-        'Viktig å huske på:<br />Vi får ikke behandlet søknaden før vi har fått inntekts&shy;opp&shy;lys&shy;ningene.',
     'om.sykepenger.tittel2': 'Skal du ut og reise?',
     'om.sykepenger.lenke1': 'Les om hva du må gjøre for å beholde sykepengene.',
-    'om.sykepenger.lenke1.url':
-        'https://www.nav.no/no/person/arbeid/sykmeldt-arbeidsavklaringspenger-og-yrkesskade/sykepenger/sykepenger-ved-utenlandsopphold',
+    'om.sykepenger.lenke1.url': 'https://www.nav.no/sykepenger#reise',
     'om.sykepenger.lenke2': 'Les mer om sykepenger her.',
-    'om.sykepenger.lenke2.url':
-        'https://www.nav.no/no/nav-og-samfunn/kontakt-nav/utbetalinger/utbetalinger/utbetalingsdatoer-feriepenger-og-skattetrekk',
+    'om.sykepenger.lenke2.url': 'https://www.nav.no/sykepenger',
 }
 
 export default OmSykepengerTekster
--- src/components/om-sykepenger/om-sykepenger.tsx
+++ src/components/om-sykepenger/om-sykepenger.tsx
@@ -1,15 +1,12 @@
-import { Accordion, Alert, BodyLong, ExpansionCard, Heading, Label } from '@navikt/ds-react'
+import { BodyLong, ExpansionCard, Heading, Label } from '@navikt/ds-react'
 import React, { useState } from 'react'
 
-import { parserWithReplace } from '../../utils/html-react-parser-utils'
 import { tekst } from '../../utils/tekster'
 import { logEvent } from '../amplitude/amplitude'
 import { LenkeMedIkon } from '../lenke-med-ikon/LenkeMedIkon'
 
 const OmSykepenger = () => {
     const [open1, setOpen1] = useState<boolean>(false)
-    const [open2, setOpen2] = useState<boolean>(false)
-    const [open3, setOpen3] = useState<boolean>(false)
 
     const tittel = tekst('om.sykepenger.tittel')
     return (
@@ -32,57 +29,16 @@ const OmSykepenger = () => {
                 <Label as="h3">{tekst('om.sykepenger.hvorfor')}</Label>
                 <BodyLong spacing>{tekst('om.sykepenger.tekst3')}</BodyLong>
 
-                <Accordion className="mb-8">
-                    <Accordion.Item open={open2}>
-                        <Accordion.Header
-                            onClick={() => {
-                                logEvent(open2 ? 'accordion lukket' : 'accordion åpnet', {
-                                    tekst: tekst('om.sykepenger.arbeidstakere.tittel'),
-                                    komponent: 'listevisning søkad - om sykepenger',
-                                })
-                                setOpen2(!open2)
-                            }}
-                        >
-                            {tekst('om.sykepenger.arbeidstakere.tittel')}
-                        </Accordion.Header>
-                        <Accordion.Content>
-                            <BodyLong spacing>{tekst('om.sykepenger.arbeidstakere.tekst1')}</BodyLong>
-                            <BodyLong spacing>{tekst('om.sykepenger.arbeidstakere.tekst2')}</BodyLong>
-                            <Alert variant="info">{tekst('om.sykepenger.arbeidstakere.alertstripe')}</Alert>
-                        </Accordion.Content>
-                    </Accordion.Item>
-
-                    <Accordion.Item open={open3}>
-                        <Accordion.Header
-                            onClick={() => {
-                                logEvent(open3 ? 'accordion lukket' : 'accordion åpnet', {
-                                    tekst: tekst('om.sykepenger.selvstendige.tittel'),
-                                    komponent: 'listevisning søkad - om sykepenger',
-                                })
-                                setOpen3(!open3)
-                            }}
-                        >
-                            {tekst('om.sykepenger.selvstendige.tittel')}
-                        </Accordion.Header>
-                        <Accordion.Content>
-                            <BodyLong spacing>{parserWithReplace(tekst('om.sykepenger.selvstendige.tekst1'))}</BodyLong>
-                            <BodyLong spacing>{tekst('om.sykepenger.selvstendige.tekst2')}</BodyLong>
-                            <BodyLong spacing>{tekst('om.sykepenger.selvstendige.tekst3')}</BodyLong>
-                            <Label as="h3">{tekst('om.sykepenger.selvstendige.husk')}</Label>
-                            <BodyLong spacing>{parserWithReplace(tekst('om.sykepenger.selvstendige.tekst4'))}</BodyLong>
-                            <Alert variant="info">
-                                {parserWithReplace(tekst('om.sykepenger.selvstendige.alertstripe'))}
-                            </Alert>
-                        </Accordion.Content>
-                    </Accordion.Item>
-                </Accordion>
-
-                <Label as="h3" className="mb-4">
+                <Label as="h3" className="mb-2">
                     {tekst('om.sykepenger.tittel2')}
                 </Label>
                 <LenkeMedIkon href={tekst('om.sykepenger.lenke1.url')} text={tekst('om.sykepenger.lenke1')} />
 
-                <LenkeMedIkon href={tekst('om.sykepenger.lenke2.url')} text={tekst('om.sykepenger.lenke2')} />
+                <LenkeMedIkon
+                    className="mt-8"
+                    href={tekst('om.sykepenger.lenke2.url')}
+                    text={tekst('om.sykepenger.lenke2')}
+                />
             </ExpansionCard.Content>
         </ExpansionCard>
     )
