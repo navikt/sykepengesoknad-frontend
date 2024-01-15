4517ab804e7accba77fc0fedb2f8074da15362b7
oivindkulsrud <oivindkulsrud@gmail.com>
2024-01-15 09:57:21 +0100

    Gjør hjørner på stor ja/nei knapp med fokus runde

--- src/components/sporsmal/typer/ja-nei-stor-style.tsx
+++ src/components/sporsmal/typer/ja-nei-stor-style.tsx
@@ -4,7 +4,7 @@ import { cn } from '../../../utils/tw-utils'
 
 export function jaNeiStorStyle(value: string, watch: any, error: boolean, mt = false) {
     return cn(
-        'focus-within:shadow-focus mb-2 block w-full [&>label]:rounded [&>label]:border-2 [&>label]:border-border-default [&>label]:px-4 [&>label]:py-4 text-text-default [&>label]:hover:bg-surface-action-subtle-hover md:mb-0 md:w-1/2',
+        'focus-within:shadow-focus mb-2 block w-full [&>label]:rounded [&>label]:border-2 [&>label]:border-border-default [&>label]:px-4 [&>label]:py-4 text-text-default [&>label]:hover:bg-surface-action-subtle-hover md:mb-0 md:w-1/2 rounded focus:outline-none focus:ring focus:ring-blue-800 focus:ring-opacity-50',
         {
             '[&>label]:bg-surface-action-subtle [&>label]:border-border-selected': watch === value,
             'mt-4': mt,
