48dc262d404f2b789d757c0aa39e3db728196903
golshidbahadorian <golshid.bahadorian@nav.no>
2024-02-21 13:15:56 +0100

    Ny tekst

--- src/components/sporsmal/bekreftelsespunkter/bekreftelsespunkter-opphold-utland-tekster.ts
+++ src/components/sporsmal/bekreftelsespunkter/bekreftelsespunkter-opphold-utland-tekster.ts
@@ -3,20 +3,23 @@ interface Tekster {
 }
 
 const BekreftelsespunkterOppholdutlandTekster = (harArbeidsgiver: boolean): Tekster => {
+    const arbeidsgiver: Tekster = harArbeidsgiver
+        ? {
+              'vaer-klar-over-at.punkt-1':
+                  'Du må avklare med arbeidsgiveren din og den som sykmeldte deg at reisen ikke hindrer planlagte aktiviteter og/eller forlenger sykefraværet ditt. Hvis reisen forlenger sykefraværet ditt eller hindrer planlagte aktiviteter, kan du risikere at sykepengene stanses i den perioden du er på reise.',
+          }
+        : {}
+
     const tekster: Tekster = {
-        'vaer-klar-over-at.punkt-1':
-            'Hvis reisen forlenger sykefraværet ditt eller hindrer planlagte aktiviteter, kan du risikere at sykepengene stanses i den perioden du er på reise.',
         'vaer-klar-over-at.punkt-2':
-            'Hvis sykepengene stanses, risikerer du også at sykepengene beregnes etter et nytt og lavere grunnlag når du er tilbake fra reisen.',
+            'Hvis reisen forlenger sykefraværet ditt eller hindrer planlagte aktiviteter, kan du risikere at sykepengene stanses i den perioden du er på reise.',
         'vaer-klar-over-at.punkt-3':
+            'Hvis sykepengene stanses, risikerer du også at sykepengene beregnes etter et nytt og lavere grunnlag når du er tilbake fra reisen.',
+        'vaer-klar-over-at.punkt-4':
             'Hvis du er på reise i 4 uker eller mer og sykepengene stanses i denne perioden, risikerer du også å få avslag på videre søknad om sykepenger.',
     }
 
-    if (harArbeidsgiver) {
-        tekster['vaer-klar-over-at.punkt-4'] = 'Du må avklare reisen med arbeidsgiveren din'
-    }
-
-    return tekster
+    return { ...arbeidsgiver, ...tekster }
 }
 
 export default BekreftelsespunkterOppholdutlandTekster
