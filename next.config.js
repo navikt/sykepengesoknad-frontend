3665869a63394b0d32c80f1de55fee9791ba995c
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-02-26 10:56:55 +0100

    Referrer-Policy: no-referrer

--- next.config.js
+++ next.config.js
@@ -26,6 +26,10 @@ const nextConfig = {
                         key: 'Content-Security-Policy',
                         value: csp,
                     },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'no-referrer',
+                    },
                 ],
             },
             {
