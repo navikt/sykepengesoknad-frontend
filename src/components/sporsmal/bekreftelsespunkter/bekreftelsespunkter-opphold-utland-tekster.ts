47bfaf25b47858f85922c30674eb0c0cceb2b299
golshidbahadorian <golshid.bahadorian@nav.no>
2024-03-04 15:07:58 +0100

    Forbedrer teksten og fjerner duplikat info

--- src/components/sporsmal/bekreftelsespunkter/bekreftelsespunkter-opphold-utland-tekster.ts
+++ src/components/sporsmal/bekreftelsespunkter/bekreftelsespunkter-opphold-utland-tekster.ts
@@ -12,10 +12,8 @@ const BekreftelsespunkterOppholdutlandTekster = (harArbeidsgiver: boolean): Teks
 
     const tekster: Tekster = {
         'vaer-klar-over-at.punkt-2':
-            'Hvis reisen forlenger sykefraværet ditt eller hindrer planlagte aktiviteter, kan du risikere at sykepengene stanses i den perioden du er på reise.',
-        'vaer-klar-over-at.punkt-3':
             'Hvis sykepengene stanses, risikerer du også at sykepengene beregnes etter et nytt og lavere grunnlag når du er tilbake fra reisen.',
-        'vaer-klar-over-at.punkt-4':
+        'vaer-klar-over-at.punkt-3':
             'Hvis du er på reise i 4 uker eller mer og sykepengene stanses i denne perioden, risikerer du også å få avslag på videre søknad om sykepenger.',
     }
 
