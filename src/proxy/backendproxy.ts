07c8b0b84dd01a8bf269d7084f16da9750fd81db
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-03-12 13:29:15 +0100

    rydder litt

--- src/proxy/backendproxy.ts
+++ src/proxy/backendproxy.ts
@@ -27,26 +27,6 @@ export async function proxyKallTilBackend(opts: Opts) {
     }
     if (!opts.req.headers.authorization) throw new Error('Mangler authorization header')
     const idportenToken = opts.req.headers.authorization.split(' ')[1]
-    // const rewritedPath = opts.req.url!.replace(`/api/${opts.backend}`, '')
-    // const api = `${opts.req.method} ${rewritedPath}`
-    // if (!opts.tillatteApier.includes(<string>cleanPathForMetric(api))) {
-    //     logger.warn('404 Ukjent api: ' + api)
-    //     opts.res.status(404)
-    //     opts.res.send(null)
-    //     return
-    // }
-    //
-    //     const idportenToken = opts.req.headers.authorization.split(' ')[1]
-
-    /*async function bearerToken(): Promise<string | undefined> {
-        if (opts.backendClientId) {
-            const idportenToken = opts.req.headers.authorization!.split(' ')[1]
-            return await getTokenxToken(idportenToken, opts.backendClientId)
-        }
-        return undefined
-    }
-
-    await proxyApiRouteRequest({ ...opts, path: rewritedPath, bearerToken: await bearerToken() })*/
 
     const tokenX = await requestOboToken(idportenToken, opts.backendClientId)
     if (!tokenX.ok) {
