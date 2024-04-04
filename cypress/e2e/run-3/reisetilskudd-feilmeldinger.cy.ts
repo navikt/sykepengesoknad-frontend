230be30ee4bc438c2c162fd231af6e5bee011aa8
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-04-04 15:38:38 +0200

    Splitter lang testfil

---
+++ cypress/e2e/run-3/reisetilskudd-feilmeldinger.cy.ts
@@ -0,0 +1,91 @@
+import { modalAktiv, modalIkkeAktiv } from '../../support/utilities'
+import { nyttReisetilskudd } from '../../../src/data/mock/data/soknad/arbeidstaker-reisetilskudd'
+
+describe('Tester feilmeldinger i reistetilskudd', () => {
+    it('URL er riktig', () => {
+        cy.clearCookies()
+
+        cy.visit(`/syk/sykepengesoknad/soknader/${nyttReisetilskudd.id}/4?testperson=reisetilskudd`)
+
+        cy.url().should('include', `/syk/sykepengesoknad/soknader/${nyttReisetilskudd.id}/4`)
+    })
+
+    it('Åpner og lukker modal', () => {
+        cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('not.exist')
+
+        cy.get('button').contains('Legg til reiseutgift').click()
+        cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('have.attr', 'open')
+        cy.get('.navds-modal').contains('Tilbake').click()
+        cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('not.exist')
+    })
+
+    it('Feilmeldinger når ingenting er valgt', () => {
+        modalIkkeAktiv()
+        cy.get('button').contains('Legg til reiseutgift').click()
+        modalAktiv()
+
+        cy.get('.navds-modal').contains('Bekreft').should('be.visible').click()
+
+        cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel').should('be.visible')
+        cy.get('[data-cy="opplasting-form"]').contains('Du må skrive inn beløp').should('be.visible')
+        cy.get('[data-cy="opplasting-form"]').contains('Du må laste opp kvittering').should('be.visible')
+        cy.get('.navds-modal').contains('Tilbake').should('be.visible').click()
+        modalIkkeAktiv()
+    })
+
+    it('Ugyldig valg', () => {
+        modalIkkeAktiv()
+        cy.get('button').should('be.visible').contains('Legg til reiseutgift').click()
+        modalAktiv()
+
+        cy.get('select[name=transportmiddel]').should('be.visible').select('')
+        cy.get('.navds-modal').contains('Bekreft').should('be.visible').click()
+        modalAktiv()
+        cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('have.attr', 'open')
+        cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel').should('be.visible')
+
+        cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel')
+        cy.get('select[name=transportmiddel]').select('PARKERING')
+        cy.get('#transportmiddel').contains('Parkering')
+        cy.get('.navds-modal').contains('Bekreft').click()
+        cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel').should('not.exist')
+    })
+
+    it('Negative beløp', () => {
+        cy.get('input[name=belop_input]').type('-100')
+        cy.get('.navds-modal').contains('Bekreft').click()
+        cy.get('[data-cy="opplasting-form"]').contains('Beløp kan ikke være negativt')
+    })
+
+    it('Høyere beløp enn maks', () => {
+        cy.get('input[name=belop_input]').clear()
+        cy.get('input[name=belop_input]').type('1000000000')
+        cy.get('.navds-modal').contains('Bekreft').click()
+        cy.get('[data-cy="opplasting-form"]').contains('Beløp kan ikke være større enn 10 000')
+    })
+
+    it('Kan ikke skrive inn med 3 desimaler', () => {
+        cy.get('input[name=belop_input]').clear()
+        cy.get('input[name=belop_input]').type('100.253')
+        cy.get('.navds-modal').contains('Bekreft').click()
+        cy.get('input[name=belop_input]')
+            .invoke('val')
+            .should((val) => {
+                expect(val).to.be.eq('100.25')
+            })
+    })
+
+    it('Gyldig beløp med 2 desimaler', () => {
+        cy.get('input[name=belop_input]').clear()
+        cy.get('input[name=belop_input]').type('100.30')
+        cy.get('.navds-modal').contains('Bekreft').click()
+        cy.get('[data-cy="opplasting-form"]').contains('Beløp kan ikke være større enn 10 000').should('not.exist')
+    })
+
+    it('Gyldig beløp uten desimaler', () => {
+        cy.get('input[name=belop_input]').clear()
+        cy.get('input[name=belop_input]').type('99')
+        cy.get('.navds-modal').contains('Bekreft').click()
+        cy.get('[data-cy="opplasting-form"]').contains('Beløp kan ikke være større enn 10 000').should('not.exist')
+    })
+})
--- cypress/e2e/run-3/reisetilskudd.cy.ts
+++ cypress/e2e/run-3/reisetilskudd.cy.ts
@@ -1,4 +1,4 @@
-import { klikkGaVidere, modalAktiv, modalIkkeAktiv, sjekkMainContentFokus } from '../../support/utilities'
+import { klikkGaVidere, sjekkMainContentFokus } from '../../support/utilities'
 import { nyttReisetilskudd } from '../../../src/data/mock/data/soknad/arbeidstaker-reisetilskudd'
 
 describe('Teste førsteside i reisetilskuddsøknaden', () => {
@@ -176,106 +176,20 @@ describe('Teste førsteside i reisetilskuddsøknaden', () => {
             cy.get('.sumlinje').should('not.exist')
         })
 
-        it('Åpner og lukker modal', () => {
-            cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('not.exist')
-
+        it('Legger inn gyldig kvittering', () => {
             cy.get('button').contains('Legg til reiseutgift').click()
             cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('have.attr', 'open')
-            cy.get('.navds-modal').contains('Tilbake').click()
-            cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('not.exist')
-        })
 
-        it('Feilmeldinger når ingenting er valgt', () => {
-            modalIkkeAktiv()
-            cy.get('button').contains('Legg til reiseutgift').click()
-            modalAktiv()
+            cy.get('[data-cy="filopplasteren"] input[type=file]').attachFile('kvittering.jpg')
+            cy.get('input[name=belop_input]').type('99')
+            cy.get('select[name=transportmiddel]').select('PARKERING')
 
-            cy.get('.navds-modal').contains('Bekreft').should('be.visible').click()
+            cy.get('button').should('be.visible').contains('Bekreft').click()
 
-            cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel').should('be.visible')
-            cy.get('[data-cy="opplasting-form"]').contains('Du må skrive inn beløp').should('be.visible')
-            cy.get('[data-cy="opplasting-form"]').contains('Du må laste opp kvittering').should('be.visible')
-            cy.get('.navds-modal').contains('Tilbake').should('be.visible').click()
-            modalIkkeAktiv()
-        })
-
-        describe('Transportmiddel feilmeldinger', () => {
-            it('Ugyldig valg', () => {
-                modalIkkeAktiv()
-                cy.get('button').should('be.visible').contains('Legg til reiseutgift').click()
-                modalAktiv()
-
-                cy.get('select[name=transportmiddel]').should('be.visible').select('')
-                cy.get('.navds-modal').contains('Bekreft').should('be.visible').click()
-                modalAktiv()
-                cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('have.attr', 'open')
-                cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel').should('be.visible')
-            })
-
-            it('Velger parkering', () => {
-                cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel')
-                cy.get('select[name=transportmiddel]').select('PARKERING')
-                cy.get('#transportmiddel').contains('Parkering')
-                cy.get('.navds-modal').contains('Bekreft').click()
-                cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel').should('not.exist')
-            })
-        })
-
-        describe('Beløp feilmeldinger', () => {
-            it('Negative beløp', () => {
-                cy.get('input[name=belop_input]').type('-100')
-                cy.get('.navds-modal').contains('Bekreft').click()
-                cy.get('[data-cy="opplasting-form"]').contains('Beløp kan ikke være negativt')
-            })
-
-            it('Høyere beløp enn maks', () => {
-                cy.get('input[name=belop_input]').clear()
-                cy.get('input[name=belop_input]').type('1000000000')
-                cy.get('.navds-modal').contains('Bekreft').click()
-                cy.get('[data-cy="opplasting-form"]').contains('Beløp kan ikke være større enn 10 000')
-            })
-
-            it('Kan ikke skrive inn med 3 desimaler', () => {
-                cy.get('input[name=belop_input]').clear()
-                cy.get('input[name=belop_input]').type('100.253')
-                cy.get('.navds-modal').contains('Bekreft').click()
-                cy.get('input[name=belop_input]')
-                    .invoke('val')
-                    .should((val) => {
-                        expect(val).to.be.eq('100.25')
-                    })
-            })
-
-            it('Gyldig beløp med 2 desimaler', () => {
-                cy.get('input[name=belop_input]').clear()
-                cy.get('input[name=belop_input]').type('100.30')
-                cy.get('.navds-modal').contains('Bekreft').click()
-                cy.get('[data-cy="opplasting-form"]')
-                    .contains('Beløp kan ikke være større enn 10 000')
-                    .should('not.exist')
-            })
-
-            it('Gyldig beløp uten desimaler', () => {
-                cy.get('input[name=belop_input]').clear()
-                cy.get('input[name=belop_input]').type('99')
-                cy.get('.navds-modal').contains('Bekreft').click()
-                cy.get('[data-cy="opplasting-form"]')
-                    .contains('Beløp kan ikke være større enn 10 000')
-                    .should('not.exist')
-            })
-        })
-
-        describe('Kvittering feilmeldinger', () => {
-            it('Legger inn gyldig kvittering', () => {
-                cy.get('[data-cy="filopplasteren"] input[type=file]').attachFile('kvittering.jpg')
-
-                cy.get('button').should('be.visible').contains('Bekreft').click()
-
-                cy.get('.navds-table').within(() => {
-                    cy.contains('Parkering')
-                    cy.contains('99 kr')
-                    cy.contains('1 utgift på til sammen')
-                })
+            cy.get('.navds-table').within(() => {
+                cy.contains('Parkering')
+                cy.contains('99 kr')
+                cy.contains('1 utgift på til sammen')
             })
         })
 
