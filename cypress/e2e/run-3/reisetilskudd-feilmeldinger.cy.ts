b6dc1f1147da938db4ad020a8817fed16313a876
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-04-04 20:37:17 +0200

    aldri-lukk

--- cypress/e2e/run-3/reisetilskudd-feilmeldinger.cy.ts
+++ cypress/e2e/run-3/reisetilskudd-feilmeldinger.cy.ts
@@ -1,4 +1,3 @@
-import { modalAktiv, modalIkkeAktiv } from '../../support/utilities'
 import { nyttReisetilskudd } from '../../../src/data/mock/data/soknad/arbeidstaker-reisetilskudd'
 
 describe('Tester feilmeldinger i reisetilskudd', () => {
@@ -8,37 +7,20 @@ describe('Tester feilmeldinger i reisetilskudd', () => {
         cy.url().should('include', `/syk/sykepengesoknad/soknader/${nyttReisetilskudd.id}/4`)
     })
 
-    it('Åpner og lukker modal', () => {
-        cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('not.exist')
-
-        cy.get('button').contains('Legg til reiseutgift').click()
-        cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('have.attr', 'open')
-        cy.get('.navds-modal').contains('Tilbake').click()
-        cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('not.exist')
-    })
-
     it('Feilmeldinger når ingenting er valgt', () => {
-        modalIkkeAktiv()
         cy.get('button').contains('Legg til reiseutgift').click()
-        modalAktiv()
 
         cy.get('.navds-modal').contains('Bekreft').should('be.visible').click()
 
         cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel').should('be.visible')
         cy.get('[data-cy="opplasting-form"]').contains('Du må skrive inn beløp').should('be.visible')
         cy.get('[data-cy="opplasting-form"]').contains('Du må laste opp kvittering').should('be.visible')
-        cy.get('.navds-modal').contains('Tilbake').should('be.visible').click()
-        modalIkkeAktiv()
     })
 
     it('Ugyldig valg', () => {
-        modalIkkeAktiv()
-        cy.get('button').should('be.visible').contains('Legg til reiseutgift').click()
-        modalAktiv()
-
         cy.get('select[name=transportmiddel]').should('be.visible').select('')
         cy.get('.navds-modal').contains('Bekreft').should('be.visible').click()
-        modalAktiv()
+
         cy.get('body').findByRole('dialog', { name: 'Legg til reiseutgift' }).should('have.attr', 'open')
         cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel').should('be.visible')
 
@@ -47,13 +29,6 @@ describe('Tester feilmeldinger i reisetilskudd', () => {
         cy.get('#transportmiddel').contains('Parkering')
         cy.get('.navds-modal').contains('Bekreft').click()
         cy.get('[data-cy="opplasting-form"]').contains('Du må velge transportmiddel').should('not.exist')
-    })
-
-    it('Reloader og åpner modal', () => {
-        cy.reload()
-        cy.url().should('include', `/syk/sykepengesoknad/soknader/${nyttReisetilskudd.id}/4`)
-        cy.contains('Legg til reiseutgift')
-        cy.get('button').should('be.visible').contains('Legg til reiseutgift').click()
     })
 
     it('Negative beløp', () => {
