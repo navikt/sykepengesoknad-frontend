bb73f4a0d021bec552ab583390de09dd0506abf6
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-04-29 13:01:40 +0200

    wip

--- src/data/mock/data/sporsmal/inntektsopplysninger.ts
+++ src/data/mock/data/sporsmal/inntektsopplysninger.ts
@@ -220,9 +220,6 @@ export const inntektsopplysningerLegacy: RSSporsmal = {
     ],
 }
 
-// Har du registrert virksomheten din som avviklet og slettet i Altinn før du ble sykmeldt?
-// https://trello.com/c/Enx57DXL
-
 export const inntektsopplysninger: RSSporsmal = {
     id: v4().toString(),
     tag: 'INNTEKTSOPPLYSNINGER_VIRKSOMHETEN_AVVIKLET',
