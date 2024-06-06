e2196409537c68cf12ec845ec42955edc4d3a3b3
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-06-06 14:32:47 +0200

    wip

--- cypress/e2e/run-2/opprett_opphold_utland.cy.ts
+++ cypress/e2e/run-2/opprett_opphold_utland.cy.ts
@@ -25,7 +25,7 @@ describe('Tester opprettelse av søknad om å beholde sykepenger utenfor EØS', 
         cy.url().should('include', `${arbeidstakersoknadMedUtenlandsoppholdSoktSykepenger.id}/2`)
 
         // Test spørsmål
-        cy.get('[data-cy="sporsmal-tittel"]').should('have.text', 'Sykepenger utenfor EØS')
+        cy.get('[data-cy="sporsmal-tittel"]').should('have.text', 'Sykepenger utenfor EU/EØS')
         cy.contains('Har du søkt om å beholde sykepengene for de dagene du var utenfor EØS?')
         cy.get('[data-cy="ja-nei-stor"] input[value=JA]').click()
 
