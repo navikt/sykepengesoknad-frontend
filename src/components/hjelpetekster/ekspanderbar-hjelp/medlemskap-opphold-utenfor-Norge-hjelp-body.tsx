4b113301ad0f4afac69f057ed88f5845a2a15dde
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-06-11 09:15:01 +0200

    Endre tekster til EU/EØS eller Sveits

--- src/components/hjelpetekster/ekspanderbar-hjelp/medlemskap-opphold-utenfor-Norge-hjelp-body.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/medlemskap-opphold-utenfor-Norge-hjelp-body.tsx
@@ -4,12 +4,10 @@ export const MedlemskapOppholdUtenforNorgeHjelpBody = () => {
     return (
         <>
             <BodyShort spacing>Dette spørsmålet gjelder opphold i utlandet hvor du ikke har arbeidet.</BodyShort>
+            <BodyShort spacing>Svar nei, hvis oppholdet var kortere enn 5 uker de siste 12 månedene.</BodyShort>
             <BodyShort spacing>
-                Svar nei, hvis du har oppholdt deg i utlandet i mindre enn 5 uker de siste 12 månedene
-            </BodyShort>
-            <BodyShort spacing>
-                Svar nei, hvis utenlandsperiodene er de samme som de du har oppgitt i forrige spørsmål (arbeidsperioder
-                i utlandet).
+                Svar nei, hvis oppholdet var de samme periodene som du har oppgitt i forrige spørsmål (arbeidsperioder i
+                utlandet).
             </BodyShort>
         </>
     )
--- src/components/hjelpetekster/ekspanderbar-hjelp/medlemskap-opphold-utenfor-eos-hjelp-body.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/medlemskap-opphold-utenfor-eos-hjelp-body.tsx
@@ -3,14 +3,13 @@ import { BodyShort } from '@navikt/ds-react'
 export const MedlemskapOppholdUtenforEOSHjelpBody = () => {
     return (
         <>
-            <BodyShort spacing>Med «utenfor EU/EØS» mener vi i land som er utenfor EU/EØS og Sveits.</BodyShort>
             <BodyShort spacing>
-                Dette spørsmålet gjelder opphold utenfor EU/EØS-området hvor du ikke har arbeidet.
+                Dette spørsmålet gjelder opphold utenfor EU/EØS eller Sveits hvor du ikke har arbeidet.
             </BodyShort>
-            <BodyShort spacing>Svar nei, hvis du har oppholdt deg mindre enn 5 uker de siste 12 månedene.</BodyShort>
+            <BodyShort spacing>Svar nei, hvis oppholdet var kortere enn 5 uker de siste 12 månedene.</BodyShort>
             <BodyShort spacing>
-                Svar nei, hvis utenlandsperiodene er de samme som de du har oppgitt i forrige spørsmål (arbeidsperioder
-                i utlandet).
+                Svar nei, hvis oppholdet var de samme periodene som du har oppgitt i forrige spørsmål (arbeidsperioder i
+                utlandet).
             </BodyShort>
         </>
     )
