b6c375b46c6ca58af0a66bb0d6f1a1161170f473
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-06-12 15:35:39 +0200

    Lag mockSøknader med kjentOppholdstillatelse

--- src/data/mock/data/personas/medlemskap.ts
+++ src/data/mock/data/personas/medlemskap.ts
@@ -10,52 +10,69 @@ import {
 import { Persona } from './personas'
 import { brukertestSoknad, brukertestSykmelding } from './brukertest'
 
-const medlemskapSoknad = deepcopyMedNyId(brukertestSoknad, '7fdc72b9-30a9-435c-9eb1-f7cc68a8b429')
-const oppholdUtlandSpm = medlemskapSoknad.sporsmal.find(
+const medlemskapOriginalOppholdstillatelse = deepcopyMedNyId(brukertestSoknad, '7fdc72b9-30a9-435c-9eb1-f7cc68a8b429')
+const oppholdUtlandSpm = medlemskapOriginalOppholdstillatelse.sporsmal.find(
     (spm) => spm.tag === 'UTLAND_V2' || spm.tag === 'OPPHOLD_UTENFOR_EOS',
 )
 if (!oppholdUtlandSpm) {
     throw new Error('Søknad mangler spørsmål UTLAND_V2 eller OPPHOLD_UTENFOR_EOS')
 }
-medlemskapSoknad.sporsmal = medlemskapSoknad.sporsmal
+medlemskapOriginalOppholdstillatelse.sporsmal = medlemskapOriginalOppholdstillatelse.sporsmal
     // ARBEID_UTENFOR_NORGE stilles ikke når det stilles spørsmål om medlemskap.
     .filter((spm) => spm.tag !== 'ARBEID_UTENFOR_NORGE')
     // Fjerner spørsmålet sånn at det kan settes inn etter medlemskapspørsmål om opphold utenfor EØS.
     .filter((spm) => spm.tag !== 'UTLAND_V2')
     .filter((spm) => spm.tag !== 'OPPHOLD_UTENFOR_EOS')
-const splittSted = medlemskapSoknad.sporsmal.findIndex((spm) => spm.tag === 'ANDRE_INNTEKTSKILDER_V2')
+const splittSted = medlemskapOriginalOppholdstillatelse.sporsmal.findIndex(
+    (spm) => spm.tag === 'ANDRE_INNTEKTSKILDER_V2',
+)
 if (splittSted === -1) {
     throw new Error('Søknad mangler spørsmål ANDRE_INNTEKTSKILDER_V2')
 }
-medlemskapSoknad.sporsmal = [
-    ...medlemskapSoknad.sporsmal.slice(0, splittSted + 1),
+medlemskapOriginalOppholdstillatelse.sporsmal = [
+    ...medlemskapOriginalOppholdstillatelse.sporsmal.slice(0, splittSted + 1),
     medlemskapUtførtArbeidUtenforNorgeSporsmal,
     medlemskapOppholdUtenforNorgeSporsmal,
     medlemskapOppholdUtenforEøsSporsmal,
     oppholdUtlandSpm,
     medlemskapOppholdstillatelseSporsmal,
-    ...medlemskapSoknad.sporsmal.slice(splittSted + 1),
+    ...medlemskapOriginalOppholdstillatelse.sporsmal.slice(splittSted + 1),
 ]
 
-const medlemskapSoknadOppholdstillatelseV2 = deepcopyMedNyId(medlemskapSoknad, '15035122-1f22-4231-be4b-c9e46131d53d')
+const medlemskapOppholdstillatelsePermanent = deepcopyMedNyId(
+    medlemskapOriginalOppholdstillatelse,
+    '15035122-1f22-4231-be4b-c9e46131d53d',
+)
 
-const oppholdstillatelseIndex = medlemskapSoknadOppholdstillatelseV2.sporsmal.findIndex(
+const oppholdstillatelseIndex = medlemskapOppholdstillatelsePermanent.sporsmal.findIndex(
     (spm) => spm.tag === 'MEDLEMSKAP_OPPHOLDSTILLATELSE',
 )
 
-medlemskapSoknadOppholdstillatelseV2.sporsmal = medlemskapSoknadOppholdstillatelseV2.sporsmal.filter(
+medlemskapOppholdstillatelsePermanent.sporsmal = medlemskapOppholdstillatelsePermanent.sporsmal.filter(
     (spm) => spm.tag !== 'MEDLEMSKAP_OPPHOLDSTILLATELSE',
 )
 
 // Setter inn nytt spørsmål om oppholdstllatelse der hvor det gamle var.
-medlemskapSoknadOppholdstillatelseV2.sporsmal = [
-    ...medlemskapSoknadOppholdstillatelseV2.sporsmal.slice(0, oppholdstillatelseIndex),
+medlemskapOppholdstillatelsePermanent.sporsmal = [
+    ...medlemskapOppholdstillatelsePermanent.sporsmal.slice(0, oppholdstillatelseIndex),
     medlemskapOppholdstillatelseV2Sporsmal,
-    ...medlemskapSoknadOppholdstillatelseV2.sporsmal.slice(oppholdstillatelseIndex),
+    ...medlemskapOppholdstillatelsePermanent.sporsmal.slice(oppholdstillatelseIndex),
 ]
 
+const medlemskapOppholdstillatelseMidlertidig = deepcopyMedNyId(
+    medlemskapOppholdstillatelsePermanent,
+    'b3c6b4f5-0c6b-4d0c-9c7e-1d5c9d9c1e2e',
+)
+
+medlemskapOppholdstillatelsePermanent.kjentOppholdstillatelse = { fom: '2024-05-01' }
+medlemskapOppholdstillatelseMidlertidig.kjentOppholdstillatelse = { fom: '2024-05-01', tom: '2024-05-31' }
+
 export const medlemskapPerson: Persona = {
-    soknader: [medlemskapSoknad, medlemskapSoknadOppholdstillatelseV2],
+    soknader: [
+        medlemskapOriginalOppholdstillatelse,
+        medlemskapOppholdstillatelsePermanent,
+        medlemskapOppholdstillatelseMidlertidig,
+    ],
     sykmeldinger: [brukertestSykmelding],
     kontonummer: '12340000000',
     beskrivelse: 'Person med søknader med forskjellig spørsmål om medlemskap',
