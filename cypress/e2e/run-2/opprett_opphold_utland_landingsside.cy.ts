3150a8b5e1561e2e7da22f3de5e8ad277e36e325
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-06-26 15:06:48 +0200

    tester landingssiden

--- cypress/e2e/run-2/opprett_opphold_utland_landingsside.cy.ts
+++ cypress/e2e/run-2/opprett_opphold_utland_landingsside.cy.ts
@@ -1,17 +1,16 @@
-
 describe('Tester opprettelse av søknad om å beholde sykepenger utenfor EØS', () => {
-    before(() => {
-        cy.visit('/syk/sykepengesoknad?testperson=integrasjon-soknader')
-    })
-
+    // before(() => {
+    //     cy.visit('/syk/sykepengesoknad?testperson=integrasjon-soknader');
+    // });
 
     it('Søknad ANSVARSERKLARING - steg 1', function () {
-        cy.visit('/syk/sykepengesoknad/sykepengesoknad-utland')
-        cy.contains('Søknad om å beholde sykepenger utenfor EU/EØS')
-        cy.contains('Du trenger ikke søke hvis du enten')
-        cy.contains('Har du allerede vært på reise?')
-        cy.contains('Er du statsborger i et land utenfor EU/EØS?')
-        cy.contains('Fortsett til søknaden')
-    })
+        cy.visit('/syk/sykepengesoknad/sykepengesoknad-utland');
 
-})
+        // Check for main elements using React Testing Library selectors
+        cy.findByRole('heading', { level: 1, name: "Søknad om å beholde sykepenger utenfor EU/EØS" }).should('exist');
+        cy.findByText("Du trenger ikke søke hvis du enten").should('exist');
+        cy.findByRole('heading', { level: 2, name: "Har du allerede vært på reise?" }).should('exist');
+        cy.findByRole('heading', { level: 3, name: "Er du statsborger i et land utenfor EU/EØS?" }).should('exist');
+        cy.findByRole('button', { name: "Fortsett til søknaden" }).should('exist').click();
+    });
+});
