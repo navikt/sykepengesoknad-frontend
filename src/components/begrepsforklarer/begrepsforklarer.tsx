193dec07b875b81250ea941131b70a7fd44fd1bf
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-07-16 14:31:44 +0200

    Dialog kan ikke være child av p

--- src/components/begrepsforklarer/begrepsforklarer.tsx
+++ src/components/begrepsforklarer/begrepsforklarer.tsx
@@ -1,36 +1,45 @@
 import { BodyShort, Modal } from '@navikt/ds-react'
-import React, { useState } from 'react'
+import React from 'react'
 
 import { ModalFooterMedLukk } from '../modal-footer-med-lukk'
 
-export interface BegrepsforklarerProps {
+export const Begrepsforklarer = ({
+    inlinetekst,
+    setOpen,
+}: {
     inlinetekst: string
-    tittel: string
-    children: React.ReactNode
+    setOpen: (open: boolean) => void
+}) => {
+    return (
+        <BodyShort
+            as="button"
+            type="button"
+            className="border-b-2 border-dotted border-border-action hover:bg-gray-100"
+            onClick={(e) => {
+                e.preventDefault()
+                setOpen(true)
+            }}
+        >
+            {inlinetekst}
+        </BodyShort>
+    )
 }
 
-export const Begrepsforklarer = (props: BegrepsforklarerProps) => {
-    const { inlinetekst, tittel, children } = props
-    const [open, setOpen] = useState(false)
-
+export const BegrepsforklarerModal = ({
+    tittel,
+    setOpen,
+    children,
+    open,
+}: {
+    tittel: string
+    children: React.ReactNode
+    open: boolean
+    setOpen: (open: boolean) => void
+}) => {
     return (
-        <>
-            <BodyShort
-                as="button"
-                type="button"
-                className="border-b-2 border-dotted border-border-action hover:bg-gray-100"
-                onClick={(e) => {
-                    e.preventDefault()
-                    setOpen(true)
-                }}
-            >
-                {inlinetekst}
-            </BodyShort>
-
-            <Modal open={open} header={{ heading: tittel, closeButton: false }} onClose={() => setOpen(false)}>
-                <Modal.Body>{children}</Modal.Body>
-                <ModalFooterMedLukk setOpen={setOpen} />
-            </Modal>
-        </>
+        <Modal open={open} header={{ heading: tittel, closeButton: false }} onClose={() => setOpen(false)}>
+            <Modal.Body>{children}</Modal.Body>
+            <ModalFooterMedLukk setOpen={setOpen} />
+        </Modal>
     )
 }
--- src/components/kvittering/innhold/arbeidstaker/over16dager.tsx
+++ src/components/kvittering/innhold/arbeidstaker/over16dager.tsx
@@ -5,7 +5,7 @@ import { tekst } from '../../../../utils/tekster'
 import Kontonummer from '../../kontonummer/kontonummer'
 import { LenkeMedIkon } from '../../../lenke-med-ikon/LenkeMedIkon'
 import { KvitteringTekster } from '../../kvittering-tekster'
-import { Begrepsforklarer } from '../../../begrepsforklarer/begrepsforklarer'
+import { Begrepsforklarer, BegrepsforklarerModal } from '../../../begrepsforklarer/begrepsforklarer'
 import Vis from '../../../vis'
 
 import UtbetalingAvPenger from './gjentagende-segmenter/UtbetalingAvPenger'
@@ -15,78 +15,82 @@ interface gradertReisetilskuddProps {
 }
 
 const Over16dager = ({ erGradert }: gradertReisetilskuddProps) => {
+    const [sekstenDagerOpen, set16dagerOpen] = React.useState(false)
+    const [inntektsmeldingOpen, setInntektsmeldingOpen] = React.useState(false)
+
     const begrepsForklaringInntektsmelding = (
-        <Begrepsforklarer inlinetekst="inntektsmelding" tittel={tekst('kvittering.hva-er-inntektsmelding')}>
-            <BodyLong>{tekst('kvittering.arbeidstaker.over16.inntektsmelding.brodtekst')}</BodyLong>
-        </Begrepsforklarer>
+        <Begrepsforklarer inlinetekst="inntektsmelding" setOpen={setInntektsmeldingOpen} />
     )
-
     const begrepsForklaring16Kalenderdager = (
-        <Begrepsforklarer
-            inlinetekst="16 kalenderdager"
-            tittel={tekst('kvittering.arbeidstaker.hvorfor-skille-ved-16-dager')}
-        >
-            <BodyLong>{tekst('kvittering.arbeidsgiveren-skal-betale')}</BodyLong>
-        </Begrepsforklarer>
+        <Begrepsforklarer inlinetekst="16 kalenderdager" setOpen={set16dagerOpen} />
     )
-
-    const begrepsForklaring16KalenderdagerGradertReisetilskudd = (
-        <Begrepsforklarer
-            inlinetekst="16 kalenderdager"
-            tittel={tekst('kvittering.arbeidstaker.hvorfor-skille-ved-16-dager')}
-        >
-            <BodyLong>{tekst('kvittering.arbeidsgiveren-skal-betale-gradertreisetilskudd')}</BodyLong>
-        </Begrepsforklarer>
-    )
-
     return (
-        <div className="mt-4">
-            <div>
-                <Label as="h2" spacing>
-                    {KvitteringTekster['kvittering.før.nav.behandler']}
-                </Label>
-                <Vis
-                    hvis={erGradert}
-                    render={() => (
-                        <BodyShort>
-                            Når sykefraværet ditt er lengre enn {begrepsForklaring16KalenderdagerGradertReisetilskudd},
-                            betyr det at du får sykepenger og reisetilskudd utbetalt av NAV. Noen arbeidsplasser
-                            fortsetter å utbetale sykepenger og reisetilskudd fra dag 17, men da får de penger tilbake
-                            fra NAV senere. Arbeidsgiveren din må derfor sende oss {begrepsForklaringInntektsmelding}så
-                            fort som mulig.
-                        </BodyShort>
-                    )}
-                />
-                <Vis
-                    hvis={!erGradert}
-                    render={() => (
-                        <BodyShort>
-                            Når sykefraværet ditt er lengre enn {begrepsForklaring16Kalenderdager}, betyr det at du får
-                            sykepenger utbetalt av NAV. Noen arbeidsplasser fortsetter å utbetale sykepenger fra dag 17,
-                            men da får de penger tilbake fra NAV senere. Arbeidsgiveren din må derfor sende oss{' '}
-                            {begrepsForklaringInntektsmelding} så fort som mulig.
-                        </BodyShort>
-                    )}
-                />
+        <>
+            <div className="mt-4">
+                <div>
+                    <Label as="h2" spacing>
+                        {KvitteringTekster['kvittering.før.nav.behandler']}
+                    </Label>
+                    <Vis
+                        hvis={erGradert}
+                        render={() => (
+                            <BodyShort>
+                                Når sykefraværet ditt er lengre enn {begrepsForklaring16Kalenderdager}, betyr det at du
+                                får sykepenger og reisetilskudd utbetalt av NAV. Noen arbeidsplasser fortsetter å
+                                utbetale sykepenger og reisetilskudd fra dag 17, men da får de penger tilbake fra NAV
+                                senere. Arbeidsgiveren din må derfor sende oss {begrepsForklaringInntektsmelding}så fort
+                                som mulig.
+                            </BodyShort>
+                        )}
+                    />
+                    <Vis
+                        hvis={!erGradert}
+                        render={() => (
+                            <BodyShort>
+                                Når sykefraværet ditt er lengre enn {begrepsForklaring16Kalenderdager}, betyr det at du
+                                får sykepenger utbetalt av NAV. Noen arbeidsplasser fortsetter å utbetale sykepenger fra
+                                dag 17, men da får de penger tilbake fra NAV senere. Arbeidsgiveren din må derfor sende
+                                oss {begrepsForklaringInntektsmelding} så fort som mulig.
+                            </BodyShort>
+                        )}
+                    />
+                </div>
+
+                <div className="mt-8">
+                    <Label as="h2" spacing>
+                        {tekst('kvittering.nav-behandler-soknaden')}
+                    </Label>
+                    <BodyShort as="span">{tekst('kvittering.arbeidstaker.saksbehandlingstid')} </BodyShort>
+                    <LenkeMedIkon
+                        href={tekst('kvittering.arbeidstaker.saksbehandlingstid.lenke.url')}
+                        text={tekst('kvittering.arbeidstaker.saksbehandlingstid.lenke')}
+                    />
+                    .
+                </div>
+
+                <UtbetalingAvPenger />
+
+                <Kontonummer />
             </div>
-
-            <div className="mt-8">
-                <Label as="h2" spacing>
-                    {tekst('kvittering.nav-behandler-soknaden')}
-                </Label>
-                <BodyShort as="span">{tekst('kvittering.arbeidstaker.saksbehandlingstid')} </BodyShort>
-                <LenkeMedIkon
-                    href={tekst('kvittering.arbeidstaker.saksbehandlingstid.lenke.url')}
-                    text={tekst('kvittering.arbeidstaker.saksbehandlingstid.lenke')}
-                />
-                .
-            </div>
-
-            <UtbetalingAvPenger />
-
-            <Kontonummer />
-        </div>
+            <BegrepsforklarerModal
+                tittel={tekst('kvittering.hva-er-inntektsmelding')}
+                open={inntektsmeldingOpen}
+                setOpen={setInntektsmeldingOpen}
+            >
+                <BodyLong>{tekst('kvittering.arbeidstaker.over16.inntektsmelding.brodtekst')}</BodyLong>
+            </BegrepsforklarerModal>
+            <BegrepsforklarerModal
+                tittel={tekst('kvittering.arbeidstaker.hvorfor-skille-ved-16-dager')}
+                open={sekstenDagerOpen}
+                setOpen={set16dagerOpen}
+            >
+                {erGradert && (
+                    <BodyLong>{tekst('kvittering.arbeidsgiveren-skal-betale-gradertreisetilskudd')}</BodyLong>
+                )}
+                {!erGradert && <BodyLong>{tekst('kvittering.arbeidsgiveren-skal-betale')}</BodyLong>}
+                <BodyLong>{tekst('kvittering.arbeidstaker.over16.inntektsmelding.brodtekst')}</BodyLong>
+            </BegrepsforklarerModal>
+        </>
     )
 }
-
 export default Over16dager
