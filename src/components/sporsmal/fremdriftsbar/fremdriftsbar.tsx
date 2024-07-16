e3432a281a9f3be9ea826b0e3c1fedec6ddfb5f8
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-07-16 15:21:53 +0200

    form progress

--- src/components/sporsmal/fremdriftsbar/fremdriftsbar.tsx
+++ src/components/sporsmal/fremdriftsbar/fremdriftsbar.tsx
@@ -1,13 +1,19 @@
-import { BodyShort, Skeleton } from '@navikt/ds-react'
+import { FormProgress } from '@navikt/ds-react'
 import React from 'react'
+import { useRouter } from 'next/router'
 
 import { RSSoknadstype } from '../../../types/rs-types/rs-soknadstype'
 import { useSoknadMedDetaljer } from '../../../hooks/useSoknadMedDetaljer'
+import { tekst } from '../../../utils/tekster'
+import { SEPARATOR } from '../../../utils/constants'
+import { useTestpersonQuery } from '../../../hooks/useTestpersonQuery'
 
 const Fremdriftsbar = () => {
     const { valgtSoknad, stegNo } = useSoknadMedDetaljer()
 
     const oppholdUtland = valgtSoknad?.soknadstype == RSSoknadstype.OPPHOLD_UTLAND
+    const router = useRouter()
+    const testperson = useTestpersonQuery()
 
     const aktivtSteg = oppholdUtland ? stegNo : stegNo - 1
     if (valgtSoknad && aktivtSteg == 0) return null
@@ -21,34 +27,63 @@ const Fremdriftsbar = () => {
         valgtSoknad?.sporsmal.filter((s) => s.tag !== 'VAER_KLAR_OVER_AT' && s.tag !== 'TIL_SLUTT').length || 9
     const antallSteg = oppholdUtland ? antallSporsmål + 1 : antallSporsmål
 
-    const bredde = valgtSoknad ? (100 / antallSteg) * aktivtSteg : 0
-
-    const valueText = aktivtSteg && antallSteg ? `${aktivtSteg} av ${antallSteg} steg` : ''
+    const stegene =
+        valgtSoknad?.sporsmal
+            .filter((s) => !['VAER_KLAR_OVER_AT', 'ANSVARSERKLARING'].includes(s.tag))
+            .map((s) => s.tag) || []
 
     return (
-        <div
-            className="my-4 md:my-6"
-            role="progressbar"
-            aria-valuenow={aktivtSteg}
-            aria-valuemin={1}
-            aria-valuemax={antallSteg}
-            aria-valuetext={valueText}
-            aria-label="Søknadssteg"
-        >
-            <div className="relative mx-auto mt-4">
-                <div className="h-3 rounded-lg bg-gray-200" />
-                <div
-                    className="-mt-3 h-3 rounded-lg bg-gray-900"
-                    style={{
-                        width: `${bredde}%`,
-                    }}
-                />
-            </div>
-            <div className="mt-4 flex justify-between">
-                <BodyShort as={valgtSoknad ? 'span' : Skeleton}>{valueText}</BodyShort>
-            </div>
-        </div>
+        <FormProgress totalSteps={antallSteg} activeStep={aktivtSteg}>
+            {stegene.map((tag, i) => {
+                const nokkel = tag.toLowerCase()
+                const tittel = tekst(`sykepengesoknad.${nokkel}.tittel` as any)
+                const completed = i < aktivtSteg - 1
+                const url = `/soknader/${valgtSoknad?.id}${SEPARATOR}${i + 2}${testperson.query()}`
+                return (
+                    <FormProgress.Step
+                        onClick={(e) => {
+                            if (completed) {
+                                e.preventDefault()
+                                router.push(url)
+                            }
+                        }}
+                        href={'/syk/sykepengesoknad' + url}
+                        key={i + ' ' + tag}
+                        completed={completed}
+                        interactive={completed}
+                    >
+                        {tittel}
+                    </FormProgress.Step>
+                )
+            })}
+        </FormProgress>
     )
+
+    /*
+        return (
+            <div
+                className="my-4 md:my-6"
+                role="progressbar"
+                aria-valuenow={aktivtSteg}
+                aria-valuemin={1}
+                aria-valuemax={antallSteg}
+                aria-valuetext={valueText}
+                aria-label="Søknadssteg"
+            >
+                <div className="relative mx-auto mt-4">
+                    <div className="h-3 rounded-lg bg-gray-200" />
+                    <div
+                        className="-mt-3 h-3 rounded-lg bg-gray-900"
+                        style={{
+                            width: `${bredde}%`,
+                        }}
+                    />
+                </div>
+                <div className="mt-4 flex justify-between">
+                    <BodyShort as={valgtSoknad ? 'span' : Skeleton}>{valueText}</BodyShort>
+                </div>
+            </div>
+        )*/
 }
 
 export default Fremdriftsbar
