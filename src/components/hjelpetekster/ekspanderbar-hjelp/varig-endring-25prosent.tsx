101c59f20b81ef4609bf2e7388fcf94212a21cf3
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-08-29 18:21:19 +0200

    Vis forskjellig innhold i ekspanderbar hjelp basert på inntekt metadata

--- src/components/hjelpetekster/ekspanderbar-hjelp/varig-endring-25prosent.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/varig-endring-25prosent.tsx
@@ -11,18 +11,22 @@ interface VarigEndring25prosentProps {
 
 export const VarigEndring25prosent = ({ sporsmal }: VarigEndring25prosentProps) => {
     const inntektMetadata = hentInntektMetadata(sporsmal)
-    return (
-        <>
-            <BodyShort spacing>Vi henter inntekten din fra Skatteetaten, og bruker ferdig lignet årsinntekt.</BodyShort>
-            {inntektMetadata && (
-                <>
-                    <BodyShort spacing>
-                        Så regnes den gjennomsnittelige årsinntekten ut basert på de tre siste årene, slik at din
-                        gjennomsnittelige årsinntekt blir{' '}
-                        <strong>{formatterTall(inntektMetadata.beregnet.snitt)}</strong> kroner.
-                    </BodyShort>
-                </>
-            )}
-        </>
-    )
+
+    if (inntektMetadata) {
+        return (
+            <>
+                <BodyShort spacing>
+                    Vi henter inntekten din fra Skatteetaten, og bruker ferdig lignet årsinntekt.
+                </BodyShort>
+
+                <BodyShort spacing>
+                    Så regnes den gjennomsnittelige årsinntekten ut basert på de tre siste årene, slik at din
+                    gjennomsnittelige årsinntekt blir <strong>{formatterTall(inntektMetadata.beregnet.snitt)}</strong>{' '}
+                    kroner.
+                </BodyShort>
+            </>
+        )
+    } else {
+        return <></>
+    }
 }
