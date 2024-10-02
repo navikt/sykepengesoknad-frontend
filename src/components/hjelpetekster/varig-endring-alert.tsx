e3c0043e8cd3793a0ca4e6bb481961287a910504
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-10-02 10:49:45 +0200

    Tekstendring hjelpetekst varig endring (#2833)

--- src/components/hjelpetekster/varig-endring-alert.tsx
+++ src/components/hjelpetekster/varig-endring-alert.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 export function VarigEndringAlert() {
     return (
         <Alert variant="info" className="my-4">
-            <BodyShort>Etter du har sendt inn søknaden trenger vi dokumentasjon på den varige endringen din.</BodyShort>
+            <BodyShort>
+                Etter du har sendt inn søknaden trenger vi dokumentasjon på inntekten din etter den varige endringen.
+            </BodyShort>
         </Alert>
     )
 }
