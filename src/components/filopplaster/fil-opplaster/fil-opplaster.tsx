db6f55084bc0a17dbe07a73539d4de6695b4611d
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-10-07 09:35:46 +0200

    noen endringer etter pr review

--- src/components/filopplaster/fil-opplaster/fil-opplaster.tsx
+++ src/components/filopplaster/fil-opplaster/fil-opplaster.tsx
@@ -1,8 +1,7 @@
-import { BodyShort, FileObject, UNSAFE_FileUpload, VStack } from '@navikt/ds-react'
+import { BodyShort, FileObject, UNSAFE_FileUpload } from '@navikt/ds-react'
 import React, { Dispatch, SetStateAction } from 'react'
 import { useFormContext } from 'react-hook-form'
 
-import Vis from '../../vis'
 import { tekst } from '../../../utils/tekster'
 
 export interface FilOpplasterProps {
@@ -16,7 +15,7 @@ const FilOpplaster = ({ valgtFil, setValgtFil }: FilOpplasterProps) => {
     } = useFormContext()
 
     return (
-        <VStack gap="6" data-cy="filopplasteren">
+        <div className="mb6" data-cy="filopplasteren">
             <UNSAFE_FileUpload.Dropzone
                 accept=".png,.jpeg,.jpg"
                 label={tekst('drag_and_drop.label')}
@@ -35,20 +34,17 @@ const FilOpplaster = ({ valgtFil, setValgtFil }: FilOpplasterProps) => {
                 />
             ))}
             <div role="alert" aria-live="assertive">
-                <Vis
-                    hvis={errors.fil_input}
-                    render={() => (
-                        <BodyShort
-                            as="span"
-                            className="mt-2 flex gap-2 font-bold text-surface-danger before:content-['•']"
-                            data-cy="feil-lokal"
-                        >
-                            <>{errors.fil_input?.message}</>
-                        </BodyShort>
-                    )}
-                />
+                {errors.fil_input && (
+                    <BodyShort
+                        as="span"
+                        className="mt-2 flex gap-2 font-bold text-surface-danger before:content-['•']"
+                        data-cy="feil-lokal"
+                    >
+                        <>{errors.fil_input?.message}</>
+                    </BodyShort>
+                )}
             </div>
-        </VStack>
+        </div>
     )
 }
 
