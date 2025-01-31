cc20e86883bad6fa492193c6676b9f478d0bd6a3
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-01-31 08:52:28 +0100

    Is ready og isalive mot idporten

---
+++ src/utils/checkIdportenJwks.ts
@@ -0,0 +1,24 @@
+import { logger } from '@navikt/next-logger'
+
+export const checkJwks = async (): Promise<boolean> => {
+    const jwksUri = process.env.IDPORTEN_JWKS_URI
+
+    if (!jwksUri) {
+        logger.error('Missing IDPORTEN_JWKS_URI environment variable')
+        return false
+    }
+
+    try {
+        const response = await fetch(jwksUri, { method: 'GET' })
+
+        if (response.ok) {
+            return true
+        } else {
+            logger.error(`Failed to contact ${jwksUri}: ${response.status} ${response.statusText}`)
+            return false
+        }
+    } catch (error) {
+        logger.error(`Error connecting to ${jwksUri}: ${JSON.stringify(error)}`)
+        return false
+    }
+}
