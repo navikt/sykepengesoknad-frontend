b1378e0647d6b87dd561b06fdca8b122e86ed92a
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-02-14 12:14:51 +0100

    Jobbsitasjonen din readmore

---
+++ src/components/hjelpetekster/ekspanderbar-hjelp/jobbsituasjonen-din.tsx
@@ -0,0 +1,12 @@
+import { BodyLong } from '@navikt/ds-react'
+
+export const JobbsituasjonenDin = () => {
+    return (
+        <>
+            <BodyLong spacing>
+                Svar ja hvis du har begynt i ny jobb, uansett om du fortsatt søker andre jobber eller ikke.
+            </BodyLong>
+            <BodyLong>Svar nei hvis du har fått ny jobb, men ikke hatt din første arbeidsdag enda.</BodyLong>
+        </>
+    )
+}
