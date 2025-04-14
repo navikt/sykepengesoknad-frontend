11ca93175c863f18b0892389a29a235d73cbedb3
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-04-14 08:23:37 +0200

    Fiks tester

--- playwright/friskmeldt-til-arbeidsformidling.spec.ts
+++ playwright/friskmeldt-til-arbeidsformidling.spec.ts
@@ -34,12 +34,7 @@ test.describe('Friskmeldt til arbeidsformidling', () => {
         await harSynligTittel(page, 'Jobbsituasjonen din', 2)
         await svarRadioGruppe(page, /Begynte du i ny jobb i perioden/i, 'Ja')
         await svarRadioGruppe(page, /Vil du fortsatt være friskmeldt til arbeidsformidling?/i, 'Ja')
-        await harSynligTekst(page, /Du har svart at du fortsatt vil være friskmeldt til arbeidsformidling./i)
         await page.getByRole('textbox', { name: 'Når begynte du i ny jobb?' }).fill('08.04.2025')
-        await harSynligTekst(
-            page,
-            /Da vil du også være registrert som arbeidssøker hos Nav i neste periode, altså 14. – 27. april 2025./i,
-        )
         await fullforSoknad(page)
         await harSynligTekst(page, /Du er friskmeldt til arbeidsformidling frem til 11. mai 2025./i)
     })
@@ -54,20 +49,14 @@ test.describe('Friskmeldt til arbeidsformidling', () => {
 
         await harSynligTekst(
             page,
-            /Du har svart at du har begynt i ny jobb og dermed ikke vil være friskmeldt til arbeidsformidling lenger./i,
+            /Du har svart at du har begynt i ny jobb og dermed ikke vil være friskmeldt til arbeidsformidling fra og med 8. april 2025./i,
         )
-        await harSynligTekst(
-            page,
-            /Da stanser vi sykepengene dine fra og med 8. april 2025, og fjerner deg fra arbeidssøkerregisteret vårt./i,
-        )
+        await harSynligTekst(page, /Da stanser vi sykepengene dine fra og med denne datoen./i)
         await fullforSoknad(page)
+        await harSynligTekst(page, /Du har nå sendt inn den siste søknaden for friskmeldt til arbeidsformidling./i)
         await harSynligTekst(
             page,
-            /Du vil ikke lenger være friskmeldt til arbeidsformidling fra og med 8. april 2025./i,
-        )
-        await harSynligTekst(
-            page,
-            /Da stanser vi sykepengene dine fra og med denne datoen, og fjerner deg fra arbeidssøkerregisteret vårt./i,
+            /Fra og med 8. april 2025 vil du ikke motta flere søknader og vi stanser sykepengene dine./i,
         )
     })
 
@@ -79,20 +68,17 @@ test.describe('Friskmeldt til arbeidsformidling', () => {
         await svarRadioGruppe(page, /Vil du fortsatt være friskmeldt til arbeidsformidling?/i, 'Nei')
         await page.getByRole('textbox', { name: 'Fra og med når' }).fill('08.04.2025')
 
-        await harSynligTekst(page, /Du har svart at du ikke vil være friskmeldt til arbeidsformidling lenger./i)
         await harSynligTekst(
             page,
-            /Da stanser vi sykepengene dine fra og med 8. april 2025, og fjerner deg fra arbeidssøkerregisteret vårt./i,
+            /Du har svart at du ikke vil være friskmeldt til arbeidsformidling fra og med 8. april 2025./i,
         )
+        await harSynligTekst(page, /Da stanser vi sykepengene dine fra og med denne datoen./i)
 
         await fullforSoknad(page)
+        await harSynligTekst(page, /Du har nå sendt inn den siste søknaden for friskmeldt til arbeidsformidling./i)
         await harSynligTekst(
             page,
-            /Du vil ikke lenger være friskmeldt til arbeidsformidling fra og med 8. april 2025./i,
-        )
-        await harSynligTekst(
-            page,
-            /Da stanser vi sykepengene dine fra og med denne datoen, og fjerner deg fra arbeidssøkerregisteret vårt./i,
+            /Fra og med 8. april 2025 vil du ikke motta flere søknader og vi stanser sykepengene dine./i,
         )
     })
 
@@ -102,12 +88,6 @@ test.describe('Friskmeldt til arbeidsformidling', () => {
         await harSynligTittel(page, 'Jobbsituasjonen din', 2)
         await svarRadioGruppe(page, /Begynte du i ny jobb i perioden/i, 'Nei')
         await svarRadioGruppe(page, /Vil du fortsatt være friskmeldt til arbeidsformidling?/i, 'Ja')
-
-        await harSynligTekst(page, /Du har svart at du fortsatt vil være friskmeldt til arbeidsformidling./i)
-        await harSynligTekst(
-            page,
-            /Da vil du også være registrert som arbeidssøker hos Nav i neste periode, altså 14. – 27. april 2025./i,
-        )
 
         await fullforSoknad(page)
         await harSynligTekst(page, /Du er friskmeldt til arbeidsformidling frem til 11. mai 2025./i)
@@ -124,6 +104,11 @@ test.describe('Friskmeldt til arbeidsformidling', () => {
 
         await page.getByRole('button', { name: 'Send søknaden' }).click()
         await harSynligTittel(page, 'Søknaden er sendt til NAV', 2)
+        await harSynligTekst(page, /Du har nå sendt inn den siste søknaden for friskmeldt til arbeidsformidling./i)
+        await harSynligTekst(
+            page,
+            /Fra og med 13. april 2025 vil du ikke motta flere søknader og vi stanser sykepengene dine./i,
+        )
     })
 
     async function fullforSoknad(page: Page) {
