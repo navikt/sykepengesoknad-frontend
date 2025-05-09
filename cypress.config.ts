baea9c495d52b78d2506ff7e23324801d253eb4b
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-05-09 08:37:41 +0200

    Bump cypress-axe til 1.6.0

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,11 +9,6 @@ export default defineConfig({
     retries: 0,
     defaultCommandTimeout: 10000,
     e2e: {
-        // We've imported your old cypress plugins here.
-        // You may want to clean this up later by importing these.
-        setupNodeEvents(on, config) {
-            return require('./cypress/plugins/index.js')(on, config)
-        },
         baseUrl: 'http://localhost:3000',
         testIsolation: false,
     },
