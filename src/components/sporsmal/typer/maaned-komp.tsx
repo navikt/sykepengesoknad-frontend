7c9531922defcd4d00a9adbf82baf42246c48ead
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-06-03 15:34:56 +0200

    Nytt spørsmål Varig Endring
    - Implementer ny MonthInput-komponent for MAANED-svartype
    - Legg til støtte for spørsmål om varige endringer for næringsdrivende
    - Utvid mock-data med underspørsmål for typer av endringer og endringsdato
    - Oppdater spørsmål-switch og oppsummeringslogikk for MAANED-type
    - Legg til tilhørende hjelpetekster og UI-komponenter

---
+++ src/components/sporsmal/typer/maaned-komp.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { useController } from 'react-hook-form'
+import { BodyShort, MonthPicker, MonthValidationT, useMonthpicker } from '@navikt/ds-react'
+
+import { SpmProps } from '../sporsmal-form/sporsmal-form'
+import { kalenderMedDropdownCaption } from '../sporsmal-utils'
+import { validerMaaned } from '../../../utils/sporsmal/valider-dato'
+
+function MonthInput(props: SpmProps) {
+    const { sporsmal } = props
+    const [monthValidation, setMonthValidation] = useState<MonthValidationT | undefined>(undefined)
+
+    const { field, fieldState } = useController({
+        name: sporsmal.id,
+        rules: {
+            validate: (value) => validerMaaned(sporsmal, value, monthValidation),
+        },
+    })
+
+    const { monthpickerProps, inputProps } = useMonthpicker({
+        fromDate: sporsmal.min ? new Date(sporsmal.min) : new Date('1900'),
+        toDate: sporsmal.max ? new Date(sporsmal.max) : new Date('2100'),
+        defaultYear: new Date('2024'),
+        allowTwoDigitYear: false,
+        locale: 'nb',
+        onMonthChange: field.onChange,
+        required: true,
+        onValidate: (validate) => {
+            setMonthValidation(validate)
+        },
+    })
+
+    return (
+        <div className="mt-8" data-cy="dato-komp">
+            <div className="axe-exclude">
+                <MonthPicker
+                    {...monthpickerProps}
+                    dropdownCaption={kalenderMedDropdownCaption(sporsmal.min, sporsmal.max)}
+                    data-cy-sporsmalid={sporsmal.id}
+                >
+                    <MonthPicker.Input
+                        {...inputProps}
+                        id={sporsmal.id}
+                        label={
+                            <>
+                                {sporsmal.sporsmalstekst}
+                                {sporsmal.undertekst && <BodyShort as="div"> {sporsmal.undertekst} </BodyShort>}
+                            </>
+                        }
+                        error={fieldState.error && fieldState.error.message}
+                        data-cy={sporsmal.id}
+                    />
+                </MonthPicker>
+            </div>
+        </div>
+    )
+}
+
+export default MonthInput
