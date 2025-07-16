798baccdcb3b7681e74e99cae73acc9e0e02a0d8
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-07-16 17:48:21 +0200

    WIP: Legger til komponent for sjekk av opphold utenfor EU/EØS og integrerer den i eksisterende spørsmål.

---
+++ src/components/sporsmal/opphold-utenfor-eu-eos/opphold-utenfor-eu-eos.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { SjekkLandEuEos } from './sjekk-land-eu-eos'
+
+export const OppholdUtenforEUEOS = () => {
+    return (
+        <>
+            <SjekkLandEuEos />
+        </>
+    )
+}
