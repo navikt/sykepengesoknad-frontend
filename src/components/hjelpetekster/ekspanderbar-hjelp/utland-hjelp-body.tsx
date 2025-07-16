b4dc084c396f5f543dd64eafb501f78e39330506
Ole Bastian Løchen <42940656+olebaba@users.noreply.github.com>
2025-07-16 15:35:19 +0200

    Forbedrer hjelpetekst for opphold utenfor EU/EØS i spørsmålet forklart ekspanderbar (#3487)

--- src/components/hjelpetekster/ekspanderbar-hjelp/utland-hjelp-body.tsx
+++ src/components/hjelpetekster/ekspanderbar-hjelp/utland-hjelp-body.tsx
@@ -4,17 +4,13 @@ export const UtlandHjelpBody = ({ medNei }: { medNei: boolean }) => {
     return (
         <>
             <BodyShort spacing>
-                Du kan ha rett til sykepenger under opphold utenfor EU/EØS i fire uker i løpet av en
-                tolvmånedersperiode. Dette forutsetter at du søker om det. Det er anbefalt å søke før du reiser så du er
-                sikker på at du beholder sykepengene før du reiser.
-            </BodyShort>
-            <BodyShort spacing>
-                Svar ja, dersom du har oppholdt deg utenfor EU/EØS i løpet av perioden som står i spørsmålet.
+                Svar ja, dersom du har oppholdt deg utenfor EU/EØS i løpet av perioden du var sykmeldt. Da oppretter vi
+                en egen søknad som du må sende inn.
             </BodyShort>
             {medNei && (
                 <BodyShort spacing>
-                    Svar nei, dersom du har avviklet lovbestemt ferie. Da svarer du i stedet på spørsmålet om ferie i
-                    søknaden.
+                    Svar nei hvis du var på reise fordi du hadde lovbestemt ferie eller du var på behandlingsreise i
+                    forbindelse med Oslo Universitetssykehus. Da trenger du ikke søke.
                 </BodyShort>
             )}
         </>
