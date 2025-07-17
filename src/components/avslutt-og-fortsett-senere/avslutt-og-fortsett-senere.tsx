ecbf6866a83cbe28707bd03950520844e6451096
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-07-17 17:00:05 +0200

    tilbakestiller avslutt og fortsett senere

--- src/components/avslutt-og-fortsett-senere/avslutt-og-fortsett-senere.tsx
+++ src/components/avslutt-og-fortsett-senere/avslutt-og-fortsett-senere.tsx
@@ -1,61 +1,22 @@
-import { watch } from 'node:fs'
-
 import { BodyShort, Button, Modal, Skeleton } from '@navikt/ds-react'
 import React, { useState } from 'react'
-import { Controller, useFormContext, useWatch } from 'react-hook-form'
 
 import { minSideUrl } from '../../utils/environment'
 import { tekst } from '../../utils/tekster'
 import { logEvent } from '../amplitude/amplitude'
 import { useSoknadMedDetaljer } from '../../hooks/useSoknadMedDetaljer'
 import { cn } from '../../utils/tw-utils'
-import { hentSporsmal, hentUndersporsmal } from '../../utils/soknad-utils'
-import { landlisteUtenforEøs, landlisteEøs } from '../sporsmal/landliste'
-
-// bør bruke denne: src/components/sporsmal/landliste.ts
-const euEosLand = landlisteEøs
-
-const erLandIEuEos = (land: string) => {
-    return euEosLand.includes(land.trim())
-}
 
 const AvsluttOgFortsettSenere = () => {
     const { valgtSoknad, stegId } = useSoknadMedDetaljer()
 
     const [aapen, setAapen] = useState<boolean>(false)
 
-    const { control } = useFormContext()
-
-    const landSporsmalId = hentSporsmal(valgtSoknad!, 'LAND')?.id || ''
-
-    const hvilkenLandVerdi: string[] = useWatch({
-        control,
-        name: landSporsmalId,
-    })
-
-    const alleLandIEuEos = hvilkenLandVerdi?.every((land) => erLandIEuEos(land)) && hvilkenLandVerdi.length > 0
-
-    // denne skal bli satt til 1 om vi vil ha en unik knapp for å avslutte og fortsette senere her
-    /*    if (valgtSoknad && valgtSoknad.soknadstype === 'OPPHOLD_UTLAND' && stegId == '1' && alleLandIEuEos) {
-        // For utenlandssøknader skal ikke denne knappen vises
-        return <div>Lagre søknad og fortsett senere [var her]</div>
-    }*/
-
     return (
         <>
-            {'alle land i eøs: ' + String(alleLandIEuEos)}
-            {/*   <br />
-            {JSON.stringify(hvilkenLandVerdi)}
-            <br />
-            {JSON.stringify(valgtSoknad)}
-            <br />
-            {JSON.stringify(stegId)}
-            <br />
-*/}
-
             <Button
                 className={cn('block', { '-ml-5': valgtSoknad })}
-                variant="primary"
+                variant="tertiary"
                 type="button"
                 as={valgtSoknad ? Button : Skeleton}
                 data-cy="avslutt-og-fortsett-senere"
