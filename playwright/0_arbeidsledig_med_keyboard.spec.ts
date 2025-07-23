2ae05f4f1a9dd4b3f37e11e78aeeade3a4bc26ba
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-07-23 12:55:08 +0200

    keyboard drevne testen virker, bør ryddes opp

--- playwright/0_arbeidsledig_med_keyboard.spec.ts
+++ playwright/0_arbeidsledig_med_keyboard.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect, Page } from '@playwright/test'
+import { tabUntilFocusedContainsText } from './utilities'
 
 // Utility function
 async function sjekkMainContentFokus(page: Page) {
@@ -22,6 +23,8 @@ async function elementInFocusLocator() {
     const text = await elementInFocus.textContent();
     console.log(`Element in focus text: ${text}`);
 }
+
+    let elementInFocusText = page.locator(':focus');
     await expect(page.locator('.navds-heading--large')).toBeVisible()
     await expect(page.locator('.navds-heading--large')).toHaveText('Søknader')
     await page.locator(`a[href*="${soknad.id}"]`).click()
@@ -82,8 +85,8 @@ async function elementInFocusLocator() {
     elementInFocusLocator()
     await page.keyboard.press('Tab')
     elementInFocusLocator()
-    await page.keyboard.press('Tab')
-    elementInFocusLocator()
+    // await page.keyboard.press('Tab')
+    // elementInFocusLocator()
     const focusedFriskmeldt = page.locator(':focus')
     await expect(focusedFriskmeldt).toHaveText('Gå videre')
     await expect(focusedFriskmeldt).toHaveCSS('box-shadow', /.+/)
@@ -99,28 +102,17 @@ async function elementInFocusLocator() {
     console.log('Andre inntektskilder')
     await expect(page.getByText('Hva mener vi med andre inntektskilder?')).toBeVisible()
     await page.keyboard.press('Tab')
-    elementInFocusLocator()
     await page.keyboard.press('Tab')
-    elementInFocusLocator()
     await page.keyboard.press('Tab')
-    elementInFocusLocator()
     await page.keyboard.press('Tab')
-    elementInFocusLocator()
     await page.keyboard.press('Tab')
-    elementInFocusLocator()
     await page.keyboard.press('Space')
     await page.keyboard.press('ArrowRight')
-    elementInFocusLocator()
+    elementInFocus = page.locator(':focus')
     await page.keyboard.press('Tab')
-    elementInFocusLocator()
+    elementInFocus = page.locator(':focus')
     await page.keyboard.press('Tab')
-    elementInFocusLocator()
-    await page.keyboard.press('Tab')
-    elementInFocusLocator()
-    await page.keyboard.press('Tab')
-    elementInFocusLocator()
-    await page.keyboard.press('Tab') // trying one more tab
-    elementInFocusLocator()
+    
 
     const focusedAi = page.locator(':focus')
 
@@ -145,18 +137,22 @@ async function elementInFocusLocator() {
     await page.keyboard.press('Tab')
     await page.keyboard.press('Space')
     await page.keyboard.press('ArrowRight')
-    await page.keyboard.press('Tab')
-    await page.keyboard.press('Tab')
-    await page.keyboard.press('Tab') // trying one more tab
-    const focusedReise = page.locator(':focus');
-    await expect(focusedReise).toHaveText('Send søknaden');
-    await expect(focusedReise).toHaveCSS('box-shadow', /.+/); // Checks for any box-shadow
+    tabUntilFocusedContainsText('chromium', page, 'Gå videre', { maxTabs: 10 })
+    // await page.keyboard.press('Tab')
+    // elementInFocus = page.locator(':focus')
+    // await page.keyboard.press('Tab')
+    // page.locator(':focus')
+    // const focusedReise = page.locator(':focus');
+    // await expect(focusedReise).toHaveText('Send søknaden');
+    // await expect(focusedReise).toHaveCSS('box-shadow', /.+/); // Checks for any box-shadow
     // const gaVidereButton3 = page.getByRole('button', { name: 'Gå videre' })
     // await expect(gaVidereButton3).toHaveCSS('box-shadow', /./)
+    elementInFocus = page.locator(':focus')
+    await expect(elementInFocus).toHaveText('Gå videre')
     await page.keyboard.press('Enter')
     await sjekkMainContentFokus(page)
 
-    /*
+    
 
     await expect(page.getByRole('heading', { name: 'Oppsummering fra søknaden' })).toBeVisible()
     await page.keyboard.press('Tab')
@@ -176,6 +172,6 @@ async function elementInFocusLocator() {
     await expect(page.getByText('Søknaden er sendt til NAV')).toBeVisible()
 
     await expect(page.getByText(/Mottatt.*kl/, { exact: false })).toBeVisible()
-   /*
-     */
+   
+    
 })
