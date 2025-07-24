03a0dd4aba8000ccf9e5287de707c2a209651d0d
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-07-24 16:46:30 +0200

    playwright test for fremtidig-soknad.spec.ts

--- cypress/e2e/alle_tester.cy.ts
+++ cypress/e2e/alle_tester.cy.ts
@@ -12,7 +12,6 @@ import './run-0/yrkesskade.cy'
 import './run-0/selvstendig-naringsdrivende.cy'
 import './run-0/gammel-oppsummering.cy'
 
-import './run-2/fremtidig_soknad.cy'
 import './run-2/kvittering.cy'
 import './run-2/kontonummer.cy'
 import './run-2/opphold_utland_med_ferie.cy'
