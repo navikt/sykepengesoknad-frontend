d029f8819c53c33ad5ca3726244f22f86aaac63a
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-07-24 16:08:44 +0200

    playwright test for datovelger-komp.spec.ts

---
+++ playwright/datovelger-komp.spec.ts
@@ -0,0 +1,39 @@
+import { arbeidstaker } from '../src/data/mock/data/soknad/arbeidstaker'
+
+import { test, expect } from './fixtures'
+import { klikkGaVidere, setPeriodeFraTil, svarJaHovedsporsmal } from './utilities'
+
+test.describe('Tester at datovelger viser korrekt feilmelding, og at man ikke kan gå videre uten å velge datoer', () => {
+    const soknad = arbeidstaker
+    const testpersonQuery = ''
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto(`/syk/sykepengesoknad/soknader/${soknad.id}/3${testpersonQuery}`)
+    })
+
+    //TODO Sjekk om denne er duplikat av en annen test i feilmeldinger.spec.ts
+    test('Trigger feilmelding', async ({ page }) => {
+        await svarJaHovedsporsmal(page)
+        await expect(page.getByText('Når tok du ut feriedager?')).toBeVisible()
+
+        await setPeriodeFraTil(page, 16, 23)
+        await page.locator('.navds-date__field-input').first().fill('')
+
+        await klikkGaVidere(page, true)
+
+        await expect(page.locator('.navds-error-message')).toContainText(
+            'Du må oppgi en fra og med dato i formatet dd.mm.åååå',
+        )
+        await expect(page).toHaveURL(new RegExp(`/syk/sykepengesoknad/soknader/${soknad.id}/3`))
+    })
+
+    test('Fyller inn korrekt dato, og går videre', async ({ page }) => {
+        await svarJaHovedsporsmal(page)
+        await page.locator('[data-cy="periode"] .navds-date__field-button').first().click()
+        await page.locator('[data-cy="periode"] .rdp-cell').getByText('16').click()
+        await klikkGaVidere(page)
+
+        await expect(page.locator('.navds-error-message')).toBeHidden()
+        await expect(page).toHaveURL(new RegExp(`/syk/sykepengesoknad/soknader/${soknad.id}/4`))
+    })
+})
