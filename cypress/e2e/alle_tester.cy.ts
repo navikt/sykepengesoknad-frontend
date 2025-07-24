64cf06189e9ae6de69c340fe6b41f06cc1bc49cd
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-07-24 15:59:57 +0200

    Fjerner fisker test ettersom den ikke tester noe

--- cypress/e2e/alle_tester.cy.ts
+++ cypress/e2e/alle_tester.cy.ts
@@ -14,7 +14,6 @@ import './run-0/gammel-oppsummering.cy'
 
 import './run-2/datovelger_komp.cy'
 import './run-2/endring-uten-endring.cy'
-import './run-2/fisker.cy'
 import './run-2/fremtidig_soknad.cy'
 import './run-2/kvittering.cy'
 import './run-2/kontonummer.cy'
--- cypress/e2e/run-2/fisker.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Tester søknad med fisker som arbeidssituasjon', () => {
-    before(() => {
-        cy.visit('/syk/sykepengesoknad?testperson=fisker')
-        cy.get('.navds-heading--large').should('be.visible').and('have.text', 'Søknader')
-    })
-})
