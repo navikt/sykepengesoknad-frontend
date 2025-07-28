bdcf74ea0a53831d1aa453982e779e9c83893fff
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-07-28 10:47:43 +0200

    endrer import av test og excpect for uu

--- playwright/3_absolutt_tvang.spec.ts
+++ playwright/3_absolutt_tvang.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from './fixtures'
 
 test.describe('Tester at åpne sykmeldinger må sendes inn', () => {
     test.describe('Tester med en usendt sykmelding', () => {
