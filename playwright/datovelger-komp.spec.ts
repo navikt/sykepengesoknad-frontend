79551aca57bf66b02207c1fd36477c728f780773
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-08-15 10:20:28 +0200

    playwright utility funksjon ser ut til å virke

--- playwright/datovelger-komp.spec.ts
+++ playwright/datovelger-komp.spec.ts
@@ -2,6 +2,7 @@ import { arbeidstaker } from '../src/data/mock/data/soknad/arbeidstaker'
 
 import { test, expect } from './fixtures'
 import { klikkGaVidere, setPeriodeFraTil, svarJaHovedsporsmal } from './utilities'
+import { validerAxeUtilityWrapper } from './uuvalidering'
 
 test.describe('Tester at datovelger viser korrekt feilmelding, og at man ikke kan gå videre uten å velge datoer', () => {
     const soknad = arbeidstaker
@@ -11,7 +12,7 @@ test.describe('Tester at datovelger viser korrekt feilmelding, og at man ikke ka
         await page.goto(`/syk/sykepengesoknad/soknader/${soknad.id}/3${testpersonQuery}`)
     })
 
-    test('Trigger feilmelding', async ({ page }) => {
+    test('Trigger feilmelding', async ({ page }, testInfo) => {
         await svarJaHovedsporsmal(page)
         await expect(page.getByText('Når tok du ut feriedager?')).toBeVisible()
 
@@ -24,9 +25,11 @@ test.describe('Tester at datovelger viser korrekt feilmelding, og at man ikke ka
             'Du må oppgi en fra og med dato i formatet dd.mm.åååå',
         )
         await expect(page).toHaveURL(new RegExp(`/syk/sykepengesoknad/soknader/${soknad.id}/3`))
+
+        await validerAxeUtilityWrapper(page, testInfo)
     })
 
-    test('Fyller inn korrekt dato, og går videre', async ({ page }) => {
+    test('Fyller inn korrekt dato, og går videre', async ({ page }, testInfo) => {
         await svarJaHovedsporsmal(page)
         await page.locator('[data-cy="periode"] .navds-date__field-button').first().click()
         await page.locator('[data-cy="periode"] .rdp-cell').getByText('16').click()
@@ -34,5 +37,7 @@ test.describe('Tester at datovelger viser korrekt feilmelding, og at man ikke ka
 
         await expect(page.locator('.navds-error-message')).toBeHidden()
         await expect(page).toHaveURL(new RegExp(`/syk/sykepengesoknad/soknader/${soknad.id}/4`))
+
+        await validerAxeUtilityWrapper(page, testInfo)
     })
 })
