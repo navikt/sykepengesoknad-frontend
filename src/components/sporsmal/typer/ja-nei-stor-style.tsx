b474388eef2429f2179827f564d0fa4dfc5197c4
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-07-22 10:22:27 +0200

    Fiks fokusring og fullbredde på stor ja/nei-knapp

--- src/components/sporsmal/typer/ja-nei-stor-style.tsx
+++ src/components/sporsmal/typer/ja-nei-stor-style.tsx
@@ -4,11 +4,11 @@ import { cn } from '../../../utils/tw-utils'
 
 export function jaNeiStorStyle(value: string, watch: any, error: boolean, mt = false) {
     return cn(
-        'focus-within:outline focus-within:outline-[3px] focus-within:outline-ax-border-focus focus-within:outline-offset-[3px] mb-2 block w-full [&>label]:rounded-sm [&>label]:border-2 [&>label]:border-ax-border-neutral [&>label]:px-4 [&>label]:py-4 text-ax-text-neutral hover:[&>label]:bg-ax-bg-accent-moderate-hover ax-md:mb-0 ax-md:w-1/2 rounded-sm focus:outline-hidden focus:ring-3 focus:ring-ax-accent-900 focus:ring-opacity-50',
+        '[&:focus-within::after]:hidden focus-within:outline focus-within:outline-[3px] focus-within:outline-ax-border-focus focus-within:outline-offset-[3px] relative mb-2 grid w-full grid-cols-[auto_1fr] items-center rounded-sm border-2 border-ax-border-neutral px-4 py-4 text-ax-text-neutral hover:bg-ax-bg-accent-moderate-hover ax-md:mb-0 ax-md:w-1/2 [&>label]:absolute [&>label]:inset-0 [&>label]:flex [&>label]:cursor-pointer [&>label]:items-center [&>label]:rounded-sm [&>label]:pl-12 [&>label]:pr-4',
         {
-            '[&>label]:bg-ax-bg-accent-soft [&>label]:border-ax-border-accent': watch === value,
+            'bg-ax-bg-accent-soft border-ax-border-accent': watch === value,
             'mt-4': mt,
-            '[&>label]:border-b-ax-border-danger [&>label]:border-ax-border-danger text-ax-text-danger-subtle': error,
+            'border-ax-border-danger text-ax-text-danger-subtle': error,
         },
     )
 }
