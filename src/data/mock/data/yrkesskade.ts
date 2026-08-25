808a82a8ea332fb437b859c11d3ee7df6d8a1f72
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-08-25 13:42:01 +0200

    Unngår delt spørsmålsreferanse mellom yrkesskade-personas

--- src/data/mock/data/yrkesskade.ts
+++ src/data/mock/data/yrkesskade.ts
@@ -1,5 +1,6 @@
 import type { RSSoknad } from '../../../types/rs-types/rs-soknad'
 import { deepcopyMedNyId } from '../deepcopyMedNyId'
+import { jsonDeepCopy } from '../../../utils/json-deep-copy'
 
 import type { Persona } from './personas/personas'
 import { brukertestSoknad, brukertestSykmelding } from './personas/brukertestPerosn'
@@ -9,14 +10,14 @@ import { yrkesskadeV1Sporsmal } from './sporsmal/yrkesskade-v1'
 export function medYrkesskadeV1Sporsmal(soknad: RSSoknad): RSSoknad {
     return {
         ...soknad,
-        sporsmal: [...soknad.sporsmal.slice(0, 8), yrkesskadeV1Sporsmal, ...soknad.sporsmal.slice(8)],
+        sporsmal: [...soknad.sporsmal.slice(0, 8), jsonDeepCopy(yrkesskadeV1Sporsmal), ...soknad.sporsmal.slice(8)],
     }
 }
 
 export function medYrkesskadeV2Sporsmal(soknad: RSSoknad): RSSoknad {
     return {
         ...soknad,
-        sporsmal: [...soknad.sporsmal.slice(0, 8), yrkesskadeV2Sporsmal, ...soknad.sporsmal.slice(8)],
+        sporsmal: [...soknad.sporsmal.slice(0, 8), jsonDeepCopy(yrkesskadeV2Sporsmal), ...soknad.sporsmal.slice(8)],
     }
 }
 
