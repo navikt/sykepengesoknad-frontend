88606e8e9c15c8546bf745391c5e1143b25da075
Malin.Haugsdal <Malin.Haugsdal@nav.no>
2026-08-25 10:11:04 +0200

    Endre format på liste over andre inntektskilder
    Co-authored-by: Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>

--- src/components/sporsmal/inntektsbulletpoints.tsx
+++ src/components/sporsmal/inntektsbulletpoints.tsx
@@ -1,44 +1,30 @@
-import { BodyShort, Label } from '@navikt/ds-react'
+import { BodyShort, Label, List } from '@navikt/ds-react'
 import React from 'react'
-import { CheckmarkCircleIcon } from '@navikt/aksel-icons'
 
 import { KjentInntektskilde, Soknad, Sporsmal } from '../../types/types'
 
 export const Inntektsbulletpoints = ({ soknad, sporsmal }: { soknad: Soknad; sporsmal: Sporsmal }) => {
-    const navn: string[] = []
+    const navnListe: string[] = []
     if (sporsmal.metadata) {
         const items = sporsmal.metadata.kjenteInntektskilder as KjentInntektskilde[]
-        items.forEach((item) => navn.push(item.navn))
+        items.forEach((item) => navnListe.push(item.navn))
     } else {
-        navn.push(soknad.arbeidsgiver!.navn)
-        soknad.inntektskilderDataFraInntektskomponenten?.forEach((inntektskilde) => navn.push(inntektskilde.navn))
+        navnListe.push(soknad.arbeidsgiver!.navn)
+        soknad.inntektskilderDataFraInntektskomponenten?.forEach((inntektskilde) => navnListe.push(inntektskilde.navn))
     }
 
     return (
         <>
-            <Label as="p" className="mb-4 mt-10">
+            <Label as="p" className="mt-10">
                 Arbeidsforhold vi har registrert på deg:
             </Label>
+            <BodyShort>Hentet fra offentlige register.</BodyShort>
 
-            <ul aria-label="Inntektskilder fra Aa-registeret" className="mb-10">
-                {navn?.map((n, index) => (
-                    <ListItemWithIcon key={index} content={n} />
+            <List aria-label="Inntektskilder fra Aa-registeret" className="mt-4 mb-10">
+                {navnListe?.map((bedriftNavn, index) => (
+                    <List.Item key={index}>{bedriftNavn}</List.Item>
                 ))}
-            </ul>
+            </List>
         </>
     )
 }
-
-const ListItemWithIcon = ({ content }: { content: string }) => (
-    <li className="mb-4 flex w-full ax-md:max-w-[320px] items-center gap-4 rounded-lg bg-ax-neutral-100 p-2 min-h-[48px]">
-        <CheckmarkCircleIcon
-            className="m-width-[24px] m-height-[24px] pt1  text-xl font-ax-bold shrink-0"
-            height={24}
-            width={24}
-            aria-hidden="true"
-            focusable="false"
-            role="presentation"
-        />
-        <BodyShort>{content}</BodyShort>
-    </li>
-)
