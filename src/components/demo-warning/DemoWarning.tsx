7f78aa72ac73e26244bbc1ff1e535e3e399e8971
LotusSVS <125738218+LotusSVS@users.noreply.github.com>
2026-09-07 15:16:53 +0200

    Bruk InfoCard for Demo Warning (#4246)
    Co-authored-by: Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>

--- src/components/demo-warning/DemoWarning.tsx
+++ src/components/demo-warning/DemoWarning.tsx
@@ -1,41 +1,19 @@
-import { GlobalAlert } from '@navikt/ds-react'
-import React, { useState } from 'react'
-import { differenceInDays } from 'date-fns'
+import { InfoCard } from '@navikt/ds-react'
 
 import { isOpplaering } from '../../utils/environment'
-import { now, toDate } from '../../utils/dato-utils'
+import { ExclamationmarkTriangleIcon } from '@navikt/aksel-icons'
 
 const DemoWarning = () => {
-    const [alertLukket, setAlertLukket] = useState(() => {
-        const lukketDato = localStorage.getItem('demo-warning-lukket')
-        if (lukketDato) {
-            const lagretDato = toDate(lukketDato)
-            const forskjellIDager = differenceInDays(now(), lagretDato)
-
-            // Sjekk om det har gått mer enn 14 dager
-            return forskjellIDager < 14
-        }
-        return false
-    })
     if (!isOpplaering()) {
-        return null
-    }
-    if (alertLukket) {
         return null
     }
 
     return (
-        <GlobalAlert status="warning">
-            <GlobalAlert.Header>
-                <GlobalAlert.Title>Dette er en demoside og inneholder ikke dine personlige data.</GlobalAlert.Title>
-                <GlobalAlert.CloseButton
-                    onClick={() => {
-                        setAlertLukket(true)
-                        localStorage.setItem('demo-warning-lukket', new Date().toISOString())
-                    }}
-                />
-            </GlobalAlert.Header>
-        </GlobalAlert>
+        <InfoCard data-color="warning">
+            <InfoCard.Message icon={<ExclamationmarkTriangleIcon aria-hidden />}>
+                Dette er en demoside og inneholder ikke dine personlige data.
+            </InfoCard.Message>
+        </InfoCard>
     )
 }
 
