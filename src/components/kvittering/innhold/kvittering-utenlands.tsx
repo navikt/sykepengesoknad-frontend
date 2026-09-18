296eda43fa914fab05c6310a8659390e83f4dfaa
Malin.Haugsdal <Malin.Haugsdal@nav.no>
2026-09-18 14:28:20 +0200

    Erstatter utdaterte komponenter i kvittering-panel.tsx og kvittering-utenlands.tsx
    Co-authored-by: Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>

--- src/components/kvittering/innhold/kvittering-utenlands.tsx
+++ src/components/kvittering/innhold/kvittering-utenlands.tsx
@@ -1,4 +1,4 @@
-import { BodyLong, Heading, Label } from '@navikt/ds-react'
+import { BodyLong, Heading, HStack, Label, VStack } from '@navikt/ds-react'
 import React from 'react'
 import { InformationSquareFillIcon } from '@navikt/aksel-icons'
 
@@ -7,8 +7,6 @@ import { tekst } from '../../../utils/tekster'
 import { LenkeMedIkon } from '../../lenke-med-ikon/LenkeMedIkon'
 import { useSoknadMedDetaljer } from '../../../hooks/useSoknadMedDetaljer'
 import { KvitteringPanel } from '../kvittering-panel'
-import GridItems from '../grid-items'
-
 export const KvitteringUtenlands = () => {
     const { valgtSoknad } = useSoknadMedDetaljer()
 
@@ -19,50 +17,44 @@ export const KvitteringUtenlands = () => {
     }
 
     return (
-        <KvitteringPanel>
-            <GridItems
-                venstre={
-                    <div className="flex h-full items-center justify-center">
-                        <InformationSquareFillIcon
-                            aria-hidden={true}
-                            title=""
-                            fontSize="1.5rem"
-                            className="text-ax-text-info-decoration"
-                        />
-                    </div>
-                }
-            >
-                <Heading size="small" level="3" className="my-4">
+        <KvitteringPanel className="p-4 pb-8">
+            <HStack gap="space-16" align="center" justify="start">
+                <InformationSquareFillIcon
+                    aria-hidden={true}
+                    title=""
+                    fontSize="1.5rem"
+                    className="text-ax-text-info-decoration"
+                />
+                <Heading size="small" level="3">
                     {tekst('kvittering.hva-skjer-videre')}
                 </Heading>
-            </GridItems>
-            <GridItems>
-                <div className="mb-6 mt-2">
-                    <Label as="h2">{tekst('kvittering.utenlands.overskrift1')}</Label>
-                    <BodyLong spacing as="span">
-                        {tekst('kvittering.utenlands.brodtekst1')}{' '}
-                    </BodyLong>
-                </div>
-                <div className="my-6">
-                    <Label as="h2">{tekst('kvittering.utenlands.overskrift2')}</Label>
-                    <BodyLong as="ul">
-                        <li>{tekst('kvittering.utenlands.liste1')}</li>
-                        <li>{tekst('kvittering.utenlands.liste2')}</li>
-                        <li>{tekst('kvittering.utenlands.liste3')}</li>
-                    </BodyLong>
-                </div>
-                <LenkeMedIkon
-                    href={tekst('kvittering.utenlands.lenke.url')}
-                    text={tekst('kvittering.utenlands.lenke')}
-                />
-                .
-                <div className="mt-6">
-                    <Label as="h2">{tekst('kvittering.utenlands.overskrift3')}</Label>
-                    <BodyLong spacing as="span">
-                        {tekst('kvittering.utenlands.brodtekst3')}{' '}
-                    </BodyLong>
-                </div>
-            </GridItems>
+                <VStack paddingInline="space-40 space-12">
+                    <div className="mb-6 mt-2">
+                        <Label as="h2">{tekst('kvittering.utenlands.overskrift1')}</Label>
+                        <BodyLong spacing as="span">
+                            {tekst('kvittering.utenlands.brodtekst1')}{' '}
+                        </BodyLong>
+                    </div>
+                    <div className="my-6">
+                        <Label as="h2">{tekst('kvittering.utenlands.overskrift2')}</Label>
+                        <BodyLong as="ul">
+                            <li>{tekst('kvittering.utenlands.liste1')}</li>
+                            <li>{tekst('kvittering.utenlands.liste2')}</li>
+                            <li>{tekst('kvittering.utenlands.liste3')}</li>
+                        </BodyLong>
+                    </div>
+                    <LenkeMedIkon
+                        href={tekst('kvittering.utenlands.lenke.url')}
+                        text={tekst('kvittering.utenlands.lenke')}
+                    />
+                    <div className="mt-6">
+                        <Label as="h2">{tekst('kvittering.utenlands.overskrift3')}</Label>
+                        <BodyLong spacing as="span">
+                            {tekst('kvittering.utenlands.brodtekst3')}{' '}
+                        </BodyLong>
+                    </div>
+                </VStack>
+            </HStack>
         </KvitteringPanel>
     )
 }
--- src/components/kvittering/kvittering-panel.tsx
+++ src/components/kvittering/kvittering-panel.tsx
@@ -1,17 +1,12 @@
 import React, { ReactNode } from 'react'
-import { Panel } from '@navikt/ds-react'
-
-import { cn } from '../../utils/tw-utils'
+import { Box } from '@navikt/ds-react'
 
 export function KvitteringPanel({ children, className }: { children: ReactNode[] | ReactNode; className?: string }) {
     return (
-        <Panel
-            role="region"
-            aria-label="Hva skjer videre?"
-            border
-            className={cn('grid grid-cols-12 gap-y-2 p-0 pb-8', className)}
-        >
-            {children}
-        </Panel>
+        <div>
+            <Box borderRadius="2" borderWidth="1" borderColor="neutral-strong" className={className}>
+                {children}
+            </Box>
+        </div>
     )
 }
